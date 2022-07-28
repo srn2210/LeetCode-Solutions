d4cd774023fca0f0c0ba52a9c5793c23392aa211
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-07-28 22:40:43 +0530

    Time: 1 ms (99.04%), Space: 45.2 MB (7.70%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.java
+++ 110-balanced-binary-tree/110-balanced-binary-tree.java
@@ -3,6 +3,7 @@ class Solution {
     int dfs(TreeNode node) {
         if(node == null) return 0;
         if(ans == false) return 0;
+        
         int left = 0;
         int right = 0;
         
@@ -10,7 +11,7 @@ class Solution {
         right += dfs(node.right);
         
         if(Math.abs(left - right) > 1) ans = false;
-        return left > right ? left+1 : right+1;
+        return left > right ? left + 1 : right + 1;
     }
     public boolean isBalanced(TreeNode root) {
         dfs(root);
