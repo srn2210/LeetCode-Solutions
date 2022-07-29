0b7c8fb38b352e2aa8b83ae1215d55bbdd98ac4d
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-07-30 00:24:09 +0530

    Time: 68 ms (16.19%), Space: 45.1 MB (58.97%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,35 +1,23 @@
 class Solution {
-        public int coinChange(int[] coins, int amount) {
-        if (amount == 0) {
-            return 0;
-        }
-
-        Queue<Integer> queue = new LinkedList<>();
-        boolean[] visited = new boolean[amount + 1];
-
-        queue.offer(amount);
-        visited[amount] = true;
-        int currLevel = 1;
-
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            System.out.println(queue);
-            for (int i = 0; i < size; i++) {
-                int curr = queue.poll();
-
-                for (int coin : coins) {
-                    int child = curr - coin;
-                    if (child == 0) {
-                        return currLevel;
-                    } else if (child > 0 && !visited[child]) {
-                        queue.add(child);
-                        visited[child] = true;
-                    }
-                }
+    int helper(int[] coins, int rem, int[] dp) {
+        if(rem < 0) return -1;
+        
+        if(rem == 0) return 0;
+        
+        if(dp[rem] != 0) return dp[rem];
+        
+        int min = Integer.MAX_VALUE;
+        
+        for(int i:coins) {
+            int temp = helper(coins, rem-i, dp);
+            if(temp != -1 && temp < min) {
+                min = 1 + temp;
             }
-            currLevel++;
         }
-
-        return -1;
+        dp[rem] = (min == Integer.MAX_VALUE) ? -1 : min;
+        return dp[rem];
+    }
+    public int coinChange(int[] coins, int amount) {
+        return helper(coins, amount, new int[amount+1]);
     }
 }
