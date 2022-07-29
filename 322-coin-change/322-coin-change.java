cce542ed12d14444b7170abfa896d6d8d664c510
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-07-30 00:21:02 +0530

    Time: 70 ms (15.57%), Space: 45.4 MB (39.23%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,19 +1,23 @@
-public class Solution {
-public int coinChange(int[] coins, int amount) {
-    if(amount<1) return 0;
-    int[] dp = new int[amount+1];
-    int sum = 0;
-    
-	while(++sum<=amount) {
-		int min = -1;
-    	for(int coin : coins) {
-    		if(sum >= coin && dp[sum-coin]!=-1) {
-    			int temp = dp[sum-coin]+1;
-    			min = min<0 ? temp : (temp < min ? temp : min);
-    		}
-    	}
-    	dp[sum] = min;
-	}
-	return dp[amount];
-}
+class Solution {
+    int helper(int[] coins, int rem, int[] dp) {
+        if(rem < 0) return -1;
+        
+        if(rem == 0) return 0;
+        
+        if(dp[rem] != 0) return dp[rem];
+        
+        int min = Integer.MAX_VALUE;
+        
+        for(int i:coins) {
+            int temp = helper(coins, rem-i, dp);
+            if(temp != -1 && temp < min) {
+                min = 1 + temp;
+            }
+        }
+        dp[rem] = (min == Integer.MAX_VALUE) ? -1 : min;
+        return dp[rem];
+    }
+    public int coinChange(int[] coins, int amount) {
+        return helper(coins, amount, new int[amount+1]);
+    }
 }
