ce7701e986988b4a38c5bf112c96362e3a1d1e7a
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-07-30 10:34:40 +0530

    Time: 2 ms (73.96%), Space: 82.4 MB (5.95%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -3,10 +3,11 @@ class Solution {
         if(nums.length == 1) return nums[0];
         int max = nums[0];
         
-        for(int i=1; i<nums.length; i++){
+        for(int i=1; i<nums.length; i++) {
             nums[i] = Math.max(nums[i], nums[i] + nums[i-1]);
             max = Math.max(max, nums[i]);
         }
+        
         return max;
     }
 }
