cc1c4dd1d1ba74914d015333116f33a33e876a75
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-08-02 07:48:56 +0530

    Time: 1 ms (55.29%), Space: 40.6 MB (71.76%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,5 +1,6 @@
 class Solution {
     public int uniquePaths(int m, int n) {
+        if(m==1 || n==1) return 1;
         int[][] dp = new int[m][n];
         
         for(int i=0; i<n; i++) {
@@ -8,8 +9,6 @@ class Solution {
         for(int i=0; i<m; i++) {
             dp[i][0] = 1;
         }
-        
-        if(m==1 || n==1) return 1;
         
         for(int i=1; i<m; i++) {
             for(int j=1; j<n; j++) {
