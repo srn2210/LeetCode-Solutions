83661529c51a0c3c2b9fc09d2938cf3dc67a28da
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-08-02 23:33:03 +0530

    Time: 2 ms (89.27%), Space: 56.7 MB (25.16%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,6 +1,13 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length/2];
+        int count = 0;
+        Integer candidate = null;
+        
+        for(int i:nums) {
+            if(count == 0) candidate = i;
+            count += candidate == i ? 1 : -1;
+        }
+        
+        return candidate;
     }
 }
