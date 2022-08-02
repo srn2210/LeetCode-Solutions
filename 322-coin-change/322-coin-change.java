664d55a5117d710809e8c5ce8c9670b57d4a4eef
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-08-02 08:28:38 +0530

    Time: 47 ms (30.70%), Space: 43.6 MB (85.65%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,5 +1,5 @@
 class Solution {
-    int helper(int[] coins, int rem, int[] dp) {
+    /*int helper(int[] coins, int rem, int[] dp) {
         if(rem < 0) return -1;
         
         if(rem == 0) return 0;
@@ -16,8 +16,22 @@ class Solution {
         }
         dp[rem] = (min == Integer.MAX_VALUE) ? -1 : min;
         return dp[rem];
-    }
+    }*/
     public int coinChange(int[] coins, int amount) {
-        return helper(coins, amount, new int[amount+1]);
+        //return helper(coins, amount, new int[amount+1]);
+        int[] dp = new int[amount+1];
+        
+        Arrays.fill(dp, -1);
+        dp[0] = 0;
+        
+        for(int i=1; i<=amount; i++) {
+            int min = Integer.MAX_VALUE;
+            for(int j=0; j<coins.length && j<=i; j++) {
+                if(i-coins[j] >= 0 && dp[i-coins[j]] != -1) min = Math.min(min, 1+dp[i-coins[j]]);
+                dp[i] = min == Integer.MAX_VALUE ? dp[i] : min;
+            }
+        }
+        
+        return dp[amount];
     }
 }
