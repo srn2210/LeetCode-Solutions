8ec95d1f3b901fe894c7b5bd3df7b89c210d023b
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-08-02 08:29:44 +0530

    Time: 26 ms (64.50%), Space: 45.7 MB (36.45%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,24 +1,5 @@
 class Solution {
-    /*int helper(int[] coins, int rem, int[] dp) {
-        if(rem < 0) return -1;
-        
-        if(rem == 0) return 0;
-        
-        if(dp[rem] != 0) return dp[rem];
-        
-        int min = Integer.MAX_VALUE;
-        
-        for(int i:coins) {
-            int temp = helper(coins, rem-i, dp);
-            if(temp != -1 && temp < min) {
-                min = 1 + temp;
-            }
-        }
-        dp[rem] = (min == Integer.MAX_VALUE) ? -1 : min;
-        return dp[rem];
-    }*/
     public int coinChange(int[] coins, int amount) {
-        //return helper(coins, amount, new int[amount+1]);
         int[] dp = new int[amount+1];
         
         Arrays.fill(dp, -1);
@@ -26,8 +7,8 @@ class Solution {
         
         for(int i=1; i<=amount; i++) {
             int min = Integer.MAX_VALUE;
-            for(int j=0; j<coins.length && j<=i; j++) {
-                if(i-coins[j] >= 0 && dp[i-coins[j]] != -1) min = Math.min(min, 1+dp[i-coins[j]]);
+            for(int j : coins) {
+                if(i-j >= 0 && dp[i-j] != -1) min = Math.min(min, 1+dp[i-j]);
                 dp[i] = min == Integer.MAX_VALUE ? dp[i] : min;
             }
         }
