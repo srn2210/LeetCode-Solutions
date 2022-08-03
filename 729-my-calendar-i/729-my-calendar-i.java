773aaf88523c1022e0cd1a8403b739bf086ebd2e
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-08-03 15:40:26 +0530

    Time: 281 ms (7.30%), Space: 42.7 MB (99.45%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.java
+++ 729-my-calendar-i/729-my-calendar-i.java
@@ -8,11 +8,10 @@ class MyCalendar {
         if(set == null) {
             List<Integer> temp = new ArrayList();
             temp.add(start);
-            temp.add(end-1);
+            temp.add(end);
             set.add(temp);
             return true;
         }
-        //Iterator entry = set.iterator();
         for(List<Integer> entry: set) {
             if( (start >= (int)entry.get(0) && start < (int)entry.get(1)) || (end > (int)entry.get(0) && end <= (int)entry.get(1)) || (start <= (int)entry.get(0) && end >= (int)entry.get(1))) return false;
         }
