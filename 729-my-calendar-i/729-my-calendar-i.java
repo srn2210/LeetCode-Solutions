954dc5bc2a5f7830ec332d6d86ac26a09539d605
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-08-03 15:41:19 +0530

    Time: 278 ms (7.36%), Space: 43 MB (93.15%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.java
+++ 729-my-calendar-i/729-my-calendar-i.java
@@ -22,9 +22,3 @@ class MyCalendar {
         return true;
     }
 }
-
-/**
- * Your MyCalendar object will be instantiated and called as such:
- * MyCalendar obj = new MyCalendar();
- * boolean param_1 = obj.book(start,end);
- */
