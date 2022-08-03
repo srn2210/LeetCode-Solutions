645eabead23e703b0a7da56d2b45069db4444526
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-08-03 21:55:51 +0530

    Time: 198 ms (15.01%), Space: 128.7 MB (56.72%) - LeetHub

--- 307-range-sum-query-mutable/307-range-sum-query-mutable.java
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.java
@@ -1,49 +1,53 @@
-class NumArray {
-    int[] arr;
-    int len;
-    int[] b;
-    public NumArray(int[] nums) {
-        this.arr = nums;
-        double d = Math.sqrt(nums.length);
-        len = (int) Math.ceil(nums.length/d);
-        b = new int[len];
-        
-        for(int i=0; i<nums.length; i++) {
-            b[i/len] += nums[i];
-        }
+class NumArray{
+int[] tree;
+int n;
+public NumArray(int[] nums) {
+    if (nums.length > 0) {
+        n = nums.length;
+        tree = new int[n * 2];
+        buildTree(nums);
     }
-    
-    public void update(int index, int val) {        
-        int in = index/len;
-        
-        b[in] = b[in] - arr[index] + val;
-        
-        arr[index] = val;
-    }
-    
-    public int sumRange(int left, int right) {
-        int res = 0;
-        
-        int l = left/len;
-        int r = right/len;
-        
-        if(l == r) {
-            for(int i=left; i<=right; i++) {
-                res += arr[i];
-            }
+}
+private void buildTree(int[] nums) {
+    for (int i = n, j = 0;  i < 2 * n; i++,  j++)
+        tree[i] = nums[j];
+    for (int i = n - 1; i > 0; --i)
+        tree[i] = tree[i * 2] + tree[i * 2 + 1];
+}
+void update(int pos, int val) {
+    pos += n;
+    tree[pos] = val;
+    while (pos > 0) {
+        int left = pos;
+        int right = pos;
+        if (pos % 2 == 0) {
+            right = pos + 1;
+        } else {
+            left = pos - 1;
         }
-        else {
-            for(int i=left; i< (l+1) * len; i++) {
-                res += arr[i];
-            }
-            for(int i=l+1; i<r; i++) {
-                res += b[i];
-            }
-            for(int i=r*len; i<=right; i++) {
-                res += arr[i];
-            }
-        }
-        
-        return res;
+        // parent is updated after child is updated
+        tree[pos / 2] = tree[left] + tree[right];
+        pos /= 2;
     }
+}
+public int sumRange(int l, int r) {
+    // get leaf with value 'l'
+    l += n;
+    // get leaf with value 'r'
+    r += n;
+    int sum = 0;
+    while (l <= r) {
+        if ((l % 2) == 1) {
+           sum += tree[l];
+           l++;
+        }
+        if ((r % 2) == 0) {
+           sum += tree[r];
+           r--;
+        }
+        l /= 2;
+        r /= 2;
+    }
+    return sum;
+}
 }
