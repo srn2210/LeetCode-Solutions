9fae4cb8ef9fb7c317f69b74b5767d36bd24b82a
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-08-04 23:42:29 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (69.44%) - LeetHub

--- 858-mirror-reflection/858-mirror-reflection.java
+++ 858-mirror-reflection/858-mirror-reflection.java
@@ -1,9 +1,16 @@
 class Solution {
+    int gcd(int p, int q) {
+        if(p == 0) return q;
+        else if(q == 0) return p;
+        if(p == q) return p;
+        if(p > q) return gcd(p-q,q);
+        else return gcd(q-p, p);
+    }
     public int mirrorReflection(int p, int q) {
-        int m = q;
+        /*int m = q;
         int n = p;
         
-        while( m % 2 == 0 && n % 2 == 0) {
+        while(m % 2 == 0 && n % 2 == 0) {
             m /= 2;
             n /= 2;
         }
@@ -17,6 +24,20 @@ class Solution {
         }
         else {
             return 1;
-        }
+        }*/
+        
+        //int res = 0;
+        
+        int gcd = gcd(p,q);
+        
+        int x = (p / gcd) % 2;
+        
+        int y = (q / gcd) % 2;
+        
+        if(x % 2 == 0) return 2;
+        
+        else if(y % 2 != 0 && x % 2 != 0) return 1;
+        
+        else return 0;
     }
 }
