a19fbb2d4246822c3c12467b1d30477d578a2860
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-08-05 16:39:16 +0530

    Time: 1 ms (92.94%), Space: 41.1 MB (77.40%) - LeetHub

---
+++ 377-combination-sum-iv/377-combination-sum-iv.java
@@ -0,0 +1,21 @@
+class Solution {
+    int helper(int[] arr, int rem, int[] dp) {
+        if(dp[rem] != -1) return dp[rem];
+        int res = 0;
+        for(int i=0; i<arr.length; i++) {
+            if(rem >= arr[i]) {
+                res += helper(arr, rem - arr[i], dp);
+            }
+        }
+        dp[rem] = res;
+        return dp[rem];
+    }
+    public int combinationSum4(int[] nums, int target) {
+        int[] dp = new int[target+1];
+        Arrays.fill(dp, -1);
+        dp[0] = 1;
+        
+        
+        return helper(nums, target, dp);
+    }
+}
