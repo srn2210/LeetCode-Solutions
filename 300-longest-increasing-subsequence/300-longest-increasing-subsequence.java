7afaa294c63ebdf48a75c73e912e4b166dcfd356
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-08-08 12:11:24 +0530

    Time: 92 ms (35.13%), Space: 43.9 MB (77.84%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,51 +1,21 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        //int[] dp = new int[nums.length];
-        TreeMap<Integer, Integer> tm = new TreeMap();
+        int[] dp = new int[nums.length];
+        //TreeMap<Integer, Integer> tm = new TreeMap();
         int ans = Integer.MIN_VALUE;
         
         
         for(int i=0; i<nums.length; i++) {
-            /*for(int j=i; j>=0; j--) {
+            for(int j=i; j>=0; j--) {
                 if(nums[j] < nums[i]) {
-                    dp[i] = dp[j] + 1;
-                    break;
-                }
-                else if(nums[j] == nums[i] && i != j) {
-                    dp[i] = dp[j];
-                    break;
-                }
-            }*/
-            Integer prev = tm.floorKey(nums[i]);
-            
-            //System.out.println("nums val = "+nums[i]+".... prev val = "+prev);
-            int max = 0;
-            if(prev != null) {
-                for(Map.Entry num:tm.entrySet()) {
-                    if((int) num.getKey() >= nums[i]) break;
-                    max = Math.max(tm.get(num.getKey()), max);
+                    dp[i] = Math.max(dp[j], dp[i]);
                 }
             }
             
-            if(prev != null) {
-                tm.put(nums[i], max+1);
-            }
-            else {
-                if(tm.containsKey(nums[i])) {
-                    continue;
-                }
-                else {
-                    tm.put(nums[i], 1);
-                }
-            }
+            dp[i] += 1;
             
-            //if(dp[i] == 0) dp[i] = 1;
-            ans = Math.max(tm.get(nums[i]), ans);
+            ans = Math.max(dp[i], ans);
         }
-        
-        /*for(Map.Entry entry:tm.entrySet()) {
-            System.out.println("key = "+entry.getKey()+"... val = "+entry.getValue());
-        }*/
         
         return ans;
     }
