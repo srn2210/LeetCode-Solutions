73e6bda7f448730854a3eeb364c35ee8d026d174
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-08-15 13:39:43 +0530

    Time: 17 ms (14.37%), Space: 45.3 MB (60.38%) - LeetHub

--- 51-n-queens/51-n-queens.java
+++ 51-n-queens/51-n-queens.java
@@ -12,16 +12,12 @@ class Solution {
         int i=row;
         int j=col-1;
         int k=col+1;
-        //System.out.println(s);
-        //System.out.println(k);
         while(i>=0) {
             if(j >= 0) {
                 if(arr.get(i).equals(qstr.get(j))) return false;
             }
             if(k < qstr.size()) {
-                //System.out.println("func string = "+s+"...before string = "+arr.get(i)+"... after col string = "+qstr.get(k));
                 if(arr.get(i).equals(qstr.get(k))) {
-                    //System.out.println("hi");
                     return false;
                 }
             }
@@ -55,16 +51,13 @@ class Solution {
         }
         qstr = new ArrayList();
         for(int i=0; i<n; i++) {
-            //st.replace(i, "Q");
             st = new StringBuilder();
             for(int j=0; j<n; j++) {
                 if(i == j) st.append("Q");
                 else st.append(".");
             }
             qstr.add(st.toString());
-            //st.replace(i, ".");
         }
-        //System.out.println(qstr);
         solve(res, new ArrayList(), n);
         return res;
     }
