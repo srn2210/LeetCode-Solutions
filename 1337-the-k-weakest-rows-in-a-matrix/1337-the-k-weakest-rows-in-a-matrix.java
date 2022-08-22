cfcc9928ec8ed4cedb72b9e1a3617965fb445f92
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-08-23 00:22:56 +0530

    Time: 4 ms (40.90%), Space: 48.4 MB (75.47%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
@@ -1,22 +1,32 @@
 class Solution {
+    int binarySearch(int[] arr) {
+        int lo = 0;
+        int hi = arr.length;
+        int mid = 0;
+        while(lo < hi) {
+            mid = lo + ((hi-lo)/2);
+            if(mid+1 < hi && (arr[mid] == 1 && arr[mid+1] == 0)) return mid+1;
+            else if( mid-1 >= 0 && (arr[mid] == 0 && arr[mid-1] == 1)) return mid;
+            if(arr[mid] == 0) {
+                hi = mid;
+            }
+            else {
+                lo = mid + 1;
+            }
+        }
+        return mid = mid == 0 ? 0 : mid+1;
+    }
     public int[] kWeakestRows(int[][] mat, int k) {
-        int[][] arr = new int[mat.length][2];
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
         int idx = 0;
         for(int[] i:mat) {
-            int count = 0;
-            for(int j:i) {
-                if(j == 1) {
-                    count++;
-                }
-            }
-            arr[idx][0] = count;
-            arr[idx][1] = idx;
+            int count = binarySearch(i);
+            pq.add(new int[]{count, idx});
             idx++;
         }
-        Arrays.sort(arr, (a,b) -> a[0]-b[0]);
         int[] res = new int[k];
         for(int i=0; i<k; i++) {
-            res[i] = arr[i][1];
+            res[i] = pq.poll()[1];
         }
         return res;
     }
