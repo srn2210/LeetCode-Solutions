66adb52996ecf6bef4bad5ed72c104df5d6f53d2
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-08-27 11:26:29 +0530

    Time: 40 ms (70.99%), Space: 119.4 MB (7.70%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -7,7 +7,7 @@ class Solution {
         for(int i=0; i<points.length; i++) {
             int x = points[i][0];
             int y = points[i][1];
-            int z = (int) (Math.pow(x, 2) + Math.pow(y, 2));
+            int z = (int) (x * x + y * y);
             pq.add(new int[]{x, y, z});
         }
         
