6cbd4e0626ba0bd06c7d0d31c2789a4d35c8dc7c
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-08-27 14:05:12 +0530

    Time: 23 ms (96.99%), Space: 58.9 MB (62.77%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,24 +1,37 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        HashSet<List<Integer>> list = new HashSet<>();
-        HashMap<Integer, Integer> map = new HashMap();
+        List<List<Integer>> list = new ArrayList<>();
+        
+        int prex = Integer.MIN_VALUE;
+        int prey = Integer.MIN_VALUE;
         for(int i=0; i<nums.length; i++) {
-            map.put(nums[i], i);
-        }
-        for(int i=0; i<nums.length; i++) {
-            int z = nums[i];
-            if(z > 0) break;
-            for(int j=i+1; j<nums.length; j++) {
-                if(i == j) continue;
-                int x = nums[j];
-                int y = x + z;
-                if(y > 0) break;
-                y = -1 * y;
-                if(map.containsKey(y) && map.get(y) > i && map.get(y) > j) {
-                    list.add(List.of(x, y, z));
+            int x = nums[i];
+            if(x > 0) break;
+            int l = i+1;
+            int r = nums.length-1;
+            while(l < r) {
+                int y = nums[l];
+                if(x + y > 0) break;
+                int sum = x + y + nums[r];
+                if(sum > 0) {
+                    r--;
                 }
+                else if(sum < 0) {
+                    l++;
+                }
+                else {
+                    list.add(List.of(x, y, nums[r]));
+                    while(l+1 < nums.length && nums[l] == nums[l+1]) {
+                        l++;
+                    }
+                    l++;
+                    r--;
+                }
+                prex = x;
+                prey = y;
             }
+            while(i+1<nums.length && nums[i] == nums[i+1]) i++;
         }
         return new ArrayList(list);
     }
