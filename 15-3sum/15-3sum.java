b67ca249b76b9289d836c0db1f1b7626b43121f3
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-08-27 12:16:10 +0530

    Time: 1793 ms (5.00%), Space: 171.2 MB (5.00%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,25 +1,25 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        HashSet<List<Integer>> l = new HashSet<>();
         Arrays.sort(nums);
-        
-        for(int i=0; i<nums.length-2; i++){
-            int j = i+1;
-            int k = nums.length-1;
+        HashSet<List<Integer>> list = new HashSet<>();
+        HashMap<Integer, Integer> map = new HashMap();
+        for(int i=0; i<nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for(int i=0; i<nums.length; i++) {
+            int z = nums[i];
             
-            while(j<k){
-                int sum = nums[i]+ nums[j]+ nums[k];
-                if(sum == 0){
-                    l.add(List.of(nums[i], nums[j], nums[k]));
-                }
-                if(sum > 0){
-                    k--;
-                }
-                else{
-                    j++;
+            for(int j=i+1; j<nums.length; j++) {
+                if(i == j) continue;
+                int x = nums[j];
+                int y = x + z;
+                y = -1 * y;
+                if(map.containsKey(y) && map.get(y) > i && map.get(y) > j) {
+                    list.add(List.of(x, y, z));
                 }
             }
         }
-        return new ArrayList<>(l);
+        //System.out.println(list);
+        return new ArrayList(list);
     }
 }
