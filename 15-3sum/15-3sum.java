a2805902c7afdd8173b222491f74751f36d5ea30
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-08-27 12:22:19 +0530

    Time: 25 ms (91.12%), Space: 58.3 MB (80.45%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,24 +1,24 @@
 class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
-        HashSet<List<Integer>> list = new HashSet<>();
-        HashMap<Integer, Integer> map = new HashMap();
-        for(int i=0; i<nums.length; i++) {
-            map.put(nums[i], i);
-        }
-        for(int i=0; i<nums.length; i++) {
-            int z = nums[i];
-            
-            for(int j=i+1; j<nums.length; j++) {
-                if(i == j) continue;
-                int x = nums[j];
-                int y = x + z;
-                y = -1 * y;
-                if(map.containsKey(y) && map.get(y) > i && map.get(y) > j) {
-                    list.add(List.of(x, y, z));
+    public List<List<Integer>> threeSum(int[] arr) {
+        Arrays.sort(arr);
+        int n = arr.length;
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > 0) break; // Since arr[i] <= arr[l] <= arr[r], if a[i] > 0 then sum=arr[i]+arr[l]+arr[r] > 0
+            int l = i + 1, r = n - 1;
+            while (l < r) {
+                int sum = arr[i] + arr[l] + arr[r];
+                if (sum < 0) l++;
+                else if (sum > 0) r--;
+                else {
+                    ans.add(Arrays.asList(arr[i], arr[l], arr[r]));
+                    while (l+1 <= r && arr[l] == arr[l+1]) l++; // Skip duplicate nums[l]
+                    l++;
+                    r--;
                 }
             }
+            while (i+1 < n && arr[i+1] == arr[i]) i++; // Skip duplicate nums[i]
         }
-        return new ArrayList(list);
+        return ans;
     }
 }
