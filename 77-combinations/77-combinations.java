fc6aa8b6bbb82e2763a5df49b4694ddf8dfaab9a
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-08-30 22:00:58 +0530

    Time: 3 ms (96.35%), Space: 43.4 MB (96.56%) - LeetHub

--- 77-combinations/77-combinations.java
+++ 77-combinations/77-combinations.java
@@ -1,10 +1,11 @@
 class Solution {
     void backtrack(List<List<Integer>> list, int n, int k, List<Integer> set, int start) {
-        if(set.size() == k) {
+        int size = set.size();
+        if(size == k) {
             list.add(new ArrayList(set));
         }
         else {
-            for(int i=start; i<=n-k+set.size()+1; i++) {
+            for(int i=start; i<=n-k+size+1; i++) {
                 set.add(i);
                 backtrack(list, n, k, set, i+1);
                 set.remove((Integer)i);
