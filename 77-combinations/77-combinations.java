7eabb14ed0fcb11a4064671f67219d6686db4a84
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-08-30 21:44:40 +0530

    Time: 143 ms (6.15%), Space: 54.9 MB (54.46%) - LeetHub

--- 77-combinations/77-combinations.java
+++ 77-combinations/77-combinations.java
@@ -1,19 +1,19 @@
 class Solution {
-    void backtrack(List<List<Integer>> list, int n, int k, Set<Integer> set, int start) {
+    void backtrack(List<List<Integer>> list, int n, int k, List<Integer> set, int start) {
         if(set.size() == k) {
-            list.add(List.copyOf(set));
+            list.add(new ArrayList(set));
         }
         else {
             for(int i=start; i<=n; i++) {
                 set.add(i);
                 backtrack(list, n, k, set, i+1);
-                set.remove(i);
+                set.remove((Integer)i);
             }
         }
     }
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> list = new ArrayList();
-        backtrack(list, n, k, new LinkedHashSet(), 1);
+        backtrack(list, n, k, new ArrayList(), 1);
         return list;
     }
 }
