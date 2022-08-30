0ef658603d714d6b695226a0ea0f81114eedfd5e
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-08-30 21:41:54 +0530

    Time: 114 ms (8.45%), Space: 67.2 MB (13.94%) - LeetHub

--- 77-combinations/77-combinations.java
+++ 77-combinations/77-combinations.java
@@ -5,8 +5,6 @@ class Solution {
         }
         else {
             for(int i=start+1; i<=n; i++) {
-                /*if(set.contains(i))
-                    continue;*/
                 set.add(i);
                 backtrack(list, n, k, set, i);
                 set.remove(i);
