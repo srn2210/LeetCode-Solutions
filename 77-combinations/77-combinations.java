d7185b752fb18d56f98505b782e3b38a75086f7d
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-08-30 22:14:29 +0530

    Time: 4 ms (92.49%), Space: 55.4 MB (25.34%) - LeetHub

--- 77-combinations/77-combinations.java
+++ 77-combinations/77-combinations.java
@@ -1,20 +1,20 @@
 class Solution {
-    void backtrack(List<List<Integer>> list, int n, int k, List<Integer> set, int start) {
-        int size = set.size();
+    void backtrack(List<List<Integer>> list, int n, int k, Deque<Integer> arr, int start) {
+        int size = arr.size();
         if(size == k) {
-            list.add(new ArrayList(set));
+            list.add(new ArrayList(arr));
         }
         else {
             for(int i=start; i<=n-k+size+1; i++) {
-                set.add(i);
-                backtrack(list, n, k, set, i+1);
-                set.remove((Integer)i);
+                arr.offer(i);
+                backtrack(list, n, k, arr, i+1);
+                arr.removeLast();
             }
         }
     }
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> list = new ArrayList();
-        backtrack(list, n, k, new ArrayList(), 1);
+        backtrack(list, n, k, new ArrayDeque(), 1);
         return list;
     }
 }
