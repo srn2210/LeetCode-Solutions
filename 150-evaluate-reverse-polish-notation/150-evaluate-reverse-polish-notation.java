6fe4039230920342b771d53021d429bac9a69bae
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-08-31 20:30:55 +0530

    Time: 7 ms (81.60%), Space: 44.1 MB (51.39%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.java
@@ -3,30 +3,33 @@ class Solution {
         if(tokens.length == 1) return Integer.valueOf(tokens[0]);
         
         Stack<Integer> st = new Stack();
+        int temp = 0;
         
         for(String s:tokens) {
-            if(s.equals("+")) {
-                int temp = st.pop();
-                temp += st.pop();
-                st.push(temp);
-            }
-            else if(s.equals("-")) {
-                int temp = st.pop();
-                temp = st.pop() - temp;
-                st.push(temp);
-            }
-            else if(s.equals("*")) {
-                int temp = st.pop();
-                temp *= st.pop();
-                st.push(temp);
-            }
-            else if(s.equals("/")) {
-                int temp = st.pop();
-                temp = st.pop() / temp;
-                st.push(temp);
-            }
-            else {
-                st.push(Integer.valueOf(s));
+            switch(s) {
+                case "+": 
+                    temp = st.pop();
+                    temp += st.pop();
+                    st.push(temp);
+                    break;
+                case "-":
+                    temp = st.pop();
+                    temp = st.pop() - temp;
+                    st.push(temp);
+                    break;
+                case "*":
+                    temp = st.pop();
+                    temp *= st.pop();
+                    st.push(temp);
+                    break;
+                case "/":
+                    temp = st.pop();
+                    temp = st.pop() / temp;
+                    st.push(temp);
+                    break;
+                default:
+                    st.push(Integer.valueOf(s));
+                    break;
             }
         }
         
