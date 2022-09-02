40491b45f455496cfb191487038ae1dd6b552466
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-09-02 18:38:15 +0530

    Time: 712 ms (47.75%), Space: 22.7 MB (58.78%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,17 +1,15 @@
 class Solution:
-    def solve(self, i: int, j: int, a: str, b: str, dp: List[List[int]]) -> int:
-        ans = 0
-        if(i == len(a) or j == len(b)):
-            return 0
-        elif dp[i][j] != -1:
-            return dp[i][j]
-        elif a[i] == b[j]:
-            dp[i][j] = 1 + self.solve(i+1, j+1, a, b, dp)
-            ans = max(dp[i][j], ans)
-        else:
-            dp[i][j] = max(self.solve(i+1, j, a, b, dp), self.solve(i, j+1, a, b, dp))
-            ans = max(dp[i][j], ans)
-        return ans
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        dp = [[-1 for i in range(len(text2))] for j in range(len(text1))]
-        return self.solve(0, 0, text1, text2, dp)
+        if(len(text2) > len(text1)):
+            return self.longestCommonSubsequence(text2, text1)
+        dp = [[0 for i in range(len(text2)+1)] for j in range(len(text1)+1)]
+        ans = 0
+        for i,c in enumerate(text1):
+            for j,d in enumerate(text2):
+                if(c == d):
+                    dp[i+1][j+1] = 1 + dp[i][j]
+                    ans = max(ans, dp[i][j])
+                else:
+                    dp[i+1][j+1] = max(dp[i+1][j], dp[i][j+1])
+                    ans = max(ans, dp[i][j])
+        return dp[-1][-1]
