c8f8894da02f850cb599d969ecf19a7b52e9db13
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-09-09 23:54:09 +0530

    Time: 3 ms (80.49%), Space: 47.7 MB (64.42%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.java
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -4,14 +4,12 @@ class Solution {
         ListNode l3 = new ListNode();
         ListNode n = l3;
         while(l1 != null || l2 != null || prev != 0){
-            n.next = new ListNode();
-            n = n.next;
             int val1 = l1 == null ? 0 : l1.val;
             int val2 = l2 == null ? 0 : l2.val;
             temp = val1 + val2 + prev;
             prev = temp / 10;
-            temp = temp % 10;
-            n.val = temp;
+            n.next = new ListNode(temp % 10);
+            n = n.next;
             l1 = l1 != null ? l1.next : null;
             l2 = l2 != null ? l2.next : null;
         }
