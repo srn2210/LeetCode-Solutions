7cd7f2d57ada4e59436fc1e57dd20f2f729498af
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-09-22 16:50:47 +0530

    Time: 3 ms (14.73%), Space: 44 MB (46.20%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -1,15 +1,19 @@
 class Solution {
+    boolean flag = true;
     void dfs(TreeNode node, ArrayList<Integer> list) {
         if(node.left != null) dfs(node.left, list);
+        if(list.size() != 0) {
+            if(list.get(list.size()-1) >= node.val) flag = false;
+        }
         list.add(node.val);
         if(node.right != null) dfs(node.right, list);
     }
     public boolean isValidBST(TreeNode root) {
         ArrayList<Integer> list = new ArrayList();
         dfs(root, list);
-        for(int i=1; i<list.size(); i++) {
+        /*for(int i=1; i<list.size(); i++) {
             if(list.get(i) <= list.get(i-1)) return false;
-        }
-        return true;
+        }*/
+        return flag;
     }
 }
