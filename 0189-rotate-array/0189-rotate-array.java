ebef6726f49bfb1d53c79c65a9a91269c494755d
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-10-25 13:32:32 +0530

    Time: 2 ms (53.87%), Space: 64.3 MB (60.10%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,14 +1,16 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        Deque<Integer> dq = new ArrayDeque();
-        for(int i:nums) {
-            dq.offer(i);
+        k = k % nums.length;
+        int[] arr = new int[k];
+        int j = 0;
+        for(int i=nums.length-k; i<nums.length; i++) {
+            arr[j++] = nums[i];
         }
-        while(k-- > 0) {
-            dq.offerFirst(dq.pollLast());
+        for(int i=nums.length-k-1; i>=0; i--) {
+            nums[i+k] = nums[i];
         }
-        for(int i=0; i<nums.length; i++) {
-            nums[i] = dq.poll();
+        for(int i=0; i<k; i++) {
+            nums[i] = arr[i];
         }
     }
 }
