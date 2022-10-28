2ef91666ded70db993ae5e64902e543bca552c2e
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-10-28 23:57:14 +0530

    Time: 0 ms (100.00%), Space: 39.7 MB (91.81%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -6,10 +6,12 @@ class Solution {
         else if(nums.length == 2){
             return Math.max(nums[0], nums[1]);
         }
-        nums[1] = Math.max(nums[0], nums[1]);
-        for(int i = 2; i<nums.length; i++){
-            nums[i] = Math.max(nums[i] + nums[i-2], nums[i-1]);
+        else {
+            nums[1] = Math.max(nums[0], nums[1]);
+            for(int i = 2; i<nums.length; i++){
+                nums[i] = Math.max(nums[i] + nums[i-2], nums[i-1]);
+            }
+            return nums[nums.length-1];
         }
-        return nums[nums.length-1];
     }
 }
