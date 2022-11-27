2e49e76797acb83860fea42c3ace9e60b0f9e24c
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-11-27 15:24:44 +0530

    Time: 258 ms (75.00%), Space: 131.4 MB (54.88%) - LeetHub

--- 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.java
+++ 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.java
@@ -11,7 +11,7 @@ class Solution {
                 int t1 = dp[i].getOrDefault(diff, 0);               // for repeated numbers; eg : 7, 7, 7
                 int t2 = dp[j].getOrDefault(diff, 0);               // j is loops through all values < i because of subsequnces 
                 dp[i].put(diff, t1 + t2 + 1);                       // 1 is ignored in ans because that is a two element subsequence
-                ans += t2;                                          // t1 already added into the ans in the prev iterations
+                ans += t2;                                          // only t2 needs to be added as t1 is just for updating dp
             }
         }
         return ans;
