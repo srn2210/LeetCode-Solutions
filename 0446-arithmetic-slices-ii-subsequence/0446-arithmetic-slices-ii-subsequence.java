4d5cd36052eeb6520fdc3e092d339dd3bdc51b17
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-11-27 15:05:50 +0530

    Time: 357 ms (42.07%), Space: 131.4 MB (54.88%) - LeetHub

--- 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.java
+++ 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.java
@@ -3,15 +3,15 @@ class Solution {
         Map<Integer, Integer>[] dp = new Map[nums.length];
         int ans = 0;
         for(int i=0; i<nums.length; i++) {
-            dp[i] = new HashMap<Integer, Integer>();
+            dp[i] = new HashMap<Integer, Integer>();                // array of maps instead of 2D array for -ve number diff
             for(int j=0; j<i; j++) {
-                long d = (long)nums[i] - (long)nums[j];
+                long d = (long)nums[i] - (long)nums[j];             // case like x - (-y) = x + y where res goes out of int range
                 if(d <= Integer.MIN_VALUE || d >= Integer.MAX_VALUE) continue;
                 int diff = (int) d;
-                int t1 = dp[i].getOrDefault(diff, 0);
-                int t2 = dp[j].getOrDefault(diff, 0);
-                dp[i].put(diff, t1 + t2 + 1);
-                ans += t2;
+                int t1 = dp[i].getOrDefault(diff, 0);               // for repeated numbers; eg : 7, 7, 7
+                int t2 = dp[j].getOrDefault(diff, 0);               // j is loops through all values < i because of subsequnces 
+                dp[i].put(diff, t1 + t2 + 1);                       // 1 is ignored in ans because that is a two element subsequence
+                ans += t2;                                          // t1 already added into the ans in the prev iterations
             }
         }
         return ans;
