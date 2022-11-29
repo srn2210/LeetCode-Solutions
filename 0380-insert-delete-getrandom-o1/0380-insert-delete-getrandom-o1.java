0f70c7455389ac19f7ecfb22e19858a22d723ce3
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-11-29 14:28:36 +0530

    Time: 42 ms (82.05%), Space: 108.3 MB (55.17%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
@@ -36,11 +36,3 @@ class RandomizedSet {
         return list.get(rand.nextInt(list.size()));
     }
 }
-
-/**
- * Your RandomizedSet object will be instantiated and called as such:
- * RandomizedSet obj = new RandomizedSet();
- * boolean param_1 = obj.insert(val);
- * boolean param_2 = obj.remove(val);
- * int param_3 = obj.getRandom();
- */
