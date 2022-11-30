aa114f5a45ab0463970685e96c33b487d4b80001
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-11-30 12:56:43 +0530

    Time: 2 ms (5.34%), Space: 41.7 MB (97.46%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,12 +1,14 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int[] t = new int[matrix[0].length];
-        t[t.length-1] = target;
-        int ans = Arrays.binarySearch(matrix, t, Comparator.comparingInt(j -> j[matrix[0].length-1]));
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[] t = new int[n];
+        t[n-1] = target;
+        int ans = Arrays.binarySearch(matrix, t, Comparator.comparingInt(j -> j[n-1]));
         if(ans >= 0) return true;
         else {
             ans = -ans - 1;
-            if(ans >= matrix.length) return false;
+            if(ans >= m) return false;
             ans = Arrays.binarySearch(matrix[ans], target);
         }
         return ans >= 0 ? true : false;
