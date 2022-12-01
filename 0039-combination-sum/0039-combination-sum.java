e89f335e088a955c7a9322eb0ce054a82a70632a
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-12-01 20:53:10 +0530

    Time: 7 ms (29.19%), Space: 45.1 MB (33.31%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,5 +1,5 @@
 class Solution {
-    void backtrack(int curr, Set<List<Integer>> res, int j, List<Integer> temp, int[] arr, int target) {
+    void backtrack(int curr, List<List<Integer>> res, int j, List<Integer> temp, int[] arr, int target) {
         if(curr > target) return;
         if(curr == target) {
             res.add(new ArrayList<>(temp));
@@ -7,6 +7,7 @@ class Solution {
         }
         if(curr < target) {
             for(int i=j; i<arr.length; i++) {
+                if(arr[i] > target) return;
                 curr += arr[i];
                 temp.add(arr[i]);
                 backtrack(curr, res, i, temp, arr, target);
@@ -16,7 +17,8 @@ class Solution {
         }
     }
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        Set<List<Integer>> res = new HashSet<>();
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(candidates);
         int curr = 0;
         backtrack(curr, res, 0, new ArrayList<>(), candidates, target);
         return new ArrayList<>(res);
