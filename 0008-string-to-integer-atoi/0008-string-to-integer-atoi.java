b577cdba9d119b55a6c71377e22a619079f40c6d
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-12-01 22:43:09 +0530

    Time: 3 ms (79.53%), Space: 43.3 MB (30.83%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,7 +1,7 @@
 class Solution {
     public int myAtoi(String s) {
         long ans = 0;
-        int sign = 1;
+        boolean sign = true;
         int flag = 0;
         for(char ch : s.toCharArray()) {
             if(flag == 2) break;
@@ -10,34 +10,31 @@ class Solution {
                 continue;
             }
             else if(ch == '-') {
-                if(flag == 1) {
-                    flag = 2;
-                    continue;
-                }
+                if(flag == 1) break;
                 flag = 1;
-                sign = sign * -1;
+                sign = false;
             }
             else if(ch == '+') {
-                if(flag == 1) {
-                    flag = 2;
-                    continue;
-                }
+                if(flag == 1) break;
                 flag = 1;
                 continue;
             }
             else if(ch >= '0' && ch <= '9') {
                 flag = 1;
-                if(sign == 1) ans = (ans * 10) + (ch - '0');
-                if(sign == -1) ans = (ans * 10) - (ch - '0');
-                if(ans > Integer.MAX_VALUE) {
-                    return Integer.MAX_VALUE;
+                if(sign) {
+                    ans = (ans * 10) + (ch - '0');
+                    if(ans > Integer.MAX_VALUE) {
+                        return Integer.MAX_VALUE;
+                    }
                 }
-                else if(ans < Integer.MIN_VALUE) {
-                    return Integer.MIN_VALUE;
+                else {
+                    ans = (ans * 10) - (ch - '0');
+                    if(ans < Integer.MIN_VALUE) {
+                        return Integer.MIN_VALUE;
+                    }
                 }
             }
             else {
-                //flag = 2;
                 break;
             }
         }
