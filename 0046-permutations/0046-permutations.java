e61b463d33b78e064d00b055889712ec90e47045
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-12-02 19:25:42 +0530

    Time: 1 ms (97.72%), Space: 44.5 MB (59.66%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,20 +1,28 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> list = new ArrayList<>();
-        backtrack(list, new LinkedHashSet<>(), nums);
-        return list;
+        List<List<Integer>> ans = new ArrayList<>();
+        backtrack(0,nums,ans);
+        return ans;
     }
-    private void backtrack(List<List<Integer>> list, Set<Integer> tempList, int [] nums){
-        if(tempList.size() == nums.length){
-            list.add(new ArrayList<>(tempList));
+    private void backtrack(int index,int[] nums,List<List<Integer>> ans){
+        if(index==nums.length){
+		// new list to store the data and copy to the ans 
+            List<Integer> ds = new ArrayList<>();
+            
+            for(int i=0;i<nums.length;i++) ds.add(nums[i]);
+            
+            ans.add(ds);
+            return;
         }
-        else{
-            for(int i:nums){
-                if(tempList.contains(i)) continue;
-                tempList.add(i);
-                backtrack(list, tempList, nums);
-                tempList.remove(i);
-            }
+        for(int i=index;i<nums.length;i++){
+            swap(i,index,nums);
+            backtrack(index+1,nums,ans);
+            swap(i,index,nums);
         }
+    }
+    private void swap(int i, int j,int[] nums){
+        int t= nums[i];
+        nums[i]=nums[j];
+        nums[j]=t;
     }
 }
