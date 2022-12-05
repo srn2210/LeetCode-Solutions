36f0c57e4c1da41302ab0cb35390715ebaff7ffe
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-12-05 18:04:49 +0530

    Time: 16 ms (99.22%), Space: 39.2 MB (83.04%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -0,0 +1,14 @@
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int lo = 1;
+        int hi = n;
+        
+        while(lo < hi) {
+            int mid = lo + ((hi-lo) / 2);
+            if(isBadVersion(mid)) hi = mid;
+            else lo = mid + 1;
+        }
+        
+        return lo;
+    }
+}
