dcb5d56a3b14b13bb7af4cf683e5066f9a4258d6
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-12-06 16:15:10 +0530

    Time: 87 ms (33.72%), Space: 25.9 MB (54.88%) - LeetHub

---
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int binarySearch(vector<int>& nums, int target) {
+        int lo = 0;
+        int hi = nums.size() - 1;
+        while(lo < hi) {
+            int mid = lo + (hi-lo) / 2;
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] < target) lo = mid + 1;
+            else hi = mid;
+        }
+        return lo;
+    }
+    vector<int> sortedSquares(vector<int>& nums) {
+        vector<int> res(nums.size());
+        int right = binarySearch(nums, 0);
+        //right = right < 0 ? -right-1 : right;
+        int left = right - 1;
+        int i = 0;
+        while(left >= 0 || right < nums.size()) {
+            if(left < 0) {
+                res[i] = nums[right] * nums[right];
+                i++;
+                right++;
+            }
+            else if(right >= nums.size()) {
+                res[i] = nums[left] * nums[left];
+                i++;
+                left--;
+            }
+            else if(nums[right] < -nums[left]) {
+                res[i] = nums[right] * nums[right];
+                i++;
+                right++;
+            }
+            else {
+                res[i] = nums[left] * nums[left];
+                i++;
+                left--;
+            }
+        }
+        return res;
+    }
+};
