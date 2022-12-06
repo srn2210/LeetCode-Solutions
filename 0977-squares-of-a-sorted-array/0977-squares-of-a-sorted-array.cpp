36928500e99f219b3d077246c3bd466bd25c643a
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-12-06 16:19:46 +0530

    Time: 86 ms (35.62%), Space: 25.8 MB (94.74%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -3,11 +3,11 @@ public:
     int binarySearch(vector<int>& nums, int target) {
         int lo = 0;
         int hi = nums.size() - 1;
-        while(lo < hi) {
+        while(lo <= hi) {
             int mid = lo + (hi-lo) / 2;
             if(nums[mid] == target) return mid;
             else if(nums[mid] < target) lo = mid + 1;
-            else hi = mid;
+            else hi = mid - 1;
         }
         return lo;
     }
