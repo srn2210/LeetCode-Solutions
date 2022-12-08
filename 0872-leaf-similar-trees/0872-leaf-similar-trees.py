37592793cf13ca0e9e8d19ca272be404196f09a6
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-12-08 12:12:51 +0530

    Time: 36 ms (89.25%), Space: 13.8 MB (87.58%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.py
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.py
@@ -1,25 +1,16 @@
 class Solution:
-    s1 = []
-    s2 = []
-    def dfs(self, node: Optional[TreeNode], flag: bool) -> None:
+    def dfs(self, node: Optional[TreeNode], arr: List[int]) -> None:
         if not node: return
         if not node.left and not node.right:
-            if flag:
-                self.s1.append(node.val)
-                return
-            else:
-                self.s2.append(node.val)
-                return
-        self.dfs(node.left, flag)
-        self.dfs(node.right, flag)
+            arr.append(node.val)
+        self.dfs(node.left, arr)
+        self.dfs(node.right, arr)
         return
     def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
         s1 = []
         s2 = []
-        self.s1 = s1
-        self.s2 = s2
-        self.dfs(root1, True)
-        self.dfs(root2, False)
+        self.dfs(root1, s1)
+        self.dfs(root2, s2)
         if len(s1) != len(s2): return False
         for i in range(len(s1)):
             if s1[i] != s2[i]: return False
