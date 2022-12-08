48854a40f4c2ce88a2120536011f5727c1fb1233
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-12-08 19:32:19 +0530

    Time: 31 ms (93.19%), Space: 13.9 MB (98.77%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -1,31 +1,27 @@
 class MyQueue:
-    #st1
-    #st2
-    #front
     def __init__(self):
         self.st1 = deque()
         self.st2 = deque()
         self.front = -1
 
     def push(self, x: int) -> None:
+        if not self.st1: self.front = x
         self.st1.append(x)
-        if self.front == -1: self.front = x
 
     def pop(self) -> int:
-        while self.st1:
-            self.st2.append(self.st1.pop())
-        ans = self.st2.pop()
-        if self.st2: self.front = self.st2[-1]
-        else: self.front = -1
-        while self.st2:
-            self.st1.append(self.st2.pop())
+        if self.st2: ans = self.st2.pop()
+        else:
+            while self.st1:
+                self.st2.append(self.st1.pop())
+            ans = self.st2.pop()
         return ans
 
     def peek(self) -> int:
-        return self.front
+        if self.st2: return self.st2[-1]
+        else: return self.front
 
     def empty(self) -> bool:
-        return len(self.st1) == 0
+        return len(self.st1) == 0 and len(self.st2) == 0
 
 
 # Your MyQueue object will be instantiated and called as such:
