ecd9dc7e0e8586eeaec7d253d0e8b3e48502411d
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-12-12 21:26:52 +0530

    Time: 20 ms (42.64%), Space: 14.2 MB (36.33%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int directions[4][2] = {{1,0}, {0,1}, {-1,0}, {0,-1}};
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        bool vis[image.size()][image[0].size()];
+        memset(vis, false, sizeof(vis));
+        queue<array<int, 2>> queue;
+        queue.push({sr, sc});
+        int init = image[sr][sc];
+        image[sr][sc] = color;
+        vis[sr][sc] = true;
+        while(!queue.empty()) {
+            int size = queue.size();
+            while(size--) {
+                auto arr = queue.front();
+                queue.pop();
+                for(auto dir : directions) {
+                    int x = dir[0] + arr[0];
+                    int y = dir[1] + arr[1];
+                    if(x < 0 || x >= image.size() || y < 0 || y >= image[0].size() || image[x][y] != init || vis[x][y]) continue;
+                    else {
+                        vis[x][y] = true;
+                        image[x][y] = color;
+                        queue.push({x,y});
+                    }
+                }
+            }
+        }
+        return image;
+    }
+};
