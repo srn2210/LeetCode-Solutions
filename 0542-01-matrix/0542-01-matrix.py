464a4a76599d26e0134818487d93a3ddf5e566ec
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-12-13 21:43:32 +0530

    Time: 1294 ms (64.96%), Space: 16.6 MB (92.67%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -9,15 +9,13 @@ class Solution:
         for i in range(m):
             for j in range(n):
                 if mat[i][j] == 0: continue
-                t = mat[i][j]
-                if j-1 >= 0: mat[i][j] = min(t, mat[i][j-1] + 1)
+                if j-1 >= 0: mat[i][j] = min(mat[i][j], mat[i][j-1] + 1)
                 if i-1 >= 0: mat[i][j] = min(mat[i][j], mat[i-1][j] + 1)
         
         for i in reversed(range(m)):
             for j in reversed(range(n)):
                 if mat[i][j] == 0: continue
-                t = mat[i][j]
-                if j+1 < n: mat[i][j] = min(t, mat[i][j+1] + 1)
+                if j+1 < n: mat[i][j] = min(mat[i][j], mat[i][j+1] + 1)
                 if i+1 < m: mat[i][j] = min(mat[i][j], mat[i+1][j] + 1)
         
         return mat
