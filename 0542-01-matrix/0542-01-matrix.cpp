13635a166e1e712d1296270cf18233bb133555bc
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-12-13 21:29:15 +0530

    Time: 95 ms (86.52%), Space: 30.6 MB (50.29%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -3,10 +3,13 @@ public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         vector<vector<int>> res(mat.size(), vector<int>(mat[0].size(), INT_MAX));
         queue<pair<int, int>> queue;
+        bool vis[mat.size()][mat[0].size()];
+        memset(vis, false, sizeof(vis));
         for(int i=0; i<mat.size(); i++) {
             for(int j=0; j<mat[0].size(); j++) {
                 if(mat[i][j] == 0) {
                     res[i][j] = 0;
+                    vis[i][j] = true;
                     queue.push(make_pair(i, j));
                 }
             }
@@ -18,10 +21,11 @@ public:
             for(auto dir : direction) {
                 int x = dir[0] + arr.first;
                 int y = dir[1] + arr.second;
-                if(x < 0 || y < 0 || x >= mat.size() || y >= mat[0].size() || res[x][y] < res[arr.first][arr.second] + 1) continue;
+                if(x < 0 || y < 0 || x >= mat.size() || y >= mat[0].size() || res[x][y] < res[arr.first][arr.second] + 1 || vis[x][y]) continue;
                 else {
                     res[x][y] = 1 + res[arr.first][arr.second];
                     queue.push(make_pair(x, y));
+                    if(res[arr.first][arr.second] == 0) vis[x][y] = true;
                 }
             }
         }
