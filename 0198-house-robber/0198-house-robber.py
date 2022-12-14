f16afc48efa51d90691a600872b9dd14116e3fa8
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-12-14 19:18:21 +0530

    Time: 39 ms (81.35%), Space: 13.9 MB (20.34%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,14 @@
+class Solution:
+    def helper(self, nums: List[int], dp: List[int], index: int) -> int:
+        if index == 0:
+            dp[index] = nums[index]
+            return dp[index]
+        if index == 1:
+            dp[index] = max(nums[0], nums[1])
+            return dp[index]
+        if dp[index] != -1: return dp[index]
+        dp[index] = max(self.helper(nums, dp, index-1), nums[index] + self.helper(nums, dp, index-2))
+        return dp[index]        
+    def rob(self, nums: List[int]) -> int:
+        dp = [-1 for i in range(len(nums)+1)]
+        return self.helper(nums, dp, len(nums)-1)
