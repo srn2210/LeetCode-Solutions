770d1d948c86632aea6b18154ace6069befa0995
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-12-14 19:31:03 +0530

    Time: 5 ms (22.94%), Space: 7.5 MB (95.23%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -3,13 +3,13 @@ public:
     int rob(vector<int>& nums) {
         if(nums.size() == 1) return nums[0];
         if(nums.size() == 2) return max(nums[0], nums[1]);
-        int dp[nums.size()];
-        memset(dp, -1, sizeof(dp));
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
+        int t1 = nums[0];
+        int t2 = max(nums[0], nums[1]);
         for(int i=2; i<nums.size(); i++) {
-            dp[i] = max(dp[i-1], nums[i] + dp[i-2]);
+            int temp = t2;
+            t2 = max(t2, nums[i] + t1);
+            t1 = temp;
         }
-        return dp[nums.size()-1];
+        return t2;
     }
 };
