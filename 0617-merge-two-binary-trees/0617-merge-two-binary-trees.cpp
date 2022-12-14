69c70f50e5fce29e102495e725f1ac87dcbc66fa
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-12-14 22:01:52 +0530

    Time: 48 ms (81.40%), Space: 32.4 MB (46.63%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.cpp
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.cpp
@@ -15,9 +15,11 @@ public:
         if(!root1 && !root2) return nullptr;
         else if(!root1 && root2 != nullptr) return root2;
         else if(root1 != nullptr && !root2) return root1;
-        root1->val = root1->val + root2->val;
-        root1->left = mergeTrees(root1->left, root2->left);
-        root1->right = mergeTrees(root1->right, root2->right);
-        return root1;
+        else {
+            root1->val = root1->val + root2->val;
+            root1->left = mergeTrees(root1->left, root2->left);
+            root1->right = mergeTrees(root1->right, root2->right);
+            return root1;
+        }
     }
 };
