89429cdc5dc98dadf442cc122ad40e7daf206c35
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-12-14 19:31:39 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (50.26%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -5,8 +5,9 @@ public:
         if(nums.size() == 2) return max(nums[0], nums[1]);
         int t1 = nums[0];
         int t2 = max(nums[0], nums[1]);
+        int temp = -1;
         for(int i=2; i<nums.size(); i++) {
-            int temp = t2;
+            temp = t2;
             t2 = max(t2, nums[i] + t1);
             t1 = temp;
         }
