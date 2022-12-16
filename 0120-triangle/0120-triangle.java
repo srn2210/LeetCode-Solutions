867733da6a17a1fbfa040181d36556212d1721af
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-12-16 19:25:28 +0530

    Time: 5 ms (68.12%), Space: 41.7 MB (95.99%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,18 +1,21 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        for(int i=1; i<triangle.size(); i++) {
-            for(int j=0; j<triangle.get(i).size(); j++) {
-                int val = triangle.get(i).get(j);
+        int m = triangle.size();
+        for(int i=1; i<m; i++) {
+            List<Integer> list = triangle.get(i);
+            List<Integer> plist = triangle.get(i-1);
+            for(int j=0; j<list.size(); j++) {
+                int val = list.get(j);
                 int a = Integer.MAX_VALUE;
-                if(j < triangle.get(i-1).size()) a = triangle.get(i-1).get(j);
+                if(j < plist.size()) a = plist.get(j);
                 int b = Integer.MAX_VALUE;
-                if(j > 0) b = triangle.get(i-1).get(j-1);
+                if(j > 0) b = plist.get(j-1);
                 triangle.get(i).set(j, Math.min(a,b) + val);
             }
         }
         int ans = Integer.MAX_VALUE;
-        for(int i=0; i<triangle.get(triangle.size()-1).size(); i++)
-            ans = Math.min(ans, triangle.get(triangle.size()-1).get(i));
+        for(int i : triangle.get(m-1))
+            ans = Math.min(ans, i);
         return ans;
     }
 }
