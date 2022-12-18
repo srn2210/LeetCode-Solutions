9e5d3129fcec31ec899be1bcda8e0d86a55b6bae
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-12-18 14:05:24 +0530

    Time: 1486 ms (79.80%), Space: 27.8 MB (95.42%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -3,13 +3,11 @@ class Solution:
         st = deque()
         res = [0 for i in range(len(temperatures))]
         for i in range(len(temperatures)):
-            if st and temperatures[st[-1]] >= temperatures[i]:
+            if not st or st and temperatures[st[-1]] >= temperatures[i]:
                 st.append(i)
-            elif st:
+            else:
                 while st and temperatures[st[-1]] < temperatures[i]:
                     res[st[-1]] = i - st[-1]
                     st.pop()
-                st.append(i)
-            else:
                 st.append(i)
         return res
