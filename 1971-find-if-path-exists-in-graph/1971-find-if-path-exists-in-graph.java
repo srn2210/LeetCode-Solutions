d75385d6f5f86910102b17849726cc06b08e38ac
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-12-19 06:22:21 +0530

    Time: 31 ms (90.49%), Space: 138.2 MB (81.92%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,21 +1,40 @@
 class Solution {
-    boolean dfs(List<Integer>[] adj, int src, int dest, Set<Integer> map) {
-        if(src == dest) return true;
-        map.add(src);
-        for(int i : adj[src]) {
-            if(i == dest) return true;
-            if(!map.contains(i) && dfs(adj, i, dest, map)) return true;
+    class UF {
+        int[] arr;
+        int[] sz;
+        UF(int n) {
+            arr = new int[n];
+            sz = new int[n];
+            for(int i=0; i<n; i++) {
+                arr[i] = i;
+                sz[i] = 1;
+            }
         }
-        return false;
+        int find(int i) {
+            if(i == arr[i]) return i;
+            return arr[i] = find(arr[i]);
+        }
+        boolean isConnected(int i, int j) {
+            return find(i) == find(j);
+        }
+        void union(int i, int j) {
+            int root1 = find(i);
+            int root2 = find(j);
+            if(sz[root1] < sz[root2]) {
+                arr[root1] = arr[root2];
+                sz[root2] += sz[root1];
+            }
+            else {
+                arr[root2] = arr[root1];
+                sz[root1] += sz[root2];
+            }
+        }
     }
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        List<Integer>[] adj = new ArrayList[n];
-        for(int i=0; i<n; i++) adj[i] = new ArrayList<>();
+        UF obj = new UF(n);
         for(int[] i : edges) {
-            adj[i[0]].add(i[1]);
-            adj[i[1]].add(i[0]);
+            obj.union(i[0], i[1]);
         }
-        Set<Integer> set = new HashSet<>();
-        return dfs(adj, source, destination, set);        
+        return obj.isConnected(source, destination);        
     }
 }
