8af269061e647db6313e025efc316c48840f1092
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-12-21 22:08:17 +0530

    Time: 8 ms (61.57%), Space: 44.2 MB (32.17%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.java
+++ 0071-simplify-path/0071-simplify-path.java
@@ -1,19 +1,18 @@
 class Solution {
     public String simplifyPath(String path) {
-        //Stack<String> st = new Stack
         StringBuilder res = new StringBuilder();
         String[] parts = path.split("/");
         res.append('/');
         for(String s : parts) {
             if(s.length() == 0) continue;
-            if(s.equals("..")) {
+            else if(s.equals("..")) {
                 if(res.length() > 1) res.deleteCharAt(res.length()-1);
                 while(res.charAt(res.length()-1) != '/') {
                     res.deleteCharAt(res.length()-1);
                 }
                 continue;
             }
-            if(s.equals(".")) continue;
+            else if(s.equals(".")) continue;
             res.append(s);
             res.append('/');
         }
