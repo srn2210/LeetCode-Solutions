8615fb45b2ae0ce9890d911dbe864514bfe27431
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-12-22 22:17:38 +0530

    Time: 176 ms (21.15%), Space: 74.2 MB (71.15%) - LeetHub

--- 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.java
+++ 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.java
@@ -1,30 +1,31 @@
 class Solution {
-    int tsize(int src, int[] arr, Map<Integer, Set<Integer>> list, Set<Integer> set, int[] sizes) {
-        if(set.contains(src)) return -1;
+    int tsize(int src, int[] arr, Map<Integer, Set<Integer>> list, int[] sizes, int root) {
+        if(sizes[src] != 0) return -1;
         if(!list.containsKey(src) || list.get(src).size() == 0) return 0;
-        set.add(src);
         int sz = 0;
         for(int i : list.get(src)) {
-            int t = 1 + tsize(i, arr, list, set, sizes);
-            sz += t;
+            if(i != root) {
+                int t = 1 + tsize(i, arr, list, sizes, src);
+                sz += t;
+            }
         }
         sizes[src] = sz;
         return sz;
     }
-    void dfs(int src, int[] arr, Map<Integer, Set<Integer>> list, int n, int root, Set<Integer> set, int[] sizes) {
-        if(set.contains(src)) return;
-        set.add(src);
+    void dfs(int src, int[] arr, Map<Integer, Set<Integer>> list, int n, int root, int[] sizes) {
+        if(arr[src] != 0) return;
         int in = sizes[src] + 1;
         int out = n - in;
         arr[src] = arr[root] - in + out;
         for(int i : list.get(src)) {
-            dfs(i, arr, list, n, src, set, sizes);
+            if(i != root) {
+                dfs(i, arr, list, n, src, sizes);
+            }
         }
         return;
     }
     public int[] sumOfDistancesInTree(int n, int[][] edges) {
         Map<Integer, Set<Integer>> list = new HashMap<>();
-        Set<Integer> set = new HashSet<>();
         int[] arr = new int[n];
         int[] sizes = new int[n];
         for(int[] edge : edges) {
@@ -33,11 +34,11 @@ class Solution {
             list.get(edge[0]).add(edge[1]);
             list.get(edge[1]).add(edge[0]);
         }
-        tsize(0, arr, list, set, sizes);
-        set.clear();
+        tsize(0, arr, list, sizes, -1);
         Queue<Integer> queue = new LinkedList<>();
         if(list.containsKey(0)) for(int l : list.get(0)) queue.offer(l);
         int cnt = 1;
+        Set<Integer> set = new HashSet<>();
         set.add(0);
         while(!queue.isEmpty()) {
             int len = queue.size();
@@ -51,11 +52,9 @@ class Solution {
             }
             cnt++;
         }
-        set.clear();
-        set.add(0);
         if(list.containsKey(0)) {
             for(int l : list.get(0)) {
-                dfs(l, arr, list, n, 0, set, sizes);
+                dfs(l, arr, list, n, 0, sizes);
             }
         }
         return arr;
