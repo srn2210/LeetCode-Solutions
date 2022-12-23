8e50713b8752482439f969ebcab5f8f7d64bd98e
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-12-23 23:30:22 +0530

    Time: 52 ms (14.09%), Space: 14.6 MB (21.49%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -3,13 +3,12 @@ public:
     bool wordBreak(string s, vector<string>& wordDict) {
         bool dp[s.size()];
         memset(dp, false, sizeof(dp));
-        //dp[0] = true;
         unordered_set<string> set;
         for(string ss : wordDict) set.insert(ss);
         for(int i=0; i<s.size(); i++) {
             for(int j=i; j<s.size(); j++) {
                 string t = s.substr(i, j-i+1);
-                if(set.count(t)) {
+                if(set.find(t) != set.end()) {
                     if(i-1 >= 0 && dp[i-1]) dp[j] = true;
                     else if(i == 0) dp[j] = true;
                 }
