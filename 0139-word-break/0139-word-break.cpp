700ec6a98c674566064184db62f32cc587b36209
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-12-23 23:29:37 +0530

    Time: 49 ms (16.59%), Space: 14.5 MB (23.06%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        bool dp[s.size()];
+        memset(dp, false, sizeof(dp));
+        //dp[0] = true;
+        unordered_set<string> set;
+        for(string ss : wordDict) set.insert(ss);
+        for(int i=0; i<s.size(); i++) {
+            for(int j=i; j<s.size(); j++) {
+                string t = s.substr(i, j-i+1);
+                if(set.count(t)) {
+                    if(i-1 >= 0 && dp[i-1]) dp[j] = true;
+                    else if(i == 0) dp[j] = true;
+                }
+            }
+        }
+        return dp[s.size()-1];
+    }
+};
