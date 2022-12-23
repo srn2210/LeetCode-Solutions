ebfac435db9ff9f43c13ce99f6291dccd45bd372
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-12-23 23:50:00 +0530

    Time: 97 ms (15.05%), Space: 14.1 MB (45.12%) - LeetHub

---
+++ 0139-word-break/0139-word-break.py
@@ -0,0 +1,13 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        book = set()
+        for word in wordDict: book.add(word)
+        dp = [False for i in range(len(s))]
+        for i in range(len(s)):
+            for j in range(i, len(s)):
+                if dp[j]: continue
+                t = s[i:j+1]
+                if t in book:
+                    if j-i >= 0 and dp[i-1]: dp[j] = True
+                    elif i == 0: dp[j] = True
+        return dp[len(s)-1]
