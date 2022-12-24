4011c48b69f0e022d0acc62780d14cc4626b59e0
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-12-25 01:10:42 +0530

    Time: 304 ms (8.07%), Space: 22.3 MB (5.01%) - LeetHub

---
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.py
@@ -0,0 +1,22 @@
+class Solution:
+    def solve(self, n: int, x: int, y: int, dp: List[List[int]]) -> int:
+        if x > n or y > n: return 0
+        if x == n and y == n: return 1
+        if dp[x][y] != -1: return dp[x][y]
+        ans = 0
+        if x == y:
+            ans += self.solve(n, x+1, y+1, dp)
+            ans += self.solve(n, x+2, y+2, dp)
+            ans += self.solve(n, x+2, y+1, dp)
+            ans += self.solve(n, x+1, y+2, dp)
+        elif x-1 == y:
+            ans += self.solve(n, x+1, y+2, dp)
+            ans += self.solve(n, x, y+2, dp)
+        else:
+            ans += self.solve(n, x+2, y+1, dp)
+            ans += self.solve(n, x+2, y, dp)
+        dp[x][y] = ans % 1000000007
+        return dp[x][y]
+    def numTilings(self, n: int) -> int:
+        dp = [[-1 for i in range(n+1)] for j in range(n+1)]
+        return self.solve(n, 0, 0, dp)
