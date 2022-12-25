d13be4a9ba31baa12cc5d72445844e416e486c58
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-12-25 23:52:58 +0530

    Time: 7 ms (79.58%), Space: 42.8 MB (88.86%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
@@ -1,14 +1,6 @@
 class Solution {
-    /*int solve(int[] dp, int[] nums, int sum, int idx, int target) {
-        if(idx >= nums.length) return 0;        
-        if(sum > target) return -1;
-        if(dp[idx] != -1) return dp[idx];
-        return dp[idx] = Math.max(solve(dp, nums, sum, idx+1, target), 1 + solve(dp, nums, sum+nums[idx], idx+1, target));
-    }*/
     public int[] answerQueries(int[] nums, int[] queries) {
         int[] list = new int[queries.length];
-        //int[] dp = new int[nums.length];
-        //Arrays.fill(dp, -1);
         Arrays.sort(nums);
         for(int i=0; i<queries.length; i++) {
             int sum = 0;
