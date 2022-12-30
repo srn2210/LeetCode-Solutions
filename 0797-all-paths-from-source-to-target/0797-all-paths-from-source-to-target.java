9676d654bae7cc5f158c689c905b9357f63dc090
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-12-30 12:18:28 +0530

    Time: 2 ms (87.35%), Space: 44 MB (88.61%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
@@ -5,17 +5,16 @@ class Solution {
             return;
         }
         else {
-            for(int i=0; i<graph[src].length; i++) {
-                list.add(graph[src][i]);
-                dfs(graph[src][i], graph, list, path, dest);
+            for(int node : graph[src]) {
+                list.add(node);
+                dfs(node, graph, list, path, dest);
                 list.remove(list.size()-1);
             }
         }
     }
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
         List<List<Integer>> res = new ArrayList<>();
-        List<Integer> list = new ArrayList<>();
-        list.add(0);
+        List<Integer> list = new ArrayList<>(List.of(0));
         dfs(0, graph, list, res, graph.length-1);
         return res;
     }
