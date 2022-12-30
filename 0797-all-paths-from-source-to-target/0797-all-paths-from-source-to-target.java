8dcfb21eb8f2d0cfab00d1bf68bade293fc28529
srn2210 <110111501+srn2210@users.noreply.github.com>
2022-12-30 12:11:11 +0530

    Time: 2 ms (87.35%), Space: 44.2 MB (84.21%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
@@ -1,23 +1,21 @@
 class Solution {
-    void dfs(int src, int[][] graph, Set<Integer> set, List<List<Integer>> path, int dest) {
+    void dfs(int src, int[][] graph, List<Integer> list, List<List<Integer>> path, int dest) {
         if(src == dest) {
-            path.add(new ArrayList<>(set));
+            path.add(new ArrayList<>(list));
             return;
         }
         else {
             for(int i=0; i<graph[src].length; i++) {
-                if(!set.contains(graph[src][i])) {
-                    set.add(graph[src][i]);
-                    dfs(graph[src][i], graph, set, path, dest);
-                    set.remove(graph[src][i]);
-                }
+                list.add(graph[src][i]);
+                dfs(graph[src][i], graph, list, path, dest);
+                list.remove(list.size()-1);
             }
         }
     }
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
         List<List<Integer>> res = new ArrayList<>();
         for(int i=0; i<graph[0].length; i++) {
-            dfs(graph[0][i], graph, new LinkedHashSet<>(List.of(0, graph[0][i])), res, graph.length-1);
+            dfs(graph[0][i], graph, new ArrayList<>(List.of(0, graph[0][i])), res, graph.length-1);
         }
         return res;
     }
