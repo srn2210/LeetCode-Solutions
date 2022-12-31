263ea725842abb2c03b3f07788b85a8e136647e8
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-01-01 01:22:05 +0530

    Time: 655 ms (9.41%), Space: 53.1 MB (22.35%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -12,8 +12,14 @@ class Solution {
         boolean two = solve(s, dp, i, j-1) && s.charAt(i) == s.charAt(j);
         boolean three = solve(s, dp, i+1, j) && s.charAt(i) == s.charAt(j);
         if(one && j-i+1 > ans.length()) ans = s.substring(i, j+1);
-        if(two && j-i > ans.length()) ans = s.substring(i, j);
-        if(three && j-i > ans.length()) ans = s.substring(i+1, j);
+        if(two && j-i > ans.length()) {
+            ans = s.substring(i, j);
+            dp[i][j-1] = true;
+        }
+        if(three && j-i > ans.length()) {
+            ans = s.substring(i+1, j);
+            dp[i+1][j] = true;
+        }
         dp[i][j] = one;
         return dp[i][j];
     }
