6a4d4d213605b6d9c544032064a04b56faf63c7f
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-01-01 01:27:15 +0530

    Time: 789 ms (7.79%), Space: 52.8 MB (22.65%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -8,16 +8,12 @@ class Solution {
             return dp[i][j] = true;
         }
         if(dp[i][j] != null) return dp[i][j];
-        boolean one = dp[i][j] = solve(s, dp, i+1, j-1) && s.charAt(i) == s.charAt(j);
-        boolean two = dp[i][j-1] = solve(s, dp, i, j-1);
-        boolean three = dp[i+1][j] = solve(s, dp, i+1, j);
-        if(one && j-i+1 > ans.length()) ans = s.substring(i, j+1);
-        if(two && j-i > ans.length()) {
-            ans = s.substring(i, j);
-        }
-        if(three && j-i > ans.length()) {
-            ans = s.substring(i+1, j);
-        }
+        dp[i][j] = solve(s, dp, i+1, j-1) && s.charAt(i) == s.charAt(j);
+        dp[i][j-1] = solve(s, dp, i, j-1);
+        dp[i+1][j] = solve(s, dp, i+1, j);
+        if(dp[i][j] && j-i+1 > ans.length()) ans = s.substring(i, j+1);
+        if(dp[i][j-1] && j-i > ans.length()) ans = s.substring(i, j);
+        if(dp[i+1][j] && j-i > ans.length()) ans = s.substring(i+1, j);
         return dp[i][j];
     }
     public String longestPalindrome(String s) {
