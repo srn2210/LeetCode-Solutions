6bc5b61860b84d057138f017b1bb4345a7143736
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-01-03 18:41:01 +0530

    Time: 6576 ms (6.52%), Space: 53.9 MB (11.41%) - LeetHub

---
+++ 2080-range-frequency-queries/2080-range-frequency-queries.py
@@ -0,0 +1,41 @@
+class RangeFreqQuery:
+    arr1 = []
+    m = dict()
+    def __init__(self, arr: List[int]):
+        self.arr1 = arr
+        self.m = dict()
+        for i in range(len(arr)):
+            if arr[i] not in self.m:
+                self.m[arr[i]] = []
+            self.m[arr[i]].append(i)
+        #print(self.m)
+
+    def query(self, left: int, right: int, value: int) -> int:
+        if value not in self.m: return 0
+        one = self.bSearch1(0, len(self.m[value])-1, self.m[value], left)
+        two = self.bSearch1(0, len(self.m[value])-1, self.m[value], right)
+        #print(one, "  ", two)
+        if one < 0 and two < 0 and one == two: return 0
+        one = -one-1 if one < 0 else one
+        two = -two-2 if two < 0 else two
+        return two - one + 1
+
+    def bSearch1(self, left: int, right: int, arr: List[int], val: int) -> int:
+        while left <= right:
+            mid = left + (right - left) // 2
+            if arr[mid] == val: return mid
+            elif val < arr[mid]: right = mid - 1
+            else: left = mid + 1
+        return -left-1
+    
+    def bSearch2(self, left: int, right: int, arr: List[int], val: int) -> int:
+        while left < right:
+            mid = left + (right - left) // 2
+            if arr[mid] == val: return mid
+            elif val < arr[mid]: right = mid
+            else: left = mid + 1
+        return left-1
+    
+# Your RangeFreqQuery object will be instantiated and called as such:
+# obj = RangeFreqQuery(arr)
+# param_1 = obj.query(left,right,value)
