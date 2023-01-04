707b5539adbec3ab403732c90a42862f1184ca19
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-01-04 22:54:13 +0530

    Time: 3 ms (45.24%), Space: 42.6 MB (12.90%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -3,7 +3,7 @@ class Solution {
         Arrays.sort(citations);
         int ans = citations[0] == 0 ? 0 : 1;
         int n = citations.length;
-        for(int i=0; i<citations.length; i++) {
+        for(int i=0; i<n; i++) {
             if(citations[i] >= n-i) ans = Math.max(ans, n-i);
         }
         return ans;
