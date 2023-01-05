62a3c4796473f6fbce3919fc4d63111905748c36
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-01-05 18:50:33 +0530

    Time: 131 ms (25.96%), Space: 96.6 MB (33.57%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
@@ -6,12 +6,10 @@ class Solution {
         int y = points[0][1];
         for(int[] point : points) {
             if(y >= point[0]) {
-                //System.out.println(y + "  " + point[0]);
                 x = Math.min(x, point[0]);
                 y = Math.min(y, point[1]);
             }
             else {
-                //System.out.println(y + "  " + point[0]);
                 ans++;
                 x = point[0];
                 y = point[1];
