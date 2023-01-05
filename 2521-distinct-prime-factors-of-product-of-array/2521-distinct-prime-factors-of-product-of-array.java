3bdf7cfb1639e036d7e91b11a0a1c3d9310592ba
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-01-05 10:43:38 +0530

    Time: 10 ms (98.28%), Space: 43.6 MB (19.67%) - LeetHub

--- 2521-distinct-prime-factors-of-product-of-array/2521-distinct-prime-factors-of-product-of-array.java
+++ 2521-distinct-prime-factors-of-product-of-array/2521-distinct-prime-factors-of-product-of-array.java
@@ -1,55 +1,4 @@
 class Solution {
-    /*static List<Integer> list = new ArrayList<>();
-    boolean isPrime(int num) {
-        if(num == 1) return false;
-        int lim = (int)Math.sqrt(num);
-        for(int i=2; i<=lim; i++) {
-            if(num % i == 0) return false;
-        }
-        return true;
-    }
-    void getPrime(int total) {
-        for(int i=2; i<=total; i++) {
-            if(isPrime(i)) list.add(i);
-        }
-    }*/
-    public int distinctPrimeFactors(int[] nums) {
-        /*int total = 1;
-        for(int i : nums) total *= i;
-        if(list.size() == 0) getPrime(1000);
-        Set<Integer> set = new HashSet<>();
-        Set<Integer> set2 = new HashSet<>(list);
-        System.out.println(list);
-        for(int i=0; i<nums.length; i++) {
-            if(set2.contains(nums[i])) {
-                set.add(nums[i]);
-                continue;
-            }
-            int k = nums[i];
-            while(k != 1) {
-                for(int l:list) {
-                    if(l > k) break;
-                    if(k % l == 0) {
-                        set.add(l);
-                        k /= l;
-                        break;
-                    }
-                }
-            }
-        }
-        return set.size();*/
-        Set<Integer> set = new HashSet<>();
-        int[] spf = new int[1001];
-        sieve(spf);
-        for(int i:nums) {
-            int k = i;
-            while(k != 1) {
-                set.add(spf[k]);
-                k = k / spf[k];
-            }
-        }
-        return set.size();
-    }
     void sieve(int[] spf) {
         for(int i=2; i<spf.length; i++) {
             spf[i] = i;
@@ -63,5 +12,18 @@ class Solution {
                 }
             }
         }
+    }
+    public int distinctPrimeFactors(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        int[] spf = new int[1001];
+        sieve(spf);
+        for(int i:nums) {
+            int k = i;
+            while(k != 1) {
+                set.add(spf[k]);
+                k = k / spf[k];
+            }
+        }
+        return set.size();
     }
 }
