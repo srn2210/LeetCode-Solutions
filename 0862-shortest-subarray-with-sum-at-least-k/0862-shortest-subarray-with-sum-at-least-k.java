f3285a9298074331b47765b4d7c2ffac553e51d7
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-01-06 00:34:41 +0530

    Time: 165 ms (7.91%), Space: 122.7 MB (27.27%) - LeetHub

--- 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.java
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.java
@@ -1,6 +1,5 @@
 class Solution {
     int binarySearch(int[] arr, List<int[]> list, int hi, int k) {
-        //int hi = list.size()-1;
         int lo = 0;
         int ans = Integer.MAX_VALUE;
         while(lo <= hi) {
@@ -13,39 +12,17 @@ class Solution {
         }
         return ans;
     }
-    /*int binarySearch(int i, int arr, List<int[]> list, int k) {
-        int lo;
-        if(i == -1) lo = 0;
-        else lo = i;
-        int lo = i;
-        int hi = list.size()-1;
-        int ans = Integer.MAX_VALUE;
-        while(lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
-            if(list.get(mid)[0] - arr >= k) {
-                ans = Math.min(ans, list.get(mid)[1] - i - 1);
-                hi = mid - 1;
-            }
-            else lo = mid + 1;
-        }
-        return ans;
-    }*/
     public int shortestSubarray(int[] nums, int k) {
         List<int[]> list = new ArrayList<>();
         list.add(new int[]{0, 0});
         int sum = 0;
         int res = Integer.MAX_VALUE;
         for(int i=0; i<nums.length; i++) {
-            /*if(sum + nums[i] >= sum) sum += nums[i];
-            else sum = 0;*/
             sum += nums[i];
             if(sum < 0) sum = 0;
             if(sum >= list.get(list.size()-1)[0]) {
                 list.add(new int[]{sum, i+1});
             }
-            /*else {
-                list.add(new int[]{list.get(list.size()-1)[0], i+1});
-            }*/
             else {
                 while(list.get(list.size()-1)[0] > sum) {
                     list.remove(list.size()-1);
@@ -54,19 +31,6 @@ class Solution {
             }
             res = Math.min(res, binarySearch(list.get(list.size()-1), list, list.size()-2, k));
         }
-        /*for(int[] i : list) {
-            System.out.println(i[0] + "  " + i[1]);
-        }*/
-        
-        /*int curr = 0;
-        for(int i:nums) if(i >= k) return 1;
-        res = Math.min(res, binarySearch(-1, curr, list, k));
-        //System.out.println(res);
-        for(int i=0; i<nums.length; i++) {
-            curr += nums[i];
-            if(curr < 0) curr = 0;
-            res = Math.min(res, binarySearch(i, curr, list, k));
-        }*/
         return res == Integer.MAX_VALUE ? -1 : res;
     }
 }
