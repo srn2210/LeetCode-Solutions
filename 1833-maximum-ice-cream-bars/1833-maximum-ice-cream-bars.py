252fdef5c2c4c967fcdd380e79ff3877fdf0726e
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-01-06 09:49:53 +0530

    Time: 1692 ms (43.19%), Space: 28 MB (61.26%) - LeetHub

---
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxIceCream(self, costs: List[int], coins: int) -> int:
+        m = max(costs)
+        freq = [0 for i in range(m+1)]
+        for cost in costs:
+            freq[cost] += 1
+        ans = 0
+        for i in range(m+1):
+            if coins >= i * freq[i]:
+                coins -= i * freq[i]
+                ans += freq[i]
+            elif coins >= i:
+                #print(i, " ", coins, " ", coins // i)
+                ans += coins // i
+                coins -= (coins // i) * i
+            else: break
+        return ans
