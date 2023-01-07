08069d72de72cfadf721ed8b1cf3c2eeadd94114
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-01-07 17:31:06 +0530

    Time: 1 ms (100.00%), Space: 62.4 MB (77.13%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -1,5 +1,8 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
+        // If the total sum of gas - cost is > 0 then there exists a valid answer
+        // The answer would be the lowest index with the highest right sum inluding the index itself
+        // This is because the lowest index with the highest presum to the right side has the highest chance of completing the journey
         int pre = gas[gas.length-1] - cost[cost.length-1];
         int ans = gas.length-1;
         int max = pre;
