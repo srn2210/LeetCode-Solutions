217346586a3c8fec5dbece696d14056e1bfd8737
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-01-08 22:33:57 +0530

    Time: 21 ms (89.62%), Space: 42.1 MB (82.37%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.java
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.java
@@ -1,20 +1,19 @@
 class Solution {
     public int maxPoints(int[][] points) {
         double m;
-        int ans = 1;
+        if(points.length == 1) return 1;
+        int ans = 0;
         for(int i=0; i<points.length; i++) {
+            Map<Double, Integer> map = new HashMap<>();
             for(int j=i+1; j<points.length; j++) {
+                if(i == j) continue;
                 m = ((double)points[j][1] - points[i][1]) / ((double)points[j][0] - points[i][0]);
-                int curr = 2;
-                ans = Math.max(ans, curr);
-                for(int k=0; k<points.length; k++) {
-                    if(k == i || k == j) continue;
-                    double t = ((double)points[j][1] - points[k][1]) / ((double)points[j][0] - points[k][0]);
-                    if(t == m) curr++;
-                    ans = Math.max(ans, curr);
-                }
+                if(m == -0.0) m = 0.0;
+                if(points[j][0] == points[i][0]) m = Double.MAX_VALUE;
+                map.put(m, map.getOrDefault(m, 0) + 1);
+                ans = Math.max(ans, map.get(m));
             }
         }
-        return ans;
+        return ans + 1;
     }
 }
