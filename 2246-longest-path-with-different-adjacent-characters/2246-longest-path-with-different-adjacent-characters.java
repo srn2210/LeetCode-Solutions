acb8c57146003a50141a0cfe1f906820235e72d6
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-01-13 20:37:50 +0530

    Time: 158 ms (69.41%), Space: 115.5 MB (59.87%) - LeetHub

--- 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.java
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.java
@@ -1,10 +1,10 @@
 class Solution {
     int ans = 1;
-    int dfs(List<Integer>[] adj, int[] parent, String s, int idx) {
-        if(adj[idx].size() == 0) return 1;
+    int dfs(Map<Integer, List<Integer>>adj, int[] parent, String s, int idx) {
+        if(adj.get(idx) == null) return 1;
         int x = 0;
         int y = 0;
-        for(int i : adj[idx]) {
+        for(int i : adj.get(idx)) {
             int t = dfs(adj, parent, s, i);
             if(s.charAt(i) != s.charAt(idx) && t > x) {
                 if(x > y) y = x;
@@ -19,9 +19,8 @@ class Solution {
         return Math.max(x, y) + 1;
     }
     public int longestPath(int[] parent, String s) {
-        List<Integer> adj[] = new List[parent.length];
-        for(int i=0; i<parent.length; i++) adj[i] = new ArrayList<>();
-        for(int i=1; i<parent.length; i++) adj[parent[i]].add(i);
+        Map<Integer, List<Integer>> adj = new HashMap<>();
+        for(int i=1; i<parent.length; i++) adj.computeIfAbsent(parent[i], a -> new ArrayList<>()).add(i);
         dfs(adj, parent, s, 0);
         return ans;
     }
