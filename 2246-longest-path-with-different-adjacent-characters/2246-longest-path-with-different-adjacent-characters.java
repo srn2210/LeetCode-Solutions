495a3f0d59d8284309615a7e58c04bae7be10aa5
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-01-13 20:27:51 +0530

    Time: 115 ms (82.24%), Space: 100.8 MB (76.65%) - LeetHub

---
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.java
@@ -0,0 +1,35 @@
+class Solution {
+    int ans = 1;
+    int dfs(List<Integer>[] adj, int[] parent, String s, int idx) {
+        if(adj[idx].size() == 0) return 1;
+        int x = 0;
+        int y = 0;
+        for(int i : adj[idx]) {
+            //if(i == prev) continue;
+            int t = dfs(adj, parent, s, i);
+            if(s.charAt(i) != s.charAt(idx) && t > x) {
+                if(x > y) y = x;
+                x = t;
+            }
+            else if(s.charAt(i) != s.charAt(idx) && t > y) {
+                if(y > x) x = y;
+                y = t;
+            }
+        }
+        ans = Math.max(ans, x + y + 1);
+        return Math.max(x, y) + 1;
+    }
+    public int longestPath(int[] parent, String s) {
+        List<Integer> adj[] = new List[parent.length];
+        for(int i=0; i<parent.length; i++) {
+            adj[i] = new ArrayList<>();
+        }
+        for(int i=1; i<parent.length; i++) {
+            adj[parent[i]].add(i);
+            //adj[i].add(parent[i]);
+        }
+        //System.out.println(Arrays.toString(adj));
+        dfs(adj, parent, s, 0);
+        return ans;
+    }
+}
