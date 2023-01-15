ac8908fd3a3799887a191e39f8f79b7ee194c4e1
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-01-15 11:17:46 +0530

    Time: 27 ms (91.67%), Space: 50.1 MB (100.00%) - LeetHub

--- 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.java
+++ 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.java
@@ -1,6 +1,5 @@
 class Solution {
     public int[][] rangeAddQueries(int n, int[][] queries) {
-        //int[][] ans = new int[n][n];
         int[][] pre = new int[n][n];
         for(int[] query : queries) {
             for(int i=query[0]; i<=query[2]; i++) {
