67ae5b2434b0814829cea9383b8638ee9f6f43f3
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-01-15 10:03:43 +0530

    Time: 95 ms (100.00%), Space: 52.9 MB (100.00%) - LeetHub

---
+++ 2537-count-the-number-of-good-subarrays/2537-count-the-number-of-good-subarrays.java
@@ -0,0 +1,36 @@
+class Solution {
+    long sum(int n) {
+        if(n == 1) return 0;
+        else return (n * (n - 1)) / 2;
+    }
+    public long countGood(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Set<Integer> set = new HashSet<>();
+        int hi = 0;
+        int lo = 0;
+        long curr = 0;
+        long ans = 0;
+        while(lo < nums.length && hi <= nums.length) {
+            if(curr < k && hi < nums.length) {
+                map.put(nums[hi], map.getOrDefault(nums[hi], 0) + 1);
+                if(map.get(nums[hi]) >= 2) {
+                    curr += (sum(map.get(nums[hi])) - sum(map.get(nums[hi])-1));
+                    set.add(nums[hi]);
+                }
+                hi++;
+            }
+            else {
+                if(curr >= k) {
+                    ans += nums.length - hi + 1;
+                }
+                if(map.get(nums[lo]) >= 2) {
+                    curr = curr - sum(map.get(nums[lo])) + sum(map.get(nums[lo])-1);
+                }
+                map.put(nums[lo], map.get(nums[lo])-1);
+                if(map.get(nums[lo]) == 1) set.remove(nums[lo]);
+                lo++;
+            }
+        }
+        return ans;
+    }
+}
