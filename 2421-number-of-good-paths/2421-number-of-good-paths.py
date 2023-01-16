789f564a7a06c7ce683dbafc2a47fd074680d6ff
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-01-17 01:25:43 +0530

    Time: 2353 ms (68.61%), Space: 33 MB (87.12%) - LeetHub

---
+++ 2421-number-of-good-paths/2421-number-of-good-paths.py
@@ -0,0 +1,48 @@
+class Solution:
+    class UnionFind:
+        arr = []
+        size = []
+        def __init__(self, n: int):
+            self.arr = []
+            self.size = []
+            for i in range(n):
+                self.arr.append(i)
+                self.size.append(1)
+        def find(self, p: int) -> int:
+            if p == self.arr[p]: return p
+            self.arr[p] = self.find(self.arr[p])
+            return self.arr[p]
+        def union(self, p: int, q: int):
+            x = self.find(p)
+            y = self.find(q)
+            if x == y: return
+            if self.size[x] < self.size[y]:
+                self.arr[x] = self.arr[y]
+                self.size[y] += self.size[x]
+            else:
+                self.arr[y] = self.arr[x]
+                self.size[x] += self.size[y]
+    def numberOfGoodPaths(self, vals: List[int], edges: List[List[int]]) -> int:
+        obj = self.UnionFind(len(vals))
+        adj = [[] for i in range(len(vals))]
+        for edge in edges:
+            adj[edge[0]].append(edge[1])
+            adj[edge[1]].append(edge[0])
+        m1 = dict()
+        ans = 0
+        for i in range(len(vals)):
+            if vals[i] not in m1:
+                m1[vals[i]] = [i]
+            else:
+                m1[vals[i]].append(i)
+        for i, val in sorted(m1.items()):
+            for node in val:
+                for neighbours in adj[node]:
+                    if vals[neighbours] <= vals[node]:
+                        obj.union(node, neighbours)
+            m2 = defaultdict(int)
+            for node in val:
+                m2[obj.find(node)] += 1
+            for x, y in m2.items():
+                ans += (y * (y + 1)) // 2
+        return ans
