0fa388084584aa559fa13661169c7e9460f911b4
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-01-19 18:10:47 +0530

    Time: 3 ms (98.01%), Space: 45.8 MB (96.39%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.java
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.java
@@ -3,14 +3,14 @@ class Solution {
         int[] arr = new int[k];
         int sum = 0;
         int ans = 0;
+        arr[0] = 1;
         for(int num : nums) {
             sum += num;
             arr[(sum % k + k) % k]++;
         }
-        for(int i=1; i<k; i++) {
+        for(int i=0; i<k; i++) {
             ans += arr[i] * (arr[i] - 1) / 2;
         }
-        ans += arr[0] * (arr[0] + 1) / 2;
         return ans;
     }
 }
