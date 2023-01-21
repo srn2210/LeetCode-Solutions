bfff002b73ec9962cf7bcbb40f809365d1dd3e80
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-01-21 19:08:19 +0530

    Time: 9 ms (37.65%), Space: 43.3 MB (25.89%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.java
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.java
@@ -9,7 +9,7 @@ class Solution {
     }
     void backtrack(List<String> ans, int cnt, String s, StringBuilder curr, int idx) {
         if(cnt == 3 ) {
-            for(int i=idx; i<s.length(); i++) curr.append(s.charAt(i));
+            curr.append(s.substring(idx));
             if(isValid(curr)) {
                 ans.add(curr.toString());
                 return;
