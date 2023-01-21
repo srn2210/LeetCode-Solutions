1498dd289e18ee385e941f0976e8ca559d12f043
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-01-21 16:44:51 +0530

    Time: 13 ms (20.03%), Space: 43.2 MB (28.29%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.java
@@ -0,0 +1,33 @@
+class Solution {
+    boolean isValid(StringBuilder curr) {
+        String[] arr = curr.toString().split("[.]");
+        if(arr.length != 4) return false;
+        for(String s : arr) {
+            if(s.length() == 0 || (s.charAt(0) == '0' && s.length() != 1) || Integer.parseInt(s) > 255) return false;
+        }
+        return true;
+    }
+    void backtrack(List<String> ans, int cnt, StringBuilder curr, int idx) {
+        if(cnt == 3) {
+            if(isValid(curr)) {
+                ans.add(curr.toString());
+                return;
+            }
+            else return;
+        }
+        else if(cnt < 3) {
+            for(int i=idx; i<curr.length(); i++) {
+                curr.insert(i+1, '.');
+                backtrack(ans, cnt + 1, curr, i+2);
+                curr.deleteCharAt(i+1);
+            }
+        }
+        else return;
+    }
+    public List<String> restoreIpAddresses(String s) {
+        if(s.length() < 4 || s.length() > 12) return new ArrayList<>();
+        List<String> ans = new ArrayList<>();
+        backtrack(ans, 0, new StringBuilder(s), 0);
+        return ans;
+    }
+}
