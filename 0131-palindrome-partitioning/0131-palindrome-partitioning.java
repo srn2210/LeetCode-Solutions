8e2b24e46d1240c612d9fe833a693bd8c57a360c
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-01-22 17:53:50 +0530

    Time: 9 ms (90.92%), Space: 127.3 MB (38.15%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -0,0 +1,46 @@
+class Solution {
+    boolean check(StringBuilder s) {
+        int l = 0;
+        int r = s.length()-1;
+        while(l < r) {
+            if(s.charAt(l++) != s.charAt(r--)) return false;
+        }
+        return true;
+    }
+    void backtrack(List<List<String>> ans, String s, String[][] dp, List<String> curr, int idx) {
+        if(idx == s.length()) {
+            ans.add(new ArrayList<>(curr));
+            return;
+        }
+        else {
+            for(int i=idx; i<s.length(); i++) {
+                if(dp[idx][i] == null) {
+                    continue;
+                }
+                curr.add(dp[idx][i]);
+                backtrack(ans, s, dp, curr, i+1);
+                curr.remove(curr.size()-1);
+            }
+        }
+    }
+    public List<List<String>> partition(String s) {
+        int n = s.length();
+        String[][] dp = new String[n][n];
+        for(int i=0; i<n; i++) {
+            StringBuilder sb = new StringBuilder();
+            for(int j=i; j<n; j++) {
+                sb.append(s.charAt(j));
+                if(check(sb)) dp[i][j] = sb.toString();
+            }
+        }
+        /*for(String[] ss : dp) {
+            for(String sss : ss) {
+                System.out.print(sss + "     ");
+            }
+            System.out.println();
+        }*/
+        List<List<String>> ans = new ArrayList<>();
+        backtrack(ans, s, dp, new ArrayList<>(), 0);
+        return ans;
+    }
+}
