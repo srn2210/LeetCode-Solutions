a66f844ff1d2e11f65e015a34179f8f53276541f
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-01-24 21:10:14 +0530

    Time: 14 ms (35.51%), Space: 48.8 MB (21.74%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.java
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.java
@@ -1,18 +1,18 @@
 class Solution {
     public int snakesAndLadders(int[][] board) {
         int n = board.length;
-        Map<Integer, Integer> map = new HashMap<>();
+        int[] map = new int[n * n + 1];
         int cnt = 1;
         for(int i=n-1; i>=0; i-=2) {
             for(int j=0; j<n; j++) {
-                map.put(cnt++, board[i][j]);
+                map[cnt++] = board[i][j];
             }
             cnt += n;
         }
         cnt = n + 1;
         for(int i=n-2; i>=0; i-=2) {
             for(int j=n-1; j>=0; j--) {
-                map.put(cnt++, board[i][j]);
+                map[cnt++] = board[i][j];
             }
             cnt += n;
         }
@@ -22,16 +22,16 @@ class Solution {
         int ans = 0;
         while(!queue.isEmpty()) {
             int size = queue.size();
-            while(size-->0) {
+            while(size-- > 0) {
                 int x = queue.poll();
                 for(int i=1; i<=6; i++) {
                     int t = x + i;
                     if(t > n * n) continue;
-                    if(map.get(t) != -1 && map.get(t) == n * n) return ans + 1;
+                    if(map[t] != -1 && map[t] == n * n) return ans + 1;
                     if(t == n * n) return ans + 1;
                     if(!set.contains(t)) {
                         set.add(t);
-                        if(map.get(t) != -1) queue.add(map.get(t));
+                        if(map[t] != -1) queue.add(map[t]);
                         else queue.add(t);
                     }
                 }
