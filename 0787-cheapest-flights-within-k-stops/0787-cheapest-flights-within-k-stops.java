5b30c1e1f7590a9cc911765a0c00b49ca416ea2e
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-01-27 02:14:03 +0530

    Time: 4 ms (97.56%), Space: 43.4 MB (59.82%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,36 +1,22 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        
-        /// TO - DO
-        
-        Map<Integer, List<int[]>> adj = new HashMap<>();
-        for (int[] i : flights)
-            adj.computeIfAbsent(i[0], value -> new ArrayList<>()).add(new int[] { i[1], i[2] });
+        // Distance from source to all other nodes.
+        int[] dist = new int[n];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[src] = 0;
 
-        int[] stops = new int[n];
-        Arrays.fill(stops, Integer.MAX_VALUE);
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
-        // {dist_from_src_node, node, number_of_stops_from_src_node}
-        pq.offer(new int[] { 0, src, 0 });
-
-        while (!pq.isEmpty()) {
-            int[] temp = pq.poll();
-            int dist = temp[0];
-            int node = temp[1];
-            int steps = temp[2];
-            // We have already encountered a path with a lower cost and fewer stops,
-            // or the number of stops exceeds the limit.
-            if (steps > stops[node] || steps > k + 1)
-                continue;
-            stops[node] = steps;
-            if (node == dst)
-                return dist;
-            if (!adj.containsKey(node))
-                continue;
-            for (int[] a : adj.get(node)) {
-                pq.offer(new int[] { dist + a[1], a[0], steps + 1 });
+        // Run only K+1 times since we want shortest distance in K hops
+        for (int i = 0; i <= k; i++) {
+            // Create a copy of dist vector.
+            int[] temp = Arrays.copyOf(dist, n);
+            for (int[] flight : flights) {
+                if (dist[flight[0]] != Integer.MAX_VALUE) {
+                    temp[flight[1]] = Math.min(temp[flight[1]], dist[flight[0]] + flight[2]);
+                }
             }
+            // Copy the temp vector into dist.
+            dist = temp;
         }
-        return -1;
+        return dist[dst] == Integer.MAX_VALUE ? -1 : dist[dst];
     }
-}
+};
