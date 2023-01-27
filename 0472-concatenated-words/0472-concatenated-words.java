29df2353a718423420c65e0a67637f872cf4c426
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-01-28 00:15:04 +0530

    Time: 143 ms (23.94%), Space: 70.7 MB (35.98%) - LeetHub

---
+++ 0472-concatenated-words/0472-concatenated-words.java
@@ -0,0 +1,35 @@
+class Solution {
+    boolean solve(String s, Set<String> set, Boolean[] dp, int idx) {
+        if(idx == s.length()) return true;
+        if(dp[idx] != null) return dp[idx];
+        for(int i=idx; i<s.length(); i++) {
+            String t = s.substring(idx, i+1);
+            if(set.contains(t) && solve(s, set, dp, i+1)) return dp[i] = true;
+        }
+        return dp[idx] = false;
+    }
+    public List<String> findAllConcatenatedWordsInADict(String[] words) {
+        List<String> ans = new ArrayList<>();
+        Set<String> set = new HashSet<>();
+        int least = Integer.MAX_VALUE;
+        int least2 = Integer.MAX_VALUE;
+        for(String s : words) {
+            if(s.length() <= least) {
+                least2 = least;
+                least = s.length();
+            }
+            else if(s.length() < least2) {
+                least2 = s.length();
+            }
+            set.add(s);
+        }
+        for(String s : words) {
+            if(s.length() >= least + least2) {
+                set.remove(s);
+                if(solve(s, set, new Boolean[s.length()], 0)) ans.add(s);
+                set.add(s);
+            }
+        }
+        return ans;
+    }
+}
