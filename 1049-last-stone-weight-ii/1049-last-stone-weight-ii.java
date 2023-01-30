47cd7725ace855a0762ac67292ac87b9baee6b43
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-01-30 20:56:38 +0530

    Time: 11 ms (21.05%), Space: 44.6 MB (5.02%) - LeetHub

--- 1049-last-stone-weight-ii/1049-last-stone-weight-ii.java
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.java
@@ -3,7 +3,6 @@ class Solution {
     int solve(int[] stones, int max, Map<Integer, Integer>[] dp, int idx) {
         if(idx == stones.length) return max;
         if(dp[idx].containsKey(max)) return dp[idx].get(max);
-        //int ans = Integer.MAX_VALUE;
         int ans1 = solve(stones, max - stones[idx], dp, idx + 1);
         int ans2 = solve(stones, max + stones[idx], dp, idx + 1);
         dp[idx].put(max, Math.min(ans1, ans2));
@@ -13,17 +12,9 @@ class Solution {
     }
     public int lastStoneWeightII(int[] stones) {
         ans = Integer.MAX_VALUE;
-        //int max = 0;
-        //for(int i : stones) max += i;
         Map<Integer, Integer>[] dp = new Map[stones.length];
         for(int i=0; i<dp.length; i++) dp[i] = new HashMap<>();
         solve(stones, 0, dp, 0);
-        //System.out.println(Arrays.toString(dp));
-        /*for(var m : dp) {
-            for(var entry : m.entrySet()) {
-                if(entry.getValue() >= 0) ans = Math.min(ans, entry.getValue());
-            }
-        }*/
         return ans;
     }
 }
