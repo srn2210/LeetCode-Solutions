ff3cd9fb3e5bdb470ed8701ff795381a5aeb9633
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-01-31 13:12:55 +0530

    Time: 41 ms (87.07%), Space: 42.6 MB (64.63%) - LeetHub

---
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int bestTeamScore(int[] scores, int[] ages) {
+        int[][] arr = new int[scores.length][2];
+        for(int i=0; i<scores.length; i++) {
+            arr[i][0] = scores[i];
+            arr[i][1] = ages[i];
+        }
+        Arrays.sort(arr, (a,b) -> Integer.compare(a[0], b[0]) == 0 ? Integer.compare(a[1], b[1]) : Integer.compare(a[0], b[0]));
+        int[] dp = new int[scores.length];
+        for(int i=0; i<scores.length; i++) {
+            int max = Integer.MIN_VALUE;
+            for(int j=i-1; j>=0; j--) {
+                if(arr[j][1] > arr[i][1]) continue;
+                max = Math.max(max, dp[j]);
+            }
+            max = max == Integer.MIN_VALUE ? 0 : max;
+            dp[i] = arr[i][0] + max;
+        }
+        int ans = Integer.MIN_VALUE;
+        for(int i : dp) ans = Math.max(ans, i);
+        return ans;
+    }
+}
