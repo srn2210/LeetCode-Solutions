792c414480ce320a03c440cddde2e0e9389f7e8b
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-02-02 16:57:31 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (81.13%) - LeetHub

---
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.java
@@ -0,0 +1,21 @@
+class Solution {
+    boolean check(String s1, String s2, int[] map) {
+        for(int i=0; i<s1.length(); i++) {
+            if(i >= s2.length()) return false;
+            if(map[s1.charAt(i)-'a'] < map[s2.charAt(i)-'a']) return true;
+            if(map[s1.charAt(i)-'a'] > map[s2.charAt(i)-'a']) return false;
+        }
+        return true;
+    }
+    public boolean isAlienSorted(String[] words, String order) {
+        int[] map = new int[26];
+        int k = 0;
+        for(char ch : order.toCharArray()) map[ch-'a'] = k++;
+        for(int i=0; i<words.length; i++) {
+            for(int j=i+1; j<words.length; j++) {
+                if(!check(words[i], words[j], map)) return false;
+            }
+        }
+        return true;
+    }
+}
