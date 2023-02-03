d67f64f9b4732651a31f5892034c08ad98ec5a54
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-02-03 16:49:02 +0530

    Time: 5 ms (82.14%), Space: 42.7 MB (72.96%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String convert(String s, int numRows) {
+        if(numRows == 1) return s;
+        StringBuilder[] list = new StringBuilder[numRows+1];
+        for(int i=0; i<=numRows; i++) list[i] = new StringBuilder();
+        //list[1].append(s.charAt(0));
+        int n = 1;
+        boolean sign = false;
+        for(int i=0; i<s.length(); i++) {
+            list[n].append(s.charAt(i));
+            if(n == numRows || n == 1) sign = !sign;
+            if(sign) n++;
+            else n--;
+        }
+        StringBuilder res = new StringBuilder();
+        for(int i=1; i<=numRows; i++) res.append(list[i]);
+        return res.toString();
+    }
+}
