4891d3fe6ecfe93aa2dc481b1fc51453c8cdd0c4
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-02-03 19:30:07 +0530

    Time: 68 ms (98.41%), Space: 46.3 MB (96.40%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,7 +1,7 @@
 class Solution {
     public int countPrimes(int n) {
-        boolean[] arr = new boolean[n];
         if(n < 3) return 0;
+        boolean[] arr = new boolean[n];
         int ans = 1;
         for(long i=3; i<n; i+=2) {
             if(!arr[(int)i]) {
