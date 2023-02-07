0c7dec2b00dc6fa7db274eb3b17f058e4b0eefea
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-02-07 18:56:50 +0530

    Time: 68 ms (27.97%), Space: 52.9 MB (26.53%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int ans = Integer.MIN_VALUE;
+        for(int left=0, right=0; right<fruits.length; right++) {
+            if(map.containsKey(fruits[right])) {
+                map.put(fruits[right], map.get(fruits[right]) + 1);
+            }
+            else if(!map.containsKey(fruits[right]) && map.size() < 2) {
+                map.put(fruits[right], 1);
+            }
+            else if(!map.containsKey(fruits[right]) && map.size() == 2) {
+                //ans = Math.max(ans, right-left+1);
+                while(left < right) {
+                    map.put(fruits[left], map.get(fruits[left])-1);
+                    if(map.get(fruits[left]) == 0) {
+                        map.remove(fruits[left]);
+                        left++;
+                        break;
+                    }
+                    left++;
+                }
+                map.put(fruits[right], 1);
+            }
+            ans = Math.max(ans, right-left+1);
+        }
+        return ans;
+    }
+}
