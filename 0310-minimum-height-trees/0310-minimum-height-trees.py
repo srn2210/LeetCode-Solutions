0afc4ba23495cd94ebd748eabb4831990a9f250a
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-02-14 22:43:57 +0530

    Time: 578 ms (40.52%), Space: 41.8 MB (6.81%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.py
+++ 0310-minimum-height-trees/0310-minimum-height-trees.py
@@ -11,17 +11,18 @@ class Solution:
                     dist = t[0]
                     e = t[1]
         return [dist, e]
-    p = []
+    path = []
     def dfs2(self, src: int, adj: List[List[int]], prev: int, end: int) -> bool:
         if len(adj[src]) == 1 and prev == adj[src][0]:
             if src == end:
-                self.p.append(end)
+                self.path.append(end)
                 return True
-            else: return False
+            else:
+                return False
         for edge in adj[src]:
             if edge != prev:
                 if self.dfs2(edge, adj, src, end):
-                    self.p.append(src)
+                    self.path.append(src)
                     return True
         return False
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
@@ -32,12 +33,12 @@ class Solution:
         for edge in edges:
             adj[edge[0]].append(edge[1])
             adj[edge[1]].append(edge[0])
-        vis = set()
         start = self.dfs(0, adj, -1)[1]
         end = self.dfs(start, adj, -1)[1]
-        self.p = []
+        self.path = []
         self.dfs2(start, adj, -1, end)
-        if len(self.p) % 2 == 0:
-            return [self.p[len(self.p)//2], self.p[len(self.p)//2-1]]
+        pathLen = len(self.path)
+        if pathLen % 2 == 0:
+            return [self.path[pathLen//2], self.path[pathLen//2-1]]
         else:
-            return [self.p[len(self.p)//2]]
+            return [self.path[pathLen//2]]
