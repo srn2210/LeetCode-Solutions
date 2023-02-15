8b0142a23fe9ce69d6fa84bf5b1c091c73bfd161
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-02-16 00:17:31 +0530

    Time: 3 ms (97.70%), Space: 44 MB (55.33%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.java
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.java
@@ -1,21 +1,21 @@
 class Solution {
     public List<Integer> addToArrayForm(int[] num, int k) {
-        var ans = new ArrayList<Integer>();
+        var ans = new LinkedList<Integer>();
         int carry = 0;
         for(int i=num.length-1; i>=0; i--) {
             int t = num[i] + (k%10) + carry;
-            ans.add(t%10);
+            ans.addFirst(t%10);
             carry = t/10;
             k /= 10;
         }
         while(k != 0) {
             int t = (k%10) + carry;
-            ans.add(t%10);
+            ans.addFirst(t%10);
             k /= 10;
             carry = t/10;
         }
-        if(carry != 0) ans.add(carry);
-        Collections.reverse(ans);
+        if(carry != 0) ans.addFirst(carry);
+        //Collections.reverse(ans);
         return ans;
     }
 }
