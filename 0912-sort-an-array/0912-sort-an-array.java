4554a6c0148b8122a0b13d11ed350981f93ef0a5
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-02-18 00:47:20 +0530

    Time: 1884 ms (7.37%), Space: 59.8 MB (20.69%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -4,13 +4,9 @@ class Solution {
         nums[pivot] = nums[ind];
         nums[ind] = t;
     }
-    /*void sort(int[] nums, int left, int right, int pivot) {
-        
-    }*/
-    void partition(int[] nums, int left, int right) {
+    void sort(int[] nums, int left, int right) {
         if(left >= right) return;
         int pivot = (int)(Math.random()*(right-left+1))+left;
-        //sort(nums, left, right, mid);
         int piv = nums[pivot];
         swap(pivot, right, nums);
         int l = left;
@@ -23,11 +19,11 @@ class Solution {
             }
         }
         swap(l, right, nums);
-        partition(nums, left, l-1);
-        partition(nums, l+1, right);
+        sort(nums, left, l-1);
+        sort(nums, l+1, right);
     }
     public int[] sortArray(int[] nums) {
-        partition(nums, 0, nums.length-1);
+        sort(nums, 0, nums.length-1);
         return nums;
     }
 }
