723db945c84426bff9cc239a2dd0ab8a7daa7072
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-02-17 23:29:15 +0530

    Time: 2112 ms (5.02%), Space: 57.6 MB (22.74%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -10,28 +10,20 @@ class Solution {
         int l = left;
         int r = right-1;
         while(l<=r) {
-            if(nums[l] <= piv) {
-                l++;
-                //if(l == r) l++;
-                //r++;
-            }
+            if(nums[l] <= piv) l++;
             else {
                 swap(l, r, nums);
                 r--;
             }
         }
         swap(l, right, nums);
-        //System.out.println(Arrays.toString(nums));
         partition(nums, left, l-1);
         partition(nums, l+1, right);
     }
     void partition(int[] nums, int left, int right) {
         if(left >= right) return;
         int mid = left + (right - left) / 2;
-        //System.out.println(left + " " + right + " " + mid);
         sort(nums, left, right, mid);
-        //partition(nums, 0, left-1);
-        //partition(nums, right+1, nums.length-1);
     }
     public int[] sortArray(int[] nums) {
         partition(nums, 0, nums.length-1);
