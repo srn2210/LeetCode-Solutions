bb420350aa935abbd6daa5e2206e9f8dd9030513
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-02-18 00:44:28 +0530

    Time: 1888 ms (7.27%), Space: 62.6 MB (20.26%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,39 +1,32 @@
 class Solution {
-    public int[] sortArray(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return nums;
-        }
-        
-        quickSort(nums, 0, nums.length - 1);
-        return nums;
+    void swap(int pivot, int ind, int[] nums) {
+        int t = nums[pivot];
+        nums[pivot] = nums[ind];
+        nums[ind] = t;
     }
-    
-    private void quickSort(int[] nums, int start, int end) {
-        if (start >= end) return;
-        
-        int left = start, right = end;
-		// Ideally, numbers are approximately increasing, which means the middle number is most likey the median number. 
-		// The closer pivot is to the median number, the less swapping and recursion we need to do.
-        int pivot = nums[left + (right - left) / 2];
-        // with (left <= right), we can end with: 
-		// 1. x, x, x, right, left, x, x, x,
-		// or
-		// 2. x, x, x, right, pivot, left, x, x, x
-		// easy to divide without boundary issues
-        while (left <= right) {
-			// if we put nums[left] <= pivot, for array like [2, 1], 1 swap with itself and leads to dead loop
-            while (left <= right && nums[left] < pivot) left++;
-            while (left <= right && nums[right] > pivot) right--;
-            if (left <= right) {
-                int temp = nums[left];
-                nums[left] = nums[right];
-                nums[right] = temp;
-                left++;
-                right--;
+    void sort(int[] nums, int left, int right, int pivot) {
+        int piv = nums[pivot];
+        swap(pivot, right, nums);
+        int l = left;
+        int r = right-1;
+        while(l<=r) {
+            if(nums[l] <= piv) l++;
+            else {
+                swap(l, r, nums);
+                r--;
             }
         }
-        
-        quickSort(nums, start, right);
-        quickSort(nums, left, end);
+        swap(l, right, nums);
+        partition(nums, left, l-1);
+        partition(nums, l+1, right);
+    }
+    void partition(int[] nums, int left, int right) {
+        if(left >= right) return;
+        int mid = (int)(Math.random()*(right-left+1))+left;
+        sort(nums, left, right, mid);
+    }
+    public int[] sortArray(int[] nums) {
+        partition(nums, 0, nums.length-1);
+        return nums;
     }
 }
