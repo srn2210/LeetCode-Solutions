5cb1b38cc2a13e18800bccc573cddf225b685fdc
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-02-21 02:33:39 +0530

    Time: 8 ms (42.85%), Space: 42.1 MB (70.37%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -4,28 +4,20 @@ class Solution {
         map.put(1, "I"); map.put(4, "IV"); map.put(5, "V"); map.put(9, "IX");
         map.put(10, "X"); map.put(40, "XL"); map.put(50, "L"); map.put(90, "XC");
         map.put(100, "C"); map.put(400, "CD"); map.put(500, "D"); map.put(900, "CM");
-        map.put(1000, "M");
+        map.put(1000, "M"); map.put(0, "");
         int t = 1;
         var res = new StringBuilder();
         while(num != 0) {
             int temp = num % 10;
             temp *= t;
-            if(temp == 0) {
-                num /= 10;
-                t *= 10;
-                continue;
-            }
-            if(!map.containsKey(temp)) {
+            if(map.containsKey(temp)) res.insert(0, map.get(temp));
+            else {
                 while(!map.containsKey(temp)) {
                     temp -= t;
                     res.insert(0, map.get(t));
                 }
                 res.insert(0, map.get(temp));
-                num /= 10;
-                t *= 10;
-                continue;
             }
-            res.insert(0, map.get(temp));
             num /= 10;
             t *= 10;
         }
