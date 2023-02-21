87709bd8fae43e89a70d8a9714fe6beb7389f445
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-02-21 15:22:50 +0530

    Time: 0 ms (100.00%), Space: 47.9 MB (85.53%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -3,7 +3,7 @@ class Solution {
         if(nums.length == 1) return nums[0];
         int l = 0;
         int r = nums.length;
-        while(l<=r) {
+        while(l<r) {
             int mid = l + (r-l) / 2;
             if(mid+1 < nums.length && mid-1 >= 0 && nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]) return nums[mid];
             else if(mid+1 == nums.length && mid-1 >= 0 && nums[mid] != nums[mid-1]) return nums[mid];
@@ -21,6 +21,6 @@ class Solution {
                 else if(mid - 1 >= 0 && nums[mid] == nums[mid-1]) l = mid+1;
             }
         }
-        return nums[r];
+        return nums[l];
     }
 }
