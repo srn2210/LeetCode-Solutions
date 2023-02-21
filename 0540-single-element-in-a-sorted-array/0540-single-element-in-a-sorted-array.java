6e8f2502d1193ae26d3d6ee6455bb09e8cff36b8
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-02-21 15:49:59 +0530

    Time: 0 ms (100.00%), Space: 48.3 MB (39.47%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -5,20 +5,17 @@ class Solution {
         int r = nums.length-1;
         while(l<=r) {
             int mid = l + (r-l) / 2;
-            if(mid+1 < nums.length && mid-1 >= 0 && nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]) return nums[mid];
-            else if(mid+1 == nums.length && mid-1 >= 0 && nums[mid] != nums[mid-1]) return nums[mid];
-            else if(mid-1 < 0 && mid+1 < nums.length && nums[mid] != nums[mid+1]) return nums[mid];
-            else if(mid % 2 == 0) {
+            if(mid % 2 == 0) {
                 if(mid + 1 < nums.length && nums[mid] == nums[mid+1]) {
                     l = mid + 1;
                 }
-                else if(mid - 1 >= 0 && nums[mid] == nums[mid-1]) r = mid-1;
+                else r = mid-1;
             }
             else {
                 if(mid + 1 < nums.length && nums[mid] == nums[mid+1]) {
                     r = mid - 1;
                 }
-                else if(mid - 1 >= 0 && nums[mid] == nums[mid-1]) l = mid+1;
+                else l = mid+1;
             }
         }
         return nums[l];
