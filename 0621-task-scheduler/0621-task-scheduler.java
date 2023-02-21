57e7da541c7f16b9a0f8ba61f3b571531e855213
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-02-21 23:54:45 +0530

    Time: 36 ms (29.05%), Space: 63.2 MB (7.70%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -2,16 +2,16 @@ class Solution {
     public int leastInterval(char[] tasks, int n) {
         var map = new int[26];
         for(char ch : tasks) map[ch-'A']++;
-        var pq = new PriorityQueue<Character>((a,b) -> map[b-'A']-map[a-'A']);
-        for(char ch='A'; ch<='Z'; ch++) if(map[ch-'A'] > 0) pq.add(ch);
+        var pq = new PriorityQueue<Integer>((a,b) -> map[b]-map[a]);
+        for(int i=0; i<26; i++) if(map[i] > 0) pq.add(i);
         int ans = 0;
         while(!pq.isEmpty()) {
             int t = n+1;
-            var remove = new ArrayList<Character>();
+            var remove = new ArrayList<Integer>();
             while(t > 0 && !pq.isEmpty()) {
                 var p = pq.poll();
-                map[p-'A']--;
-                if(map[p-'A'] != 0) remove.add(p);
+                map[p]--;
+                if(map[p] != 0) remove.add(p);
                 ans++;
                 t--;
             }
