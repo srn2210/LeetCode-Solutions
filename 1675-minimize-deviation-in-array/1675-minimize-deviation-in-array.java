b9c4f1923873268681ffc2435202b9a24e9410c9
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-02-25 00:13:35 +0530

    Time: 841 ms (5.66%), Space: 51.8 MB (20.75%) - LeetHub

---
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minimumDeviation(int[] nums) {
+        int n = nums.length;
+        var pq = new PriorityQueue<Long>(n, Collections.reverseOrder());
+        long[] arr = new long[n];
+        long min = Integer.MAX_VALUE;
+        for(int i=0; i<n; i++) {
+            arr[i] = nums[i] % 2 == 0 ? nums[i] : (long)nums[i] * 2;
+            min = Math.min(min, arr[i]);
+            pq.add(arr[i]);
+        }
+        System.out.println(Arrays.toString(arr));
+        long ans = Integer.MAX_VALUE;
+        while(pq.peek() % 2 == 0) {
+            ans = Math.min(ans, pq.peek() - min);
+            min = Math.min(min, pq.peek()/2);
+            pq.add(pq.poll() / 2);
+        }
+        return Math.min((int)ans, (int)(pq.peek()-min));
+    }
+}
