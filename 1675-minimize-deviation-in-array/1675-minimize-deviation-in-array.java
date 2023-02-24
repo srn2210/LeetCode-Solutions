797887c84c9ece5b3596bb1738c24bdf87014d81
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-02-25 00:51:58 +0530

    Time: 96 ms (90.57%), Space: 51 MB (37.74%) - LeetHub

--- 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.java
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.java
@@ -2,8 +2,8 @@ class Solution {
     public int minimumDeviation(int[] nums) {
         var set = new TreeSet<Long>();
         for(int i : nums) set.add((long)i);
-        int ans = Integer.MAX_VALUE;
-        while(set.first() % 2 != 0) {
+        int ans = (int)(set.last()-set.first());
+        /*while(set.first() % 2 != 0) {
             var min = set.first();
             var max = set.last();
             ans = Math.min(ans, (int)(max-min));
@@ -19,20 +19,20 @@ class Solution {
         }
         ans = Math.min(ans, (int)(set.last()-set.first()));
         set = new TreeSet<Long>();
-        for(int i : nums) set.add((long)i);
-        while(set.last() % 2 == 0) {
-            var min = set.first();
-            var max = set.last();
-            ans = Math.min(ans, (int)(max-min));
-            set.remove(max);
-            set.add(max/2);
-        }
+        for(int i : nums) set.add((long)i);*/
         while(set.first() % 2 != 0) {
             var min = set.first();
             var max = set.last();
             ans = Math.min(ans, (int)(max-min));
             set.remove(min);
             set.add(min*2);
+        }
+        while(set.last() % 2 == 0) {
+            var min = set.first();
+            var max = set.last();
+            ans = Math.min(ans, (int)(max-min));
+            set.remove(max);
+            set.add(max/2);
         }
         return Math.min(ans, (int)(set.last()-set.first()));
     }
