b87ec49791d95f61722f594b51ae4a895046e562
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-02-25 00:52:53 +0530

    Time: 97 ms (90.57%), Space: 50.9 MB (47.17%) - LeetHub

--- 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.java
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.java
@@ -3,23 +3,6 @@ class Solution {
         var set = new TreeSet<Long>();
         for(int i : nums) set.add((long)i);
         int ans = (int)(set.last()-set.first());
-        /*while(set.first() % 2 != 0) {
-            var min = set.first();
-            var max = set.last();
-            ans = Math.min(ans, (int)(max-min));
-            set.remove(min);
-            set.add(min*2);
-        }
-        while(set.last() % 2 == 0) {
-            var min = set.first();
-            var max = set.last();
-            ans = Math.min(ans, (int)(max-min));
-            set.remove(max);
-            set.add(max/2);
-        }
-        ans = Math.min(ans, (int)(set.last()-set.first()));
-        set = new TreeSet<Long>();
-        for(int i : nums) set.add((long)i);*/
         while(set.first() % 2 != 0) {
             var min = set.first();
             var max = set.last();
