b0ae7a5913fe00179e94c2c5e87d94160d5289b7
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-02-25 00:57:19 +0530

    Time: 97 ms (90.57%), Space: 51.6 MB (20.75%) - LeetHub

--- 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.java
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.java
@@ -1,25 +1,22 @@
 class Solution {
-    static final int[] q = new int[1600000];
-
     public int minimumDeviation(int[] nums) {
-        final int n = nums.length;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] % 2 == 1) {
-                nums[i] *= 2;
-            }
+        var set = new TreeSet<Long>();
+        for(int i : nums) set.add((long)i);
+        int ans = (int)(set.last()-set.first());
+        while(set.first() % 2 != 0) {
+            var min = set.first();
+            var max = set.last();
+            ans = Math.min(ans, (int)(max-min));
+            set.remove(min);
+            set.add(min*2);
         }
-        Arrays.sort(nums);
-        int qstart = 0;
-        int qend = 0;
-        int best = nums[n - 1] - nums[0];
-        int numptr = n - 2;
-        q[qend++] = nums[n - 1];
-        int min = nums[0];
-        while (true) {
-            final int pick = numptr >= 0 && nums[numptr] > q[qstart] ? nums[numptr--] : q[qstart++];
-            best = Math.min(best, pick - min);
-            if (pick % 2 == 1) return best;
-            min = Math.min(min, q[qend++] = pick / 2);
+        while(set.last() % 2 == 0) {
+            var min = set.first();
+            var max = set.last();
+            ans = Math.min(ans, (int)(max-min));
+            set.remove(max);
+            set.add(max/2);
         }
+        return Math.min(ans, (int)(set.last()-set.first()));
     }
 }
