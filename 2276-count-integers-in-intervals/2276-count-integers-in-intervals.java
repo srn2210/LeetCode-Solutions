e83667e358e69869b76e29f8fbe9fa66e69c98f7
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-02-26 12:14:30 +0530

    Time: 117 ms (32.87%), Space: 105.4 MB (19.44%) - LeetHub

--- 2276-count-integers-in-intervals/2276-count-integers-in-intervals.java
+++ 2276-count-integers-in-intervals/2276-count-integers-in-intervals.java
@@ -6,7 +6,7 @@ class CountIntervals {
         count = 0;
     }
     
-    Pair<Integer, Integer> leftMerge(int left, int right) {
+    Pair<Integer, Integer> merge(int left, int right) {
         var t = map.floorEntry(left);
         if(t != null && left <= t.getValue()) {
             left = Math.min(left, t.getKey());
@@ -25,32 +25,7 @@ class CountIntervals {
     }
     
     public void add(int left, int right) {
-        //var t1f = map.floorEntry(left);
-        //var t1c = map.ceilingEntry(left);
-        //var t2f = map.floorEntry(right);
-        //var t2c = map.ceilingEntry(right);
-        /*if(t2f == null || (t1c == null && t1f.getValue() < left) || (t1f.getValue() < left && right < t1c.getKey())) {
-            map.put(left, right);
-            count += right-left+1;
-        }
-        else if(t1f.getValue() >= left && right <= t1f.getValue()) return;*/
-        /*else if(t1f.getValue() >= left && t1f.getValue() < right && t1c.getKey() <= right && right <= t1c.getValue()) {
-            count += t1c.getKey() - 1 + t1f.getValue() + 1 - 1;
-            int r = t1c.getValue();
-            map.remove(t1c.getKey());
-            map.put(t1f.getKey(), r);
-        }
-        else if(t1f.getValue() >= left && t1f.getValue() < right) {
-            count += right - t1f.getValue();
-            map.put(t1f.getKey(), right);
-        }
-        else if(left < t2f.getValue()) {
-            while(left < t2f.getValue()) {
-                
-            }
-        }*/
-        var p = leftMerge(left, right);
-        //p = rightMerge(p.getKey(), p.getValue());
+        var p = merge(left, right);
         map.put(p.getKey(), p.getValue());
         count += p.getValue() - p.getKey() + 1;
     }
