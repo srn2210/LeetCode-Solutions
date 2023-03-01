6347e30b69c1694ebdd2ef5fbeb25d311c5b66c6
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-03-02 04:01:06 +0530

    Time: 22 ms (23.56%), Space: 46.6 MB (32.55%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,31 +1,12 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        /*Map<Character, Integer>[] map = new Map[strs.length];
-        for(int i=0; i<strs.length; i++) map[i] = new HashMap<>();
-        int idx = 0;
+        Map<String, List<String>> map = new HashMap<>();
+        int[] a = new int[26];
         for(var s : strs) {
-            for(char ch : s) {
-                map[i].put(ch, map[i].getOrDefault(ch,0)+1);
-            }
-            idx++;
-        }*/
-        /*String[][] arr = new String[strs.length][2];
-        for(int i=0; i<strs.length; i++) {
-            arr[i][0] = strs[i];
-            arr[i][1] = String.valueOf(i);
+            Arrays.fill(a, 0);
+            for(char ch : s.toCharArray()) a[ch-'a']++;
+            map.computeIfAbsent(Arrays.toString(a), arr -> new ArrayList<>()).add(s);
         }
-        for(int i=0; i<arr.length; i++) {
-            Arrays.sort(arr[i][0]);
-        }
-        Arrays.sort(arr, (a,b) -> Integer.compare(a[0].length(), b[0].length()));
-        for(int i=0; i<arr.length; i++) System.out.println(Arrays.toString(arr[i]));*/
-        Map<Set<Map.Entry<Character, Integer>>, List<String>> map = new HashMap<>();
-        for(var s : strs) {
-            Map<Character, Integer> m = new HashMap<>();
-            for(char ch : s.toCharArray()) m.put(ch, m.getOrDefault(ch, 0) + 1);
-            map.computeIfAbsent(m.entrySet(), arr -> new ArrayList<>()).add(s);
-        }
-        //System.out.println(map.entrySet());
         return new ArrayList<>(map.values());
     }
 }
