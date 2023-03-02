62ec842cafe52790d765b604f70cf6f1cb278285
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-03-02 22:20:28 +0530

    Time: 1 ms (98.42%), Space: 42.4 MB (29.22%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int compress(char[] chars) {
+        //if(chars.length == 1) return 1;
+        int left = 0;
+        int right = 1;
+        char prev = chars[0];
+        int cnt = 1;
+        while(right < chars.length) {
+            if(chars[right] != prev) {
+                chars[left++] = prev;
+                prev = chars[right];
+                if(cnt == 1) {
+                    cnt = 0;
+                    continue;
+                }
+                else {
+                    var t = String.valueOf(cnt);
+                    for(char ch : t.toCharArray()) chars[left++] = ch;
+                }
+                cnt = 1;
+            }
+            else {
+                cnt++;
+            }
+            right++;
+        }
+        chars[left++] = prev;
+        if(cnt == 1) return left;
+        else {
+            var t = String.valueOf(cnt);
+            for(char ch : t.toCharArray()) chars[left++] = ch;
+        }
+        return left;
+    }
+}
