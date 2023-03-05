83e4280dcfa6780d560de7a2d7332e2919f6470c
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-03-06 01:50:34 +0530

    Time: 24 ms (5.40%), Space: 61.1 MB (37.40%) - LeetHub

--- 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.java
+++ 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.java
@@ -1,13 +1,31 @@
 class Solution {
+    long solve(int[] nums, int minK, int maxK, int left, int right) {
+        if(left > right) return 0;
+        var qMin = new LinkedList<Integer>();
+        var qMax = new LinkedList<Integer>();
+        for(int i=left; i<=right; i++) {
+            if(nums[i] == minK) qMin.add(i);
+            if(nums[i] == maxK) qMax.add(i);
+        }
+        long ans = 0;
+        for(int i=left; i<=right; i++) {
+            while(!qMin.isEmpty() && qMin.peek() < i) qMin.poll();
+            while(!qMax.isEmpty() && qMax.peek() < i) qMax.poll();
+            if(qMin.isEmpty() || qMax.isEmpty()) break;
+            ans += right - Math.max(qMin.peek(), qMax.peek()) + 1;
+        }
+        return ans;
+    }
     public long countSubarrays(int[] nums, int minK, int maxK) {
         long ans = 0;
-        int minP = -1, maxP = -1, leftBound = -1;
+        int prev = 0;
         for(int i=0; i<nums.length; i++) {
-            if(nums[i] < minK || nums[i] > maxK) leftBound = i;
-            if(nums[i] == minK) minP = i;
-            if(nums[i] == maxK) maxP = i;
-            ans += Math.max(0, Math.min(minP, maxP) - leftBound);
+            if(nums[i] < minK || nums[i] > maxK) {
+                ans += solve(nums, minK, maxK, prev, i-1);
+                prev = i+1;
+            }
         }
+        ans += solve(nums, minK, maxK, prev, nums.length-1);
         return ans;
     }
 }
