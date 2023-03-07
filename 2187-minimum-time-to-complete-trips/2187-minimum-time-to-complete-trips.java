490fdaebb764856aae0c55186a5ee1d92edb8eaf
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-03-07 18:24:46 +0530

    Time: 206 ms (76.25%), Space: 57.2 MB (24.62%) - LeetHub

--- 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.java
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.java
@@ -1,19 +1,13 @@
 class Solution {
     boolean check(long t, int[] arr, int total) {
         long ans = 0;
-        for(int i : arr) {
-            ans += t/(long)i;
-        }
+        for(int i : arr) ans += t/(long)i;
         return ans >= total;
     }
     public long minimumTime(int[] time, int totalTrips) {
         long ans = 0;
         int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
-        for(int i : time) {
-            min = Math.min(min, i);
-            max = Math.max(max, i);
-        }
+        for(int i : time) min = Math.min(min, i);
         long l = (long)min;
         long r = (long)min * totalTrips;
         while(l <= r) {
