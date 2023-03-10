01e813b4eff4136f39ce2bd859cb0ecc9fe3bf63
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-03-10 13:40:05 +0530

    Time: 13 ms (35.62%), Space: 44.9 MB (16.06%) - LeetHub

--- 0382-linked-list-random-node/0382-linked-list-random-node.java
+++ 0382-linked-list-random-node/0382-linked-list-random-node.java
@@ -9,24 +9,20 @@
  * }
  */
 class Solution {
-    Map<Integer, ListNode> map;
-    int size;
+    List<ListNode> map;
     Random obj;
     public Solution(ListNode head) {
-        map = new HashMap<>();
-        size = -1;
+        map = new ArrayList<>();
         var temp = head;
         obj = new Random();
         while(temp != null) {
-            var t = temp;
+            map.add(temp);
             temp = temp.next;
-            size++;
-            map.put(size, t);
         }
     }
     
     public int getRandom() {
-        return map.get((obj.nextInt(size+1))).val;
+        return map.get(obj.nextInt(map.size())).val;
     }
 }
 
