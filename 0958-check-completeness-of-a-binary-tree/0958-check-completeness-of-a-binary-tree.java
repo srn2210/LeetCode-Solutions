a17965fcbb39ad9073dfea1c79fee7604fefa24f
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-03-16 01:50:27 +0530

    Time: 1 ms (83.14%), Space: 42.1 MB (36.83%) - LeetHub

--- 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.java
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.java
@@ -15,25 +15,17 @@
  */
 class Solution {
     public boolean isCompleteTree(TreeNode root) {
+        var flag = false;
         var queue = new LinkedList<TreeNode>();
         queue.add(root);
-        int nodes = 1;
         while(!queue.isEmpty()) {
-            int size = queue.size();
-            int levelNodes = size;
-            var q = new LinkedList<TreeNode>();
-            for(int i=0; i<size; i++) {
-                var node = queue.poll();
-                if(node.left == null && node.right != null) return false;
-                if(node.left != null) {
-                    if(q.size() != i * 2) return false;
-                }
-                if(node.left != null) q.add(node.left);
-                if(node.right != null) q.add(node.right);
+            var node = queue.poll();
+            if(node == null) flag = true;
+            else {
+                if(flag) return false;
+                queue.add(node.left);
+                queue.add(node.right);
             }
-            queue = q;
-            if(!queue.isEmpty() && nodes != levelNodes) return false;
-            nodes *= 2;
         }
         return true;
     }
