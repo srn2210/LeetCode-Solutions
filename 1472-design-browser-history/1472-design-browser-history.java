49c417c5e8cc2f791ff68d1be731093e77fb25e7
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-03-18 06:44:42 +0530

    Time: 57 ms (67.07%), Space: 50.4 MB (53.18%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.java
+++ 1472-design-browser-history/1472-design-browser-history.java
@@ -1,36 +1,28 @@
 class BrowserHistory {
-    ArrayList<String> visitedURLs;
-    int currURL, lastURL;
-
+    List<String> history;
+    int curr;
+    int end;
     public BrowserHistory(String homepage) {
-        // 'homepage' is the first visited URL.
-        visitedURLs = new ArrayList<String>(Arrays.asList(homepage));
-        currURL = 0; 
-        lastURL = 0;
+        history = new ArrayList<>();
+        history.add(homepage);
+        curr = 0;
+        end = curr;
     }
     
     public void visit(String url) {
-        currURL += 1;
-        if (visitedURLs.size() > currURL) {
-            // We have enough space in our array to overwrite an old 'url' entry with new one.
-            visitedURLs.set(currURL, url);
-        } else {
-            // We have to insert a new 'url' entry at the end.
-            visitedURLs.add(url);
-        }
-        // This 'url' will be last URL if we try to go forward.
-        lastURL = currURL;
+        if(curr + 1 == history.size()) history.add(url);
+        else history.set(curr + 1, url);
+        curr++;
+        end = curr;
     }
     
     public String back(int steps) {
-        // Move 'currURL' pointer in left direction.
-        currURL = Math.max(0, currURL - steps);
-        return visitedURLs.get(currURL);
+        curr = steps > curr ? 0 : curr - steps;
+        return history.get(curr);
     }
     
     public String forward(int steps) {
-        // Move 'currURL' pointer in right direction.
-        currURL = Math.min(lastURL, currURL + steps);
-        return visitedURLs.get(currURL);
+        curr = curr + steps >= end ? end : curr + steps;
+        return history.get(curr);
     }
 }
