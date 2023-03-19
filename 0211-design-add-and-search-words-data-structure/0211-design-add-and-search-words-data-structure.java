9430fe1e590bd121890a2c563df156b2d0ae0106
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-03-19 06:37:51 +0530

    Time: 1002 ms (31.98%), Space: 106.9 MB (22.28%) - LeetHub

---
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.java
@@ -0,0 +1,87 @@
+class WordDictionary {
+    class TrieNode {
+        Map<Character, TrieNode> map;
+        boolean isEnd;
+        TrieNode() {
+            map = new HashMap<>();
+            isEnd = false;
+        }
+        Set<Map.Entry<Character, TrieNode>> iterator() {
+            return map.entrySet();
+        }
+        TrieNode get(char ch) {
+            return map.get(ch);
+        }
+        boolean containsKey(char ch) {
+            return map.containsKey(ch);
+        }
+        void add(char ch) {
+            map.put(ch, new TrieNode());
+        }
+        void setEnd() {
+            isEnd = true;
+        }
+        boolean isEnd() {
+            return isEnd;
+        }
+        boolean isEmpty() {
+            return map.isEmpty();
+        }
+    }
+    
+    TrieNode root;
+    
+    public WordDictionary() {
+        root = new TrieNode();
+    }
+    
+    public void addWord(String word) {
+        var node = root;
+        for(char ch : word.toCharArray()) {
+            if(!node.containsKey(ch)) node.add(ch);
+            node = node.get(ch);
+        }
+        node.setEnd();
+    }
+    
+    boolean search(String word, int i, TrieNode node) {
+        if(word.length() == i) {
+            return node.isEnd();
+        }
+        if(word.charAt(i) == '.') {
+            var itr = node.iterator();
+            for(var it : itr) {
+                if(search(word, i+1, it.getValue())) return true;
+            }
+        }
+        else {
+            if(node.containsKey(word.charAt(i)) && search(word, i+1, node.get(word.charAt(i)))) return true;
+        }
+        
+        return false;
+    }
+    
+    public boolean search(String word) {
+        var node = root;
+        for(int i=0; i<word.length(); i++) {
+            char ch = word.charAt(i);
+            if(ch == '.') {
+                if(search(word, i, node)) return true;
+                else return false;
+            }
+            else {
+                if(!node.containsKey(ch)) return false;
+                node = node.get(ch);
+                if(i == word.length()-1) return node.isEnd();
+            }
+        }
+        return false;
+    }
+}
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary obj = new WordDictionary();
+ * obj.addWord(word);
+ * boolean param_2 = obj.search(word);
+ */
