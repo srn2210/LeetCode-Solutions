8c2e315e6d92dc7d65f4a079e34844b55d666662
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-03-25 02:47:16 +0530

    Time: 104 ms (51.02%), Space: 72.4 MB (53.05%) - LeetHub

---
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minReorder(int n, int[][] connections) {
+        var map = new HashMap<Integer, List<List<Integer>>>();
+        for(var conn : connections) {
+            map.computeIfAbsent(conn[0], a -> new ArrayList<>()).add(List.of(conn[1], 0));
+            map.computeIfAbsent(conn[1], a -> new ArrayList<>()).add(List.of(conn[0], 1));
+        }
+        int ans = 0;
+        var queue = new LinkedList<Integer>();
+        queue.add(0);
+        var visited = new boolean[n];
+        while(!queue.isEmpty()) {
+            visited[queue.peek()] = true;
+            var t = map.get(queue.poll());
+            for(var l : t) {
+                if(visited[l.get(0)]) continue;
+                if(l.get(1) == 0) ans++;
+                queue.add(l.get(0));
+            }
+        }
+        return ans;
+    }
+}
