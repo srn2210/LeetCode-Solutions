31ff1a3bb20dcd94cdf944a91e56af92c9a9f80e
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-03-26 03:22:05 +0530

    Time: 2133 ms (76.39%), Space: 108.8 MB (27.08%) - LeetHub

---
+++ 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.py
@@ -0,0 +1,27 @@
+class Solution:
+    def countPairs(self, n: int, edges: List[List[int]]) -> int:
+        adj = defaultdict(list)
+        
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+        
+        visited = [False] * n
+        
+        def dfs(node: int) -> int:
+            count = 1
+            visited[node] = True
+            for e in adj[node]:
+                if not visited[e]:
+                    count += dfs(e)
+            return count
+        
+        ans = 0
+        
+        for i in range(n):
+            if not visited[i]:
+                nodes = dfs(i)
+                n -= nodes
+                ans += nodes * n
+                
+        return ans
