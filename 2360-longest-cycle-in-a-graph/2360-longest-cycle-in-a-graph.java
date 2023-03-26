b3ce1e78e63d514f4007ea925cc16396e501f616
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-03-27 00:08:31 +0530

    Time: 39 ms (71.43%), Space: 62 MB (66.80%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.java
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.java
@@ -1,17 +1,17 @@
 class Solution {
     int ans;
-    void dfs(int src, int[] edges, int[] visited, int cLength, Map<Integer, Integer> map) {
+    void dfs(int src, int[] edges, int[] visited, int cLength, int[] map) {
         int i = edges[src];
         if(i == -1) {
             visited[src] = 2;
             return;
         }
         else if(visited[i] == 1) {
-            ans = Math.max(ans , cLength-map.get(i));
+            ans = Math.max(ans , cLength-map[i]);
         }
         else if(visited[i] == 0) {
             visited[i] = 1;
-            map.put(i, cLength);
+            map[i] = cLength;
             dfs(i, edges, visited, cLength+1, map);
         }
         
@@ -21,12 +21,11 @@ class Solution {
     public int longestCycle(int[] edges) {
         int n = edges.length;
         int[] visited = new int[n];
-        var map = new HashMap<Integer, Integer>();
+        var map = new int[n];
         ans = -1;
         for(int i=0; i<n; i++) {
             if(visited[i] == 0) {
                 visited[i] = 1;
-                map.put(i, 0);
                 dfs(i, edges, visited, 1, map);
             }
         }
