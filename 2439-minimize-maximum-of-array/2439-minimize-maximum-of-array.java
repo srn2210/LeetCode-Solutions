ae0b882958a29f43d4bb1d2d20a04047b7cfef0d
srn2210 <110111501+srn2210@users.noreply.github.com>
2023-04-05 19:35:17 +0530

    Time: 9 ms (74.55%), Space: 62.6 MB (10.75%) - LeetHub

--- 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.java
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.java
@@ -1,32 +1,5 @@
 class Solution {
     public int minimizeArrayValue(int[] nums) {
-        /*long sum = nums[0];
-        int cnt = 1;
-        int ans = Integer.MIN_VALUE;
-        var list = new ArrayDeque<Pair<Long, Integer>>();
-        for(int i=1; i<nums.length; i++) {
-            if(nums[i] - nums[i-1] < 0) {
-                list.add(new Pair(sum, cnt));
-                sum = 0;
-                cnt = 0;
-            }
-            sum += nums[i];
-            cnt++;
-        }
-        if(sum != 0) list.add(new Pair(sum, cnt));
-        System.out.println(list);
-        while(list.size() >= 2) {
-            var top1 = list.pop();
-            double t1 = (double)top1.getKey() / top1.getValue();
-            var top2 = list.pop();
-            double t2 = (double) top2.getKey() / top2.getValue();
-            if(t2 > t1) list.add(new Pair(top1.getKey() + top2.getKey(), top1.getValue() + top2.getValue()));
-            else {
-                ans = Math.max(ans, (int) Math.ceil(t1));
-                list.add(top2);
-            }
-        }
-        return Math.max(ans, (int) Math.ceil((double)list.peek().getKey() / list.peek().getValue()));*/
         long sum = 0;
         int n = nums.length;
         double cnt = 1;
@@ -35,8 +8,6 @@ class Solution {
             sum += i;
             max = Math.max(max, (int) Math.ceil(sum / cnt++));
         }
-        //int avgTotal = (int) Math.ceil(sum / (double)n);
-        //int avg = (int) Math.ceil((sum-nums[0]) / (double) (n-1));
         return max;
     }
 }
