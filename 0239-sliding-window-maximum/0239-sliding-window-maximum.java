d7d984749323c5350635e77b8da2f692126d93e8
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-04-11 17:27:49 +0530

    Time: 771 ms (5.01%), Space: 62.9 MB (13.47%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -3,7 +3,7 @@ class Solution {
         var tmap = new TreeMap<Integer, Integer>(Collections.reverseOrder());
         for(int i=0; i<k; i++) tmap.put(nums[i], tmap.getOrDefault(nums[i], 0) + 1);
         int[] ans = new int[nums.length - k + 1];
-        int max = 100000;
+        int max = 10001;
         ans[0] = tmap.ceilingKey(max);
         int left = 1;
         for(int i=k, j=0; i<nums.length; i++) {
