4c54128b10b886fd52d51fd76f3257e0578817c0
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-04-15 23:31:27 +0530

    Time: 1451 ms (100.00%), Space: 51.3 MB (100.00%) - LeetHub

---
+++ 2642-design-graph-with-shortest-path-calculator/2642-design-graph-with-shortest-path-calculator.java
@@ -0,0 +1,42 @@
+class Graph {
+    int size;
+    int[][] shortest;
+    int max = Integer.MAX_VALUE;
+    
+    void floydWarshall() {
+        for(int k=0; k<size; k++) {
+            for(int i=0; i<size; i++) {
+                for(int j=0; j<size; j++) {
+                    if(i == j) shortest[i][j] = 0;
+                    if(shortest[i][k] != max && shortest[k][j] != max && shortest[i][k] + shortest[k][j] < shortest[i][j]) shortest[i][j] = shortest[i][k] + shortest[k][j];
+                }
+            }
+        }
+    }
+    
+    public Graph(int n, int[][] edges) {
+        size = n;
+        shortest = new int[n][n];
+        for(int i=0; i<n; i++) Arrays.fill(shortest[i], max);
+        for(int[] edge : edges) shortest[edge[0]][edge[1]] = edge[2];
+        floydWarshall();
+    }
+    
+    public void addEdge(int[] edge) {
+        if(edge[2] < shortest[edge[0]][edge[1]]) {
+            shortest[edge[0]][edge[1]] = edge[2];
+            floydWarshall();
+        }
+    }
+    
+    public int shortestPath(int node1, int node2) {
+        return shortest[node1][node2] == max ? -1 : shortest[node1][node2];
+    }
+}
+
+/**
+ * Your Graph object will be instantiated and called as such:
+ * Graph obj = new Graph(n, edges);
+ * obj.addEdge(edge);
+ * int param_2 = obj.shortestPath(node1,node2);
+ */
