55113d0a4ae508aa592f4322d58a6cb52ad779e5
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-04-16 07:48:01 +0530

    Time: 660 ms (100.00%), Space: 51 MB (100.00%) - LeetHub

--- 2642-design-graph-with-shortest-path-calculator/2642-design-graph-with-shortest-path-calculator.java
+++ 2642-design-graph-with-shortest-path-calculator/2642-design-graph-with-shortest-path-calculator.java
@@ -1,40 +1,41 @@
 class Graph {
-    int size;
-    int[][] shortest;
+    
+    Map<Integer, Map<Integer, Integer>> list;
+    int n;
     int max = Integer.MAX_VALUE;
     
-    void floydWarshall() {
-        for(int k=0; k<size; k++) {
-            for(int i=0; i<size; i++) {
-                for(int j=0; j<size; j++) {
-                    if(i == j) shortest[i][j] = 0;
-                    if(shortest[i][k] != max && shortest[k][j] != max) shortest[i][j] = Math.min(shortest[i][j], shortest[i][k] + shortest[k][j]);
-                }
-            }
-        }
-    }
-    
     public Graph(int n, int[][] edges) {
-        size = n;
-        shortest = new int[n][n];
-        for(int i=0; i<n; i++) Arrays.fill(shortest[i], max);
-        for(int[] edge : edges) shortest[edge[0]][edge[1]] = edge[2];
-        floydWarshall();
+        this.n = n;
+        list = new HashMap<>(n);
+        for(var edge : edges) {
+            list.computeIfAbsent(edge[0], a -> new HashMap<>()).put(edge[1], edge[2]);
+        }
     }
     
     public void addEdge(int[] edge) {
-        if(edge[2] < shortest[edge[0]][edge[1]]) {
-            shortest[edge[0]][edge[1]] = edge[2];
-            for(int i=0; i<size; i++) {
-                for(int j=0; j<size; j++) {
-                    if(shortest[i][edge[0]] != max && shortest[edge[1]][j] != max) shortest[i][j] = Math.min(shortest[i][j], shortest[i][edge[0]] + shortest[edge[1]][j] + edge[2]);
-                }
-            }
-        }
+        list.computeIfAbsent(edge[0], a -> new HashMap<>()).put(edge[1], edge[2]);
     }
     
     public int shortestPath(int node1, int node2) {
-        return shortest[node1][node2] == max ? -1 : shortest[node1][node2];
+        int[] dist = new int[n];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[node1] = 0;
+        var q = new PriorityQueue<Pair<Integer, Integer>>((a,b) -> Integer.compare(a.getValue(), b.getValue()));
+        q.add(new Pair(node1, 0));
+        while(!q.isEmpty()) {
+            int node = q.peek().getKey();
+            int cost = q.poll().getValue();
+            if(!list.containsKey(node)) continue;
+            for(var neighbours : list.get(node).entrySet()) {
+                int adjNode = neighbours.getKey();
+                int costToNeighbour = neighbours.getValue();
+                if(cost + costToNeighbour < dist[adjNode]) {
+                    dist[adjNode] = cost + costToNeighbour;
+                    q.add(new Pair(adjNode, dist[adjNode]));
+                }
+            }
+        }
+        return dist[node2] == max ? -1 : dist[node2];
     }
 }
 
