5eea7997323ccbea194ba445d38429ce9d3f50a4
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-04-19 01:28:06 +0530

    Time: 3 ms (98.36%), Space: 42.8 MB (77.42%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,28 +1,33 @@
 class Solution {
-    boolean check(int[] arr, int[] target) {
+    /*boolean check(int[] arr, int[] target) {
         for(int i=0; i<arr.length; i++) if(target[i] != 0 && arr[i] < target[i]) return false;
         return true;
-    }
+    }*/
     public String minWindow(String s, String t) {
         int left = 0;
         int right = 0;
         int[] targetArr = new int[58];
         for(char ch : t.toCharArray()) targetArr[ch-'A']++;
-        int[] sArr = new int[58];
+        //int[] sArr = new int[58];
         int ansLeft = 0;
         int ansRight = 0;
+        int rem = t.length();
         int ans = s.length() + 1;
         while(right < s.length()) {
             char ch = s.charAt(right++);
-            sArr[ch-'A']++;
-            if(check(sArr, targetArr)) {
-                while(left <= right && check(sArr, targetArr)) {
+            if(targetArr[ch-'A'] > 0) {
+                rem--;
+            }
+            targetArr[ch-'A']--;
+            if(rem == 0) {
+                while(left <= right && rem == 0) {
                     if(right - left < ans) {
                         ans = right - left + 1;
                         ansLeft = left;
                         ansRight = right;
                     }
-                    sArr[s.charAt(left++)-'A']--;
+                    targetArr[s.charAt(left)-'A']++;
+                    if(targetArr[s.charAt(left++)-'A'] > 0) rem += 1;
                 }
             }
         }
