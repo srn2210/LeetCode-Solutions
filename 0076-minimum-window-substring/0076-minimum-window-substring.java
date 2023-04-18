192b59f186d857ff30ca93d4a2b12c093b76f54d
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-04-19 01:31:49 +0530

    Time: 2 ms (100.00%), Space: 42.5 MB (87.38%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,33 +1,27 @@
 class Solution {
-    /*boolean check(int[] arr, int[] target) {
-        for(int i=0; i<arr.length; i++) if(target[i] != 0 && arr[i] < target[i]) return false;
-        return true;
-    }*/
     public String minWindow(String s, String t) {
         int left = 0;
         int right = 0;
         int[] targetArr = new int[58];
         for(char ch : t.toCharArray()) targetArr[ch-'A']++;
-        //int[] sArr = new int[58];
         int ansLeft = 0;
         int ansRight = 0;
         int rem = t.length();
         int ans = s.length() + 1;
         while(right < s.length()) {
             char ch = s.charAt(right++);
-            if(targetArr[ch-'A'] > 0) {
-                rem--;
-            }
+            if(targetArr[ch-'A'] > 0) rem--;
             targetArr[ch-'A']--;
             if(rem == 0) {
                 while(left <= right && rem == 0) {
-                    if(right - left < ans) {
+                    char c = s.charAt(left++);
+                    if(right - left + 1 < ans) {
                         ans = right - left + 1;
-                        ansLeft = left;
+                        ansLeft = left-1;
                         ansRight = right;
                     }
-                    targetArr[s.charAt(left)-'A']++;
-                    if(targetArr[s.charAt(left++)-'A'] > 0) rem += 1;
+                    targetArr[c-'A']++;
+                    if(targetArr[c-'A'] > 0) rem += 1;
                 }
             }
         }
