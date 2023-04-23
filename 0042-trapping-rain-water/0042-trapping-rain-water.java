4e4bfa546542e62901a81eff62a7a4ad6333a3c2
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-04-23 06:02:40 +0530

    Time: 4 ms (14.86%), Space: 42.7 MB (94.71%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -3,7 +3,7 @@ class Solution {
         var st = new LinkedList<Integer>();
         int ans = 0;
         for(int i=0; i<height.length; i++) {
-            while(!st.isEmpty() && height[i] > height[st.peekLast()]) {
+            while(!st.isEmpty() && height[i] >= height[st.peekLast()]) {
                 int prev = st.removeLast();
                 if(st.isEmpty()) continue;
                 ans += (i - st.peekLast() - 1) * (Math.min(height[i], height[st.peekLast()]) - height[prev]);
