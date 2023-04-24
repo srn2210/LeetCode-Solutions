60db80e71f5609a13cf4fe2edbcc9f3744dd8c93
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-04-24 10:29:10 +0530

    Time: 4 ms (99.00%), Space: 49 MB (31.50%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.java
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.java
@@ -1,14 +1,27 @@
 class Solution {
-    int solve(int[] nums, int idx, int rem, int[][] dp) {
-        if(idx == nums.length) return rem == 0 ? 0 : -1000000000;
-        if(dp[idx][rem] != -1) return dp[idx][rem];
-        int t1 = nums[idx] + solve(nums, idx + 1, (rem + nums[idx]) % 3, dp);
-        int t2 = solve(nums, idx + 1, rem, dp);
-        return dp[idx][rem] = Math.max(t1,t2);
-    }
     public int maxSumDivThree(int[] nums) {
-        var dp = new int[nums.length][3];
-        for(int i=0; i<nums.length; i++) Arrays.fill(dp[i], -1);
-        return solve(nums, 0, 0, dp);
+        int sum = 0;
+        int max = (int)1e4+1;
+        int r11 = max, r12 = max, r21 = max, r22 = max;
+        for(int i : nums) {
+            sum += i;
+            if(i % 3 == 1) {
+                if(i <= r11) {
+                    r12 = r11;
+                    r11 = i;
+                }
+                else if(i < r12) r12 = i;
+            }
+            if(i % 3 == 2) {
+                if(i <= r21) {
+                    r22 = r21;
+                    r21 = i;
+                }
+                else if(i < r22) r22 = i;
+            }
+        }
+        if(sum % 3 == 0) return sum;
+        else if(sum % 3 == 1) return sum - Math.min(r11, r21+r22);
+        else return sum - Math.min(r11+r12, r21);
     }
 }
