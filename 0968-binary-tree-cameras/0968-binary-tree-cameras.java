a858df221e9ef38f4f150e25346ed68baaaa2a56
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-04-25 06:27:00 +0530

    Time: 5 ms (17.96%), Space: 41.8 MB (98.15%) - LeetHub

---
+++ 0968-binary-tree-cameras/0968-binary-tree-cameras.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int solve(TreeNode node, int status, Map<TreeNode, int[]> dp) {
+        if(node == null) return 0;
+        if(dp.containsKey(node) && dp.get(node)[status] != -1) return dp.get(node)[status];
+        if(status == 1) {
+            int ans = solve(node.left, 2, dp) + solve(node.right, 2, dp);
+            dp.computeIfAbsent(node, a -> new int[]{-1, -1, -1, -1})[status] = ans;
+            return ans;
+        }
+        else if(status == 2) {
+            int setCam = 1 + solve(node.left, 2, dp) + solve(node.right, 2, dp);
+            int noCam = solve(node.left, 3, dp) + solve(node.right, 3, dp);
+            int ans = Math.min(setCam, noCam);
+            dp.computeIfAbsent(node, a -> new int[]{-1, -1, -1, -1})[status] = ans;
+            return ans;
+        }
+        else {
+            int setCam = 1 + solve(node.left, 2, dp) + solve(node.right, 2, dp);
+            int leftCam = node.left == null ? 10001 : 1 + solve(node.left, 1, dp) + solve(node.right, 3, dp);
+            int rightCam = node.right == null ? 10001 : 1 + solve(node.left, 3, dp) + solve(node.right, 1, dp);
+            int ans = Math.min(setCam, Math.min(leftCam, rightCam));
+            dp.computeIfAbsent(node, a -> new int[]{-1, -1, -1, -1})[status] = ans;
+            return ans;
+        }
+    }
+    public int minCameraCover(TreeNode root) {
+        var dp = new HashMap<TreeNode, int[]>();
+        return solve(root, 3, dp);
+    }
+}
