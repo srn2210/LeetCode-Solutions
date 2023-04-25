a1e7963c14b3c0ca9b92952687935f9cdcf3967d
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-04-25 06:53:44 +0530

    Time: 5 ms (17.96%), Space: 42.4 MB (47.78%) - LeetHub

--- 0968-binary-tree-cameras/0968-binary-tree-cameras.java
+++ 0968-binary-tree-cameras/0968-binary-tree-cameras.java
@@ -14,35 +14,38 @@
  * }
  */
 class Solution {
+    // status 1 == hasCameraAtThatNode
+    // status 2 == doesNotHaveCamButBeingMonitored
+    // status 3 == noCamAndNotMonitored
     int solve(TreeNode node, int status, Map<TreeNode, int[]> dp) {
         if(node == null) {
-            if(status == 1) return 1001;
+            if(status == 1) return 1001;                // cannot set camera on a non existant node, so return big number
             return 0;
         }
-        if(dp.containsKey(node) && dp.get(node)[status] != -1) return dp.get(node)[status];
-        if(status == 1) {
+        if(dp.containsKey(node) && dp.get(node)[status] != -1) return dp.get(node)[status];         // memo
+        if(status == 1) {               // has a cam, so its children will be monitored
             int ans = solve(node.left, 2, dp) + solve(node.right, 2, dp);
             dp.computeIfAbsent(node, a -> new int[]{-1, -1, -1, -1})[status] = ans;
             return ans;
         }
-        else if(status == 2) {
-            int setCam = 1 + solve(node.left, 2, dp) + solve(node.right, 2, dp);
-            int noCam = solve(node.left, 3, dp) + solve(node.right, 3, dp);
+        else if(status == 2) {          // is being monitored, so two choices put cam on this node or not
+            int setCam = 1 + solve(node.left, 2, dp) + solve(node.right, 2, dp);        // set cam on the node, update child to beingMonitored
+            int noCam = solve(node.left, 3, dp) + solve(node.right, 3, dp);             // No cam on the node, child will be unMonitored
             int ans = Math.min(setCam, noCam);
             dp.computeIfAbsent(node, a -> new int[]{-1, -1, -1, -1})[status] = ans;
             return ans;
         }
-        else {
-            int setCam = 1 + solve(node.left, 2, dp) + solve(node.right, 2, dp);
-            int leftCam = 1 + solve(node.left, 1, dp) + solve(node.right, 3, dp);
-            int rightCam = 1 + solve(node.left, 3, dp) + solve(node.right, 1, dp);
+        else {                          // unMonitored, we can set cam here, or set a cam to the left or right child
+            int setCam = 1 + solve(node.left, 2, dp) + solve(node.right, 2, dp);        // set cam here same as previous
+            int leftCam = 1 + solve(node.left, 1, dp) + solve(node.right, 3, dp);       // set cam at left child, right child unMonitored
+            int rightCam = 1 + solve(node.left, 3, dp) + solve(node.right, 1, dp);      // set cam at right child, left child unMonitored
             int ans = Math.min(setCam, Math.min(leftCam, rightCam));
             dp.computeIfAbsent(node, a -> new int[]{-1, -1, -1, -1})[status] = ans;
             return ans;
         }
     }
     public int minCameraCover(TreeNode root) {
-        var dp = new HashMap<TreeNode, int[]>();
+        var dp = new HashMap<TreeNode, int[]>();            // cache soln for each node for each status through 1,2,3
         return solve(root, 3, dp);
     }
 }
