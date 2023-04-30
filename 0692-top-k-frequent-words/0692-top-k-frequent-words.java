03fa8a5341e18e7dee5e96b7f29016819c38f0c2
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-05-01 03:33:18 +0530

    Time: 7 ms (33.66%), Space: 43.2 MB (10.40%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.java
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -1,16 +1,14 @@
 class Solution {
     public List<String> topKFrequent(String[] words, int k) {
         var map = new HashMap<String, Integer>();
-        for(var s : words) {
-            map.put(s, map.getOrDefault(s, 0)+1);
-        }
-        var pq = new PriorityQueue<Pair<String, Integer>>((a,b) -> a.getValue() == b.getValue() ? b.getKey().compareTo(a.getKey()) : Integer.compare(a.getValue(), b.getValue()));
+        for(var s : words) map.put(s, map.getOrDefault(s, 0)+1);
+        var pq = new PriorityQueue<String>((a,b) -> map.get(a) == map.get(b) ? b.compareTo(a) : Integer.compare(map.get(a), map.get(b)));
         for(var entry : map.entrySet()) {
-            pq.add(new Pair(entry.getKey(), entry.getValue()));
+            pq.add(entry.getKey());
             if(pq.size() > k) pq.poll();
         }
         var ans = new LinkedList<String>();
-        while(!pq.isEmpty()) ans.addFirst(pq.poll().getKey());
+        while(!pq.isEmpty()) ans.addFirst(pq.poll());
         return ans;
     }
 }
