137a41b44ed7f650b3632c65d19f1cd51167afff
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-05-01 11:13:20 +0530

    Time: 51 ms (89.03%), Space: 50.3 MB (16.32%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -7,34 +7,52 @@ class Solution {
         }
         return ans==1;
     }
+    List<String> generate(String s) {
+        var ans = new ArrayList<String>();
+        for(int i=0; i<s.length(); i++) {
+            ans.add(s.substring(0,i) + "*" + s.substring(i+1));
+        }
+        return ans;
+    }
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         var map = new HashMap<String, List<String>>();
         for(int i=0; i<wordList.size(); i++) {
-            for(int j=i+1; j<wordList.size(); j++) {
-                if(diff(wordList.get(i), wordList.get(j))) {
-                    map.computeIfAbsent(wordList.get(i), a -> new ArrayList<>()).add(wordList.get(j));
-                    map.computeIfAbsent(wordList.get(j), a -> new ArrayList<>()).add(wordList.get(i));
-                }
-            }
+            //for(int j=i+1; j<wordList.size(); j++) {
+                //if(diff(wordList.get(i), wordList.get(j))) {
+            List<String> temp = generate(wordList.get(i));
+            for(var s : temp) map.computeIfAbsent(s, a -> new ArrayList<>()).add(wordList.get(i));
+                    //map.computeIfAbsent(wordList.get(i), a -> new ArrayList<>()).add(wordList.get(j));
+                    //map.computeIfAbsent(wordList.get(j), a -> new ArrayList<>()).add(wordList.get(i));
+                //}
+            //}
         }
-        var queue = new LinkedList<String>();
-        var vis = new HashSet<String>();
-        for(int i=0; i<wordList.size(); i++) if(diff(beginWord, wordList.get(i))) {
+        //var queue = new LinkedList<String>();
+        
+        /*for(int i=0; i<wordList.size(); i++) if(diff(beginWord, wordList.get(i))) {
             queue.add(wordList.get(i));
             vis.add(wordList.get(i));
-        }
-        int ans = 2;
+        }*/
+        var queue = new LinkedList<String>();
+        var vis = new HashSet<String>();
+        queue.add(beginWord);
+        vis.add(beginWord);
+        //var ansCands = new ArrayList<>(generate(endWord));
+        int ans = 1;
+        //System.out.println(map);
         while(!queue.isEmpty()) {
             int size = queue.size();
             while(size-- > 0) {
                 var t = queue.poll();
-                if(t.equals(endWord)) return ans;
-                if(!map.containsKey(t)) continue;
-                for(var s : map.get(t)) {
-                    if(vis.contains(s)) continue;
-                    else {
-                        queue.add(s);
-                        vis.add(s);
+                //var candidates = generate(t);
+                if(endWord.equals(t)) return ans;
+                for(var candidate : generate(t)) {
+                    if(!map.containsKey(candidate)) continue;
+                    for(var s : map.get(candidate)) {
+                        if(vis.contains(s)) continue;
+                        else {
+                            queue.add(s);
+                            vis.add(s);
+                        }
                     }
                 }
             }
