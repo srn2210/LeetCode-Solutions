250a81307637140ca705542bb70b4a31da36acee
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-05-01 10:41:06 +0530

    Time: 680 ms (17.56%), Space: 43.6 MB (87.73%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,66 +1,13 @@
 class Solution {
-    /*class TrieNode {
-        TrieNode[] arr;
-        boolean isWord;
-        TrieNode() {
-            arr = new TrieNode[26];
-            isWord = false;
-        }
-        void setEnd() {
-            this.isWord = true;
-        }
-        boolean isEnd() {
-            return this.isWord;
-        }
-    }
-    class Trie {
-        TrieNode root;
-        Trie() {
-            root = new TrieNode();
-        }
-        void insert(String s) {
-            var node = root;
-            for(char ch : s.toCharArray()) {
-                if(node.arr[ch-'a'] == null) node.arr[ch-'a'] = new TrieNode();
-                node = node.arr[ch-'a'];
-            }
-            node.setEnd();
-        }
-        boolean wordExists(String s) {
-            var node = root;
-            for(char ch : s.toCharArray()) {
-                if(node.arr[ch-'a'] == null) return false;
-                node = node.arr[ch-'a'];
-            }
-            return node.isEnd();
-        }
-        TrieNode getNode() {
-            var node = root;
-            return node;
-        }
-    }
-    int solve(TrieNode node, char[] given, char[] end, int idx) {
-        //if(idx == end.length()) return node.isEnd() ? 0 : 1000001;
-        for(int i=0; i<given.length; i++) {
-            if(given[i] != end[i]) break;
-            if(i == given.length-1) return 0;
-        }
-        int ans = 0;
-    }*/
     boolean diff(String s1, String s2) {
         int ans = 0;
         for(int i=0; i<s1.length(); i++) {
             if(s1.charAt(i) != s2.charAt(i)) ans++;
+            if(ans > 1) return false;
         }
         return ans==1;
     }
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        /*Trie obj = new Trie();
-        for(var s : wordList) {
-            obj.insert(s);
-        }
-        if(!obj.wordExists(endWord)) return 0;
-        return solve(obj.getNode(), beginWord.toCharArray(), endWord.toCharArray(), 0);*/
         var map = new HashMap<String, List<String>>();
         for(int i=0; i<wordList.size(); i++) {
             for(int j=i+1; j<wordList.size(); j++) {
@@ -70,18 +17,15 @@ class Solution {
                 }
             }
         }
-        //System.out.println(map);
         var queue = new LinkedList<String>();
         var vis = new HashSet<String>();
         for(int i=0; i<wordList.size(); i++) if(diff(beginWord, wordList.get(i))) {
             queue.add(wordList.get(i));
             vis.add(wordList.get(i));
         }
-        //System.out.println(queue);
         int ans = 2;
         while(!queue.isEmpty()) {
             int size = queue.size();
-            //System.out.println(queue);
             while(size-- > 0) {
                 var t = queue.poll();
                 if(t.equals(endWord)) return ans;
