b51bc3976a32822a29f80028f1c2c0ca706e20c8
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-05-02 06:57:23 +0530

    Time: 3 ms (96.08%), Space: 43.1 MB (27.19%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.java
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -33,38 +33,9 @@ class Solution {
                     curr = curr * 10 + Character.getNumericValue(ch);
             }
         }
-        return res;
+        return res + (curr * sign);
     }
     public int calculate(String s) {
-        int sign = 1;
-        int curr = 0;
-        int res = 0;
-        for(int i=0; i<s.length(); i++) {
-            char ch = s.charAt(i);
-            switch(ch) {
-                case '+':
-                    res += sign * curr;
-                    curr = 0;
-                    sign = 1;
-                    break;
-                case '-':
-                    res += sign * curr;
-                    curr = 0;
-                    sign = -1;
-                    break;
-                case '(':
-                    int t = evaluate(s, i+1) * sign;
-                    res += t;
-                    i = j;
-                    curr = 0;
-                    sign = 1;
-                    break;
-                case ' ':
-                    break;
-                default:
-                    curr = curr * 10 + Character.getNumericValue(ch);
-            }
-        }
-        return res + (curr * sign);
+        return evaluate(s, 0);
     }
 }
