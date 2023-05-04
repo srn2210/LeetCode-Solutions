24ba198fee7afd1be7e2750c47fa225e8dd7df7e
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-05-04 22:20:56 +0530

    Time: 1 ms (69.95%), Space: 39.5 MB (46.45%) - LeetHub

--- 1201-ugly-number-iii/1201-ugly-number-iii.java
+++ 1201-ugly-number-iii/1201-ugly-number-iii.java
@@ -16,8 +16,7 @@ class Solution {
         int lo = 1, hi = (int)2e9;
         while(lo < hi) {
             int mid = lo + (hi - lo) / 2;
-            int ans = check(mid, n, a, b, c);
-            if(ans >= n) hi = mid;
+            if(check(mid, n, a, b, c) >= n) hi = mid;
             else lo = mid + 1;
         }
         return lo;
