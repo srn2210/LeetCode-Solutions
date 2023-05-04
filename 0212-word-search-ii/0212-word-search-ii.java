6261a575e59a95892e046ef425054d6591691f44
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-05-05 04:36:14 +0530

    Time: 199 ms (62.25%), Space: 43.2 MB (49.51%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.java
+++ 0212-word-search-ii/0212-word-search-ii.java
@@ -1,20 +1,22 @@
 class Solution {
     class TrieNode {
         TrieNode[] arr;
-        boolean isWord;
+        //boolean isWord;
+        String word;
         TrieNode() {
             arr = new TrieNode[26];
-            isWord = false;
+            //isWord = false;
+            word = null;
         }
         TrieNode get(char ch) {
             return arr[ch-'a'];
         }
-        void setEnd() {
+        /*void setEnd() {
             isWord = true;
         }
         boolean isEnd() {
             return isWord;
-        }
+        }*/
         void create(char ch) {
             arr[ch-'a'] = new TrieNode();
         }
@@ -30,33 +32,36 @@ class Solution {
                 if(node.get(ch) == null) node.create(ch);
                 node = node.get(ch);
             }
-            node.setEnd();
+            node.word = s;
         }
     }
-    void explore(char[][] board, int i, int j, TrieNode node, boolean[][] vis, Set<String> ans, StringBuilder s) {
+    void explore(char[][] board, int i, int j, TrieNode node, boolean[][] vis, List<String> ans, StringBuilder s) {
         if(i < 0 || j < 0 || i >= board.length || j >= board[0].length || node == null || vis[i][j]) return;
         vis[i][j] = true;
-        s.append(board[i][j]);
-        if(node.isEnd()) ans.add(s.toString());
+        //s.append(board[i][j]);
+        if(node.word != null) {
+            ans.add(node.word);
+            node.word = null;
+        }
         if(i+1 < board.length) explore(board, i+1, j, node.get(board[i+1][j]), vis, ans, s);
         if(j+1 < board[0].length) explore(board, i, j+1, node.get(board[i][j+1]), vis, ans, s);
         if(i-1 >= 0) explore(board, i-1, j, node.get(board[i-1][j]), vis, ans, s);
         if(j-1 >= 0) explore(board, i, j-1, node.get(board[i][j-1]), vis, ans, s);
         vis[i][j] = false;
-        s.deleteCharAt(s.length()-1);
+        //s.deleteCharAt(s.length()-1);
     }
     public List<String> findWords(char[][] board, String[] words) {
         Trie t = new Trie();
         for(String s : words) t.insert(s);
         int m = board.length, n = board[0].length;
         boolean[][] vis = new boolean[m][n];
-        var ans = new HashSet<String>();
+        var ans = new ArrayList<String>();
         var word = new StringBuilder();
         for(int i=0; i<m; i++) {
             for(int j=0; j<n; j++) {
                 if(t.root.get(board[i][j]) != null) explore(board, i, j, t.root.get(board[i][j]), vis, ans, word);
             }
         }
-        return new ArrayList<String>(ans);
+        return ans;
     }
 }
