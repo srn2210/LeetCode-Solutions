c5ee59e193e4e0ed450a460e1a4753865fc7b2b4
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-05-04 22:20:34 +0530

    Time: 1 ms (69.95%), Space: 39.3 MB (77.05%) - LeetHub

--- 1201-ugly-number-iii/1201-ugly-number-iii.java
+++ 1201-ugly-number-iii/1201-ugly-number-iii.java
@@ -4,27 +4,12 @@ class Solution {
         return gcd(b % a, a);
     }
     long lcm(long x, long y) {
-        /*long lo = Math.min(x, Math.min(y, z)), hi = x * y * z;
-        //System.out.println(lo + " - " + hi);
-        long ans = 1;
-        while(lo <= hi) {
-            long mid = lo + (hi - lo) / 2;
-            if(mid % x == 0 && mid % y == 0 && mid % z == 0) {
-                ans = mid;
-                hi = mid - 1;
-            }
-            else lo = mid + 1;
-        }
-        return ans;*/
         long ans = gcd(x, y);
-        //System.out.println(ans);
         return (x * y) / ans;
     }
     int check(int mid, int n, int a, int b, int c) {
         long ab = lcm(a, b), bc = lcm(b, c), ca = lcm(a, c), abc = lcm(a, lcm(b, c));
         long ans = (mid / a) + (mid / b) + (mid / c) - (mid / ab) - (mid / bc) - (mid / ca) + (mid / abc);
-        //System.out.println(ab + " - " + bc + " - " + ca + " - " + abc);
-        //System.out.println(ans);
         return (int)ans;
     }
     public int nthUglyNumber(int n, int a, int b, int c) {
@@ -32,7 +17,6 @@ class Solution {
         while(lo < hi) {
             int mid = lo + (hi - lo) / 2;
             int ans = check(mid, n, a, b, c);
-            //if(ans == n) return mid;
             if(ans >= n) hi = mid;
             else lo = mid + 1;
         }
