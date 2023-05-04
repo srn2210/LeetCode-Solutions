e26814820043d6ceae403b8aef15b051c8961ab3
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-05-04 13:45:26 +0530

    Time: 11 ms (71.59%), Space: 43.4 MB (22.16%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.java
+++ 0649-dota2-senate/0649-dota2-senate.java
@@ -1,52 +1,19 @@
 class Solution {
     public String predictPartyVictory(String senate) {
-        var arr = senate.toCharArray();
+        int n = senate.length();
         var r = new LinkedList<Integer>();
         var d = new LinkedList<Integer>();
-        var rp = new LinkedList<Integer>();
-        var dp = new LinkedList<Integer>();
-        var lhmap = new LinkedHashMap<Integer, Character>();
-        var set = new HashSet<Integer>();
-        for(int i=0; i<arr.length; i++) {
-            if(arr[i] == 'R') r.add(i);
+        for(int i=0; i<n; i++) {
+            char ch = senate.charAt(i);
+            if(ch == 'R') r.add(i);
             else d.add(i);
-            lhmap.put(i, arr[i]);
         }
-        while(!lhmap.isEmpty()) {
-            for(var entry : lhmap.entrySet()) {
-                int key = entry.getKey();
-                int val = entry.getValue();
-                if(set.contains(key)) continue;
-                else {
-                    switch(val) {
-                        case 'R':
-                            if(d.isEmpty() && dp.isEmpty()) return "Radiant";
-                            while(!d.isEmpty() && d.peek() < key) dp.addLast(d.pollFirst());
-                            if(!d.isEmpty()) set.add(d.pollFirst());
-                            else set.add(dp.pollFirst());
-                            break;
-                        case 'D':
-                            if(r.isEmpty() && rp.isEmpty()) return "Dire";
-                            while(!r.isEmpty() && r.peek() < key) rp.addLast(r.pollFirst());
-                            if(!r.isEmpty()) set.add(r.pollFirst());
-                            else set.add(rp.pollFirst());
-                            break;
-                    }
-                }
-            }
-            for(int i : set) {
-                lhmap.remove(i);
-            }
-            set.clear();
-            while(!r.isEmpty()) rp.addLast(r.pollFirst());
-            while(!d.isEmpty()) dp.addLast(d.pollFirst());
-            var t = r;
-            r = rp;
-            rp = t;
-            t = d;
-            d = dp;
-            dp = t;
+        while(!r.isEmpty() && !d.isEmpty()) {
+            int rPop = r.pollFirst();
+            int dPop = d.pollFirst();
+            if(rPop < dPop) r.add(rPop + n);
+            else d.add(dPop + n);
         }
-        return null;
+        return r.isEmpty() ? "Dire" : "Radiant";
     }
 }
