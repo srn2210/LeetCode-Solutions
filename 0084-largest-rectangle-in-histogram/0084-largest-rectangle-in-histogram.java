be192e1561ed07c5891c77fb741df1c40e57bac8
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-05-05 13:41:22 +0530

    Time: 34 ms (81.72%), Space: 54.7 MB (73.94%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,27 +1,26 @@
 class Solution {
+    int ans;
+    void updateStack(Deque<Integer> st, int[] heights, int num) {
+        int og = st.peekLast();
+        while(!st.isEmpty() && heights[st.peekLast()] > num) {
+            int t = st.removeLast();
+            if(!st.isEmpty()) ans = Math.max(ans, heights[t] * (og - st.peekLast()));
+            else ans = Math.max(ans, heights[t] * (og + 1));
+                    
+        }
+    }
     public int largestRectangleArea(int[] heights) {
-        int ans = 0, min = Integer.MAX_VALUE;
+        ans = 0;
         var st = new ArrayDeque<Integer>();
         
         for(int i=0; i<heights.length; i++) {
             if(st.isEmpty() || heights[st.peekLast()] <= heights[i]) st.addLast(i);
             else {
-                int og = st.peekLast();
-                while(!st.isEmpty() && heights[st.peekLast()] > heights[i]) {
-                    int t = st.removeLast();
-                    if(!st.isEmpty()) ans = Math.max(ans, heights[t] * (og - st.peekLast()));
-                    else ans = Math.max(ans, heights[t] * (og + 1));
-                    
-                }
+                updateStack(st, heights, heights[i]);
                 st.addLast(i);
             }
         }
-        int og = st.peekLast();
-        while(!st.isEmpty()) {
-            int t = st.removeLast();
-            if(!st.isEmpty()) ans = Math.max(ans, heights[t] * (og - st.peekLast()));
-            else ans = Math.max(ans, heights[t] * (og + 1));
-        }
+        updateStack(st, heights, 0);
         
         return ans;
     }
