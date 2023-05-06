ed06f5618bf66e451f6ccb8dcb3c57ca0f3c963a
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-05-06 06:58:06 +0530

    Time: 65 ms (19.06%) | Memory: 51.6 MB (23.29%) - LeetSync

--- number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.java
+++ number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.java
@@ -1,4 +1,3 @@
-import java.math.*;
 class Solution {
     int binarySearch(int[] nums, int rem) {
         int lo = 0, hi = nums.length-1;
@@ -13,27 +12,27 @@ class Solution {
         }
         return ans;
     }
-    int modPow(int base, int exp, int mod) {
+    int modPow(int base, int exp, int mod, int[] cache) {
         if(exp == 0) return 1;
+        if(cache[exp] != -1) return cache[exp];
         long res = 1;
         if(exp % 2 != 0) res *= base;
-        int t = modPow(base, exp/2, mod) % mod;
+        int t = modPow(base, exp/2, mod, cache) % mod;
         res = (res%mod) * t * t;
-        return (int)(res % mod);
+        return cache[exp] = (int)(res % mod);
     }
     public int numSubseq(int[] nums, int target) {
        Arrays.sort(nums);
        int mod = (int)1e9+7;
        int ans = 0;
+       int[] cache = new int[nums.length+1];
+       Arrays.fill(cache, -1);
        for(int i=0; i<nums.length; i++) {
            int rem = target - nums[i];
            if(rem < nums[i]) continue;
            int idx = binarySearch(nums, rem);
-           ans %= (int)1e9+7;
-           //ans += t.modPow(new BigInteger(String.valueOf(idx-i)), mod).intValue();
-           ans += modPow(2, idx-i, mod);
-           //System.out.println(modPow(2, 31, mod));
-           ans %= (int)1e9+7;
+           ans += modPow(2, idx-i, mod, cache);
+           ans %= mod;
        }
        return ans;
     }
