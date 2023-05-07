d206af531c4d90c436552610f7a42abbda8cffd9
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-05-08 05:19:37 +0530

    Time: 43 ms (23.84%) | Memory: 51.4 MB (52.11%) - LeetSync

--- number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.java
+++ number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.java
@@ -9,19 +9,19 @@ class Solution {
         return cache[exp] = (int)(res % mod);
     }
     public int numSubseq(int[] nums, int target) {
-       Arrays.sort(nums);
-       int mod = (int)1e9+7, ans = 0;
-       int[] cache = new int[nums.length+1];
-       Arrays.fill(cache, -1);
-       int left = 0, right = nums.length-1;
-       while(left <= right) {
-           if(nums[left] + nums[right] > target) right--;
-           else {
-               ans += modPow(2, right-left, mod, cache);
-               ans %= mod;
-               left++;
-           }
-       }
-       return ans;
+        Arrays.sort(nums);
+        int mod = (int)1e9+7, ans = 0;
+        int[] cache = new int[nums.length+1];
+        Arrays.fill(cache, -1);
+        int left = 0, right = nums.length-1;
+        while(left <= right) {
+            if(nums[left] + nums[right] > target) right--;
+            else {
+                ans += modPow(2, right-left, mod, cache);
+                ans %= mod;
+                left++;
+            }
+        }
+        return ans;
     }
 }
