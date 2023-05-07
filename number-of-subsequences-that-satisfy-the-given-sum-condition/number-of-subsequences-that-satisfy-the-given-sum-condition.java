be4dab47cb21cd37f834ff92a68a92322a9d1b39
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-05-08 05:15:38 +0530

    Time: 42 ms (24.17%) | Memory: 51.4 MB (39.68%) - LeetSync

--- number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.java
+++ number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.java
@@ -1,17 +1,4 @@
 class Solution {
-    int binarySearch(int[] nums, int rem) {
-        int lo = 0, hi = nums.length-1;
-        int ans = 0;
-        while(lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
-            if(nums[mid] <= rem) {
-                ans = mid;
-                lo = mid + 1;
-            }
-            else hi = mid - 1;
-        }
-        return ans;
-    }
     int modPow(int base, int exp, int mod, int[] cache) {
         if(exp == 0) return 1;
         if(cache[exp] != -1) return cache[exp];
@@ -23,16 +10,17 @@ class Solution {
     }
     public int numSubseq(int[] nums, int target) {
        Arrays.sort(nums);
-       int mod = (int)1e9+7;
-       int ans = 0;
+       int mod = (int)1e9+7, ans = 0;
        int[] cache = new int[nums.length+1];
        Arrays.fill(cache, -1);
-       for(int i=0; i<nums.length; i++) {
-           int rem = target - nums[i];
-           if(rem < nums[i]) break;
-           int idx = binarySearch(nums, rem);
-           ans += modPow(2, idx-i, mod, cache);
-           ans %= mod;
+       int left = 0, right = nums.length-1;
+       while(left <= right) {
+           if(nums[left] + nums[right] > target) right--;
+           else {
+               ans += modPow(2, right-left, mod, cache);
+               ans %= mod;
+               left++;
+           }
        }
        return ans;
     }
