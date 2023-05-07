1e6e6a33a82091f2f905b36c2b820a444fbb9d30
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-05-08 04:41:08 +0530

    Time: 125 ms (14.93%) | Memory: 59.3 MB (23.88%) - LeetSync

--- find-the-longest-valid-obstacle-course-at-each-position/find-the-longest-valid-obstacle-course-at-each-position.java
+++ find-the-longest-valid-obstacle-course-at-each-position/find-the-longest-valid-obstacle-course-at-each-position.java
@@ -1,21 +1,25 @@
 class Solution {
-    int binarySearch(int[] arr, int height, int hi) {
-        int lo = 0;
+    int binarySearch(List<Integer> arr, int height) {
+        int lo = 0, hi = arr.size();
         while(lo < hi) {
             int mid = lo + (hi - lo) / 2;
-            if(arr[mid] > height) hi = mid;
+            if(arr.get(mid) > height) hi = mid;
             else lo = mid + 1;
         }
         return lo;
     }
     public int[] longestObstacleCourseAtEachPosition(int[] obstacles) {
-        int[] ans = new int[obstacles.length], lis = new int[obstacles.length+1];
-        int hi = 0;
+        int[] ans = new int[obstacles.length];
+        var lis = new ArrayList<Integer>();
         for(int i=0; i<ans.length; i++) {
             int height = obstacles[i];
-            int idx = binarySearch(lis, height, hi);
-            if(idx == hi) hi++;
-            lis[idx] = height;
+            int idx = 0;
+            if(lis.isEmpty()) lis.add(height);
+            else {
+                idx = binarySearch(lis, height);
+                if(idx == lis.size()) lis.add(height);
+                else lis.set(idx, height);
+            }
             ans[i] = idx+1;
         }
         return ans;
