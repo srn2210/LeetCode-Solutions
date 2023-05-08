f8a78945c66899ada1efdc86bca0f3a052a2b1a4
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-05-08 12:21:25 +0530

    Time: 356 ms (16.30%) | Memory: 49.1 MB (7.18%) - LeetSync

--- find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.java
+++ find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.java
@@ -1,18 +1,6 @@
 class Solution {
     public int smallestDistancePair(int[] nums, int k) {
         Arrays.sort(nums);
-        /*int n = nums.length, init = n-1, idx = 0;
-        while(init < k) {
-            idx++;
-            k -= init;
-            init = n - idx - 1;
-        }
-        System.out.println(Arrays.toString(nums));
-        return Math.abs(nums[idx] - nums[idx+k]);*/
-        /*for(int i=0; i<nums.length-1; i++) {
-            binarySearch(i+1, nums.length-1, nums, )
-        }*/
-        //k = 2 * k;
         int[] diff = new int[1000002];
         for(int i=0; i<nums.length; i++) {
             for(int j=i+1; j<nums.length; j++) {
