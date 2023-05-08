b5f93e43280717eb3c3df059f7b054be69c1e968
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-05-08 14:00:06 +0530

    Time: 6 ms (96.96%) | Memory: 42.5 MB (92.27%) - LeetSync

--- find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.java
+++ find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.java
@@ -5,11 +5,13 @@ class Solution {
         while(lo < hi) {
             int mid = lo + (hi - lo) / 2;
             int count = 0;
-            for(int i=0; i<nums.length-1; i++) {
-                for(int j=i+1; j<nums.length; j++) {
-                    if(Math.abs(nums[i] - nums[j]) > mid) break;
-                    count++;
+            int left = 0, right = 1;
+            while(right < nums.length) {
+                if(nums[right] - nums[left] <= mid) {
+                    count += right - left;
+                    right++;
                 }
+                else left++;
             }
             if(count >= k) hi = mid;
             else lo = mid + 1;
