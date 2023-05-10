ed36ba9fb33f8a59e0cf39ebad75237ac9b04302
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-05-10 21:01:32 +0530

    Time: 5 ms (98.52%) | Memory: 50.9 MB (74.55%) - LeetSync

--- find-eventual-safe-states/find-eventual-safe-states.java
+++ find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,29 +1,35 @@
 class Solution {
+    boolean dfs(int node, int n, int[][] graph, boolean[] vis, boolean[] terminal) {
+        if(graph[node].length == 0) {
+            if(terminal[node]) {
+                return true;
+            }
+            return false;
+        }
+        vis[node] = true;
+        boolean ans = true;
+        for(int edge : graph[node]) {
+            if(!vis[edge]) {
+                ans = ans && dfs(edge, n, graph, vis, terminal);
+            }
+            else if(vis[edge] && !terminal[edge]) return terminal[node] = false;
+        }
+        return terminal[node] = ans;
+    }
     public List<Integer> eventualSafeNodes(int[][] graph) {
         int n = graph.length;
-        var list = new ArrayList<Set<Integer>>();
-        var rlist = new ArrayList<Set<Integer>>();
-        var queue = new LinkedList<Integer>();
-        for(int i=0; i<n; i++) {
-            list.add(new HashSet<>());
-            rlist.add(new HashSet<>());
-        }
-        for(int i=0; i<n; i++) {
-            if(graph[i].length == 0) queue.add(i);
-            for(int j=0; j<graph[i].length; j++) {
-                rlist.get(graph[i][j]).add(i);
-                list.get(i).add(graph[i][j]);
-            }
-        }
-        while(!queue.isEmpty()) {
-            int x = queue.poll();
-            for(int edge : rlist.get(x)) {
-                list.get(edge).remove(x);
-                if(list.get(edge).isEmpty()) queue.add(edge);
-            }
-        }
+        var set = new boolean[n];
         var ans = new ArrayList<Integer>();
-        for(int i=0; i<n; i++) if(list.get(i).isEmpty()) ans.add(i);
+        var vis = new boolean[n];
+        for(int i=0; i<n; i++) {
+            if(graph[i].length == 0) set[i] = true;
+        }
+        for(int i=0; i<n; i++) {
+            if(!vis[i]) {
+                dfs(i, n, graph, vis, set);
+            }
+        }
+        for(int i=0; i<n; i++) if(set[i]) ans.add(i);
         return ans;
     }
 }
