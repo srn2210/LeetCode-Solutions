8f4d87b90607001dd090e9669375578176e15ef5
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-05-10 17:21:23 +0530

    Time: 0 ms (100.00%) | Memory: 39.3 MB (80.13%) - LeetSync

--- fibonacci-number/fibonacci-number.java
+++ fibonacci-number/fibonacci-number.java
@@ -1,14 +1,12 @@
 class Solution {
-    int solve(int n, int[] dp) {
+    public int fib(int n) {
         if(n == 0) return 0;
         if(n == 1) return 1;
-        if(dp[n] != -1) return dp[n];
-        int ans = solve(n-1, dp) + solve(n-2, dp);
-        return dp[n] = ans;
-    }
-    public int fib(int n) {
         int[] dp = new int[n+1];
-        Arrays.fill(dp, -1);
-        return solve(n, dp);
+        dp[1] = 1;
+        for(int i=2; i<=n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 }
