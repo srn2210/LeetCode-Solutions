03b758a79ea04fb1b2187ee34bdfa1573258e1c4
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-05-12 13:11:48 +0530

    Time: 31 ms (20.11%) | Memory: 112.7 MB (38.62%) - LeetSync

--- solving-questions-with-brainpower/solving-questions-with-brainpower.java
+++ solving-questions-with-brainpower/solving-questions-with-brainpower.java
@@ -2,8 +2,7 @@ class Solution {
     long solve(int[][] que, int idx, long[] dp) {
         if(idx >= que.length) return 0;
         if(dp[idx] != -1) return dp[idx];
-        long ans = 0;
-        ans = Math.max(solve(que, idx+1, dp), que[idx][0] + solve(que, idx+que[idx][1]+1, dp));
+        long ans = Math.max(solve(que, idx+1, dp), que[idx][0] + solve(que, idx+1+que[idx][1], dp));
         return dp[idx] = ans;
     }
     public long mostPoints(int[][] questions) {
