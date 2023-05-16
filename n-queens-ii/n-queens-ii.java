2eaa28078eb71bac9c37b21251bb1959833d4d4c
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-05-16 16:45:06 +0530

    Time: 4 ms (37.63%) | Memory: 39.1 MB (91.81%) - LeetSync

---
+++ n-queens-ii/n-queens-ii.java
@@ -0,0 +1,53 @@
+class Solution {
+    boolean check(boolean[][] board, int row, int col) {
+        int n = board.length;
+        for(int i=0; i<n; i++) {
+            if(i == row) continue;
+            else {
+                if(board[i][col]) return false;
+            }
+        }
+        for(int i=0; i<n; i++) {
+            if(i == col) continue;
+            else {
+                if(board[row][i]) return false;
+            }
+        }
+        int x = row-1, y = col-1;
+        while(x < n && y < n && x >= 0 && y >= 0) {
+            if(board[x--][y--]) return false;
+        }
+        x = row-1;
+        y = col+1;
+        while(x < n && y < n && x >= 0 && y >= 0) {
+            if(board[x--][y++]) return false;
+        }
+        x = row+1;
+        y = col-1;
+        while(x < n && y < n && x >= 0 && y >= 0) {
+            if(board[x++][y--]) return false;
+        }
+        x = row+1;
+        y = col+1;
+        while(x < n && y < n && x >= 0 && y >= 0) {
+            if(board[x++][y++]) return false;
+        }
+        return true;
+    }
+    int backtrack(int row, int col, int n, boolean[][] board) {
+        if(col == n) {
+            //if(check(board)) return 1;
+            return 1;
+        }
+        int ans = 0;
+        for(int i=0; i<n; i++) {
+            board[i][col] = true;
+            if(check(board, i, col)) ans += backtrack(row, col+1, n, board);
+            board[i][col] = false;
+        }
+        return ans;
+    }
+    public int totalNQueens(int n) {
+        return backtrack(0, 0, n, new boolean[n][n]);
+    }
+}
