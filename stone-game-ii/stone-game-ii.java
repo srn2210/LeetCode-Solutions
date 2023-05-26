79aebe8fbd3d2d0e49b22798afffb1cb24fc437d
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-05-27 00:37:31 +0530

    Time: 38 ms (11.30%) | Memory: 44.2 MB (5.65%) - LeetSync

---
+++ stone-game-ii/stone-game-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    int[] pre;
+    int solve(int[] piles, int idx, int M, int flag, int[][][] dp) {
+        if(idx == piles.length) return 0;
+        if(dp[idx][M][flag] != -1) return dp[idx][M][flag];
+        int ans = flag == 0 ? -1 : 1000000, s = 0;
+        for(int i=1; i<=Math.min(2*M, piles.length-idx); i++) {
+            s += piles[idx + i - 1];
+            if(flag == 0) ans = Math.max(ans, s + solve(piles, i+idx, Math.max(M, i), 1, dp));
+            else ans = Math.min(ans, solve(piles, i+idx, Math.max(M, i), 0, dp));
+        }
+        return dp[idx][M][flag] = ans;
+    }
+    public int stoneGameII(int[] piles) {
+        int[][][] dp = new int[piles.length][201][2];
+        for(int i=0; i<dp.length; i++) {
+            for(int j=0; j<dp[i].length; j++) {
+                Arrays.fill(dp[i][j], -1);
+            }
+        }
+        return solve(piles, 0, 1, 0, dp);
+    }
+}
