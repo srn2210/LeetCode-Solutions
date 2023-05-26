22ffa9901dce52577ebca24d9e0db5f11290694a
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-05-27 03:55:02 +0530

    Time: 6 ms (58.38%) | Memory: 41.7 MB (92.49%) - LeetSync

--- stone-game-ii/stone-game-ii.java
+++ stone-game-ii/stone-game-ii.java
@@ -1,23 +1,23 @@
 class Solution {
     int[] pre;
-    int solve(int[] piles, int idx, int M, int flag, int[][][] dp) {
-        if(idx == piles.length) return 0;
-        if(dp[idx][M][flag] != -1) return dp[idx][M][flag];
-        int ans = flag == 0 ? -1 : 1000000, s = 0;
-        for(int i=1; i<=Math.min(2*M, piles.length-idx); i++) {
+    int solve(int[] piles, int idx, int M, int[][] dp) {
+        if(idx >= piles.length) return 0;
+        if(dp[idx][M] != -1) return dp[idx][M];
+        int ans = -1000000, s = 0;
+        for(int i=1; i<=2*M; i++) {
+            if(i + idx - 1 >= piles.length) break;
             s += piles[idx + i - 1];
-            if(flag == 0) ans = Math.max(ans, s + solve(piles, i+idx, Math.max(M, i), 1, dp));
-            else ans = Math.min(ans, solve(piles, i+idx, Math.max(M, i), 0, dp));
+            ans = Math.max(ans, s - solve(piles, idx + i, Math.max(M, i), dp));
         }
-        return dp[idx][M][flag] = ans;
+        return dp[idx][M] = ans;
     }
     public int stoneGameII(int[] piles) {
-        int[][][] dp = new int[piles.length][piles.length+1][2];
+        int[][] dp = new int[piles.length][piles.length+1];
         for(int i=0; i<dp.length; i++) {
-            for(int j=0; j<dp[i].length; j++) {
-                Arrays.fill(dp[i][j], -1);
-            }
+            Arrays.fill(dp[i], -1);
         }
-        return solve(piles, 0, 1, 0, dp);
+        int total = 0;
+        for(int i : piles) total += i;
+        return (solve(piles, 0, 1, dp) + total) / 2;
     }
 }
