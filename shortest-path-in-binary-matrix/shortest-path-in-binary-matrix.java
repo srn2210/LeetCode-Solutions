657a0445711063ba1a30e4592cc5425da6bb4e19
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-06-01 16:41:05 +0530

    Time: 19 ms (43.72%) | Memory: 44.7 MB (22.14%) - LeetSync

--- shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,16 +1,15 @@
 class Solution {
     int[][] directions = new int[][]{{1,0}, {0,1}, {-1,0}, {0,-1}, {1,1}, {1,-1}, {-1,1}, {-1,-1}};
-    boolean isSafe(int x, int y, int[][] grid) {
-        return x >= 0 && y >= 0 && x < grid.length && y < grid[0].length && grid[x][y] == 0;
+    boolean isSafe(int x, int y, int[][] grid, boolean[][] vis) {
+        return x >= 0 && y >= 0 && x < grid.length && y < grid[0].length && grid[x][y] == 0 && !vis[x][y];
     }
     public int shortestPathBinaryMatrix(int[][] grid) {
         if(grid[0][0] == 1) return -1;
-        int m = grid.length, n = grid[0].length;
+        int m = grid.length, n = grid[0].length, distance = 1;
         var q = new LinkedList<int[]>();
         var vis = new boolean[m][n];
         q.add(new int[]{0,0});
         vis[0][0] = true;
-        int distance = 1;
         while(!q.isEmpty()) {
             int size = q.size();
             while(size-- > 0) {
@@ -19,7 +18,7 @@ class Solution {
                 for(int[] dir : directions) {
                     int x = dir[0] + arr[0];
                     int y = dir[1] + arr[1];
-                    if(isSafe(x, y, grid) && !vis[x][y]) {
+                    if(isSafe(x, y, grid, vis)) {
                         vis[x][y] = true;
                         q.add(new int[]{x,y});
                     }
