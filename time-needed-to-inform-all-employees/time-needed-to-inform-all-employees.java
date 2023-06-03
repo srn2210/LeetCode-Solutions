c2af9ffd9d4a49b456b33fb74631cbe07140721e
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-06-04 00:24:06 +0530

    Time: 84 ms (54.09%) | Memory: 61.4 MB (49.72%) - LeetSync

--- time-needed-to-inform-all-employees/time-needed-to-inform-all-employees.java
+++ time-needed-to-inform-all-employees/time-needed-to-inform-all-employees.java
@@ -1,23 +1,22 @@
 class Solution {
     public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
-        var adj = new ArrayList<List<Integer>>(n);
+        var adj = new ArrayList<List<Integer>>();
         for(int i=0; i<n; i++) adj.add(new ArrayList<>());
-        for(int i=0; i<n; i++) if(manager[i] != -1) adj.get(manager[i]).add(i);
-        int[] cost = new int[n];
-        Arrays.fill(cost, Integer.MAX_VALUE);
-        cost[headID] = 0;
-        var q = new LinkedList<Integer>();
-        q.add(headID);
+        for(int i=0; i<n; i++) {
+            if(manager[i] != -1) adj.get(manager[i]).add(i);
+        }
+        var q = new LinkedList<Pair<Integer, Integer>>();
+        q.add(new Pair<>(headID, 0));
         int ans = 0;
         while(!q.isEmpty()) {
             int size = q.size();
             while(size-- > 0) {
-                int node = q.poll();
-                int curr = cost[node];
-                ans = Math.max(curr, ans);
-                for(int i : adj.get(node)) {
-                    cost[i] = curr + informTime[node];
-                    q.add(i);
+                var node = q.poll();
+                int emp = node.getKey();
+                int time = node.getValue();
+                ans = Math.max(ans, time);
+                for(int neigh : adj.get(emp)) {
+                    q.add(new Pair<>(neigh, time + informTime[emp]));
                 }
             }
         }
