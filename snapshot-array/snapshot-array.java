62d35d565b376233ee2b39d63f3c9c0b021a9e73
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-06-04 02:22:03 +0530

    Time: 72 ms (35.17%) | Memory: 89.1 MB (9.32%) - LeetSync

--- snapshot-array/snapshot-array.java
+++ snapshot-array/snapshot-array.java
@@ -3,12 +3,14 @@ class SnapshotArray {
     List<List<Pair<Integer, Integer>>> arr;
     public SnapshotArray(int length) {
         arr = new ArrayList<>();
+        
         snaps = 0;
         for(int i=0; i<length; i++) {
             var list = new ArrayList<Pair<Integer, Integer>>();
             list.add(new Pair(0,0));
             arr.add(list);
         }
+        //arr.add(list);
     }
     
     public void set(int index, int val) {
@@ -16,19 +18,28 @@ class SnapshotArray {
     }
     
     public int snap() {
+        //arr.add(new HashMap<>());
         return snaps++;
     }
     
     public int get(int index, int snap_id) {
+        //System.out.println(arr);
+        //System.out.println(snaps);
         int left = 0, right = arr.get(index).size()-1;
         int ans = 0;
+        //System.out.println(left);
+        //System.out.println(right);
         while(left < right) {
             int mid = left + (right - left) / 2 + 1;
+            //System.out.println(mid);
             if(arr.get(index).get(mid).getKey() <= snap_id) {
+                //if(mid <= snap_id) ans = mid;
                 left = mid;
             }
             else right = mid - 1;
         }
+        //System.out.println(arr.get(ans));
+
         return arr.get(index).get(left).getValue();
     }
 }
