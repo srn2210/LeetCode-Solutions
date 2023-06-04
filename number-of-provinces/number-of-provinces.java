b93bdb420ac38f192f3629eb61cfdef22959648f
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-06-04 07:51:55 +0530

    Time: 12 ms (5.80%) | Memory: 44.5 MB (7.22%) - LeetSync

--- number-of-provinces/number-of-provinces.java
+++ number-of-provinces/number-of-provinces.java
@@ -1,52 +1,26 @@
 class Solution {
-    class UnionFind {
-        int[] arr;
-        int[] size;
-        int components;
-        UnionFind(int n) {
-            arr = new int[n];
-            size = new int[n];
-            components = n;
-            for(int i=0; i<n; i++) {
-                arr[i] = i;
-                size[i] = 1;
+    void dfs(int row, int[][] grid, boolean[][] vis) {
+        if(row < 0 || row >= grid.length) return;
+        for(int i=0; i<grid.length; i++) {
+            if(grid[row][i] == 1 && !vis[row][i]) {
+                vis[row][i] = true;
+                dfs(i, grid, vis);
             }
-        }
-        int find(int p) {
-            if(arr[p] == p) return p;
-            else return arr[p] = find(arr[p]);
-        }
-        boolean isConnected(int p, int q) {
-            return find(p) == find(q);
-        }
-        void union(int p, int q) {
-            int x = find(p);
-            int y = find(q);
-            if(isConnected(x, y)) return;
-            else {
-                components--;
-                if(size[x] < size[y]) {
-                    arr[x] = arr[y];
-                    size[y] += size[x];
-                }
-                else {
-                    arr[y] = arr[x];
-                    size[x] += size[y];
-                }
-            }
-        }
-        int size() {
-            return components;
         }
     }
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
-        UnionFind uf = new UnionFind(n);
+        int ans = 0;
+        boolean[][] vis = new boolean[n][n];
         for(int i=0; i<n; i++) {
             for(int j=0; j<n; j++) {
-                if(isConnected[i][j] == 1) uf.union(i, j);
+                if(!vis[i][j] && isConnected[i][j] == 1) {
+                    ans++;
+                    vis[i][j] = true;
+                    dfs(i, isConnected, vis);
+                }
             }
         }
-        return uf.size();
+        return ans;
     }
 }
