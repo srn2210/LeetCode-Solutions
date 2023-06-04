f19a074196959f72602f446fd8509dcc395fca8c
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-06-04 14:17:34 +0530

    Time: 13 ms (41.73%) | Memory: 43.2 MB (24.70%) - LeetSync

--- sliding-puzzle/sliding-puzzle.java
+++ sliding-puzzle/sliding-puzzle.java
@@ -24,7 +24,6 @@ class Solution {
         for(int i=0; i<board.length; i++) {
             for(int j=0; j<board[0].length; j++) {
                 res.append(board[i][j]);
-                //res.append('_');
             }
         }
         return res.toString();
