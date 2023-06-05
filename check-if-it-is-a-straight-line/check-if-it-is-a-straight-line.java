f110b4d915ef1c2ca19d4987b2b0fd8a3d02575d
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-06-05 09:33:33 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (20.98%) - LeetSync

--- check-if-it-is-a-straight-line/check-if-it-is-a-straight-line.java
+++ check-if-it-is-a-straight-line/check-if-it-is-a-straight-line.java
@@ -1,16 +1,15 @@
 class Solution {
+    int yDiff(int[] arr1, int[] arr2) {
+        return arr1[1] - arr2[1];
+    }
+    int xDiff(int[] arr1, int[] arr2) {
+        return arr1[0] - arr2[0];
+    }
     public boolean checkStraightLine(int[][] coordinates) {
-        Arrays.sort(coordinates, (a,b) -> Integer.compare(a[1], b[1]));
-        int left = 0, right = 1;
-        double mPrev = (coordinates[right][1] - coordinates[left][1]) / (double)(coordinates[right][0] - coordinates[left][0]);
-        right++;
-        left++;
-        while(right < coordinates.length) {
-            double m = (coordinates[right][1] - coordinates[left][1]) / (double)(coordinates[right][0] - coordinates[left][0]);
-            if(m != mPrev) return false;
-            mPrev = m;
-            right++;
-            left++;
+        int dy = yDiff(coordinates[1], coordinates[0]);
+        int dx = xDiff(coordinates[1], coordinates[0]);
+        for(int i=2; i<coordinates.length; i++) {
+            if(dy * xDiff(coordinates[i], coordinates[0]) != dx * yDiff(coordinates[i], coordinates[0])) return false;
         }
         return true;
     }
