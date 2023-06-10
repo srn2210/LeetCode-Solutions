a8146c4099e26d954e3779bbbaa95b5111c01b56
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-06-10 13:30:00 +0530

    Time: 1 ms (100.00%) | Memory: 39.4 MB (92.06%) - LeetSync

---
+++ maximum-value-at-a-given-index-in-a-bounded-array/maximum-value-at-a-given-index-in-a-bounded-array.java
@@ -0,0 +1,24 @@
+class Solution {
+    long sum(long k, int a) {
+        return k * ( (2 * a) - k - 1) / 2;
+    }
+    boolean safe(int target, int n, int idx, int max) {
+        long left = Math.min(target-1, idx), right = Math.min(target - 1, n - idx - 1);
+        left = sum(left, target) + idx - left;
+        right = sum(right, target) + n - idx - 1 - right;
+        //System.out.println(target + " " + left + " " + right);
+        long total = left + right + target;
+        return total <= max;
+    }
+    public int maxValue(int n, int index, int maxSum) {
+        int left = 0, right = maxSum - (n - 1);
+        while(left < right) {
+            int mid = left + (right - left) / 2 + 1;
+            if(safe(mid, n, index, maxSum)) {
+                left = mid;
+            }
+            else right = mid - 1;
+        }
+        return left;
+    }
+}
