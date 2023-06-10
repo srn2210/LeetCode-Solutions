3b4e603eb3f3256adad0196bd02477e2888294e4
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-06-10 21:51:02 +0530

    Time: 13 ms (100.00%) | Memory: 55.2 MB (100.00%) - LeetSync

--- movement-of-robots/movement-of-robots.java
+++ movement-of-robots/movement-of-robots.java
@@ -1,22 +1,16 @@
 class Solution {
     public int sumDistance(int[] nums, String s, int d) {
-        int mod = (int)1e9+7;
-        int n = nums.length;
-        long[][] arr = new long[n][2];
+        int mod = (int)1e9+7, n = nums.length;
+        long[] arr = new long[n];
         for(int i=0; i<n; i++) {
-            int dir = 0;
-            if(s.charAt(i) == 'R') dir = 1;
-            else dir = -1;
-            arr[i] = new long[]{nums[i], dir};
+            if(s.charAt(i) == 'R') arr[i] = nums[i] + (long)d;
+            else arr[i] = nums[i] - (long)d;
         }
-        for(int i=0; i<n; i++) {
-            arr[i][0] = arr[i][0] + (d * arr[i][1]);
-        }
-        Arrays.sort(arr, (a,b) -> Long.compare(a[0], b[0]));
+        Arrays.sort(arr);
         long sum = 0;
         int left = 0, right = n-1;
         while(left < right) {
-            sum = sum + ((right - left) * Math.abs(arr[left][0] - arr[right][0]));
+            sum = sum + ((right - left) * Math.abs(arr[left] - arr[right]));
             sum = sum % mod;
             left++;
             right--;
