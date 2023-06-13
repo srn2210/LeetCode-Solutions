856570cd1fa11464a94a70561997032cdfe056c4
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-06-13 16:26:16 +0530

    Time: 35 ms (73.31%) | Memory: 48.6 MB (78.99%) - LeetSync

--- equal-row-and-column-pairs/equal-row-and-column-pairs.java
+++ equal-row-and-column-pairs/equal-row-and-column-pairs.java
@@ -1,13 +1,14 @@
 class Solution {
     public int equalPairs(int[][] grid) {
         int m = grid.length, n = grid[0].length, ans = 0;
-        var rows = new ArrayList<String>();
-        var cols = new ArrayList<String>();
         for(int i=0; i<m; i++) {
             for(int k=0; k<m; k++) {
                 var flag = true;
                 for(int j=0; j<n; j++) {
-                    if(grid[i][j] != grid[j][k]) flag = false;
+                    if(grid[i][j] != grid[j][k]) {
+                        flag = false;
+                        break;
+                    }
                 }
                 if(flag) ans++;
             }
