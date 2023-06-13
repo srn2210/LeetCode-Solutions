721c5bb5f2c8f2c7d5218b6c794fdde55a3f7dde
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-06-13 16:25:32 +0530

    Time: 81 ms (25.54%) | Memory: 48.2 MB (85.68%) - LeetSync

--- equal-row-and-column-pairs/equal-row-and-column-pairs.java
+++ equal-row-and-column-pairs/equal-row-and-column-pairs.java
@@ -1,28 +1,15 @@
 class Solution {
     public int equalPairs(int[][] grid) {
-        int m = grid.length, n = grid[0].length;
+        int m = grid.length, n = grid[0].length, ans = 0;
         var rows = new ArrayList<String>();
         var cols = new ArrayList<String>();
         for(int i=0; i<m; i++) {
-            StringBuilder row = new StringBuilder();
-            for(int j=0; j<n; j++) {
-                row.append(grid[i][j]);
-                row.append(',');
-            }
-            rows.add(row.toString());
-        }
-        for(int i=0; i<n; i++) {
-            StringBuilder col = new StringBuilder();
-            for(int j=0; j<m; j++) {
-                col.append(grid[j][i]);
-                col.append(',');
-            }
-            cols.add(col.toString());
-        }
-        int ans = 0;
-        for(var row : rows) {
-            for(var col : cols) {
-                if(row.equals(col)) ans++;
+            for(int k=0; k<m; k++) {
+                var flag = true;
+                for(int j=0; j<n; j++) {
+                    if(grid[i][j] != grid[j][k]) flag = false;
+                }
+                if(flag) ans++;
             }
         }
         return ans;
