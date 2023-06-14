663faad277c548ae0913a405ad3ecdf8341b673e
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-06-15 02:16:07 +0530

    Time: 2 ms (63.13%) | Memory: 59.3 MB (16.02%) - LeetSync

--- first-missing-positive/first-missing-positive.java
+++ first-missing-positive/first-missing-positive.java
@@ -11,7 +11,7 @@ class Solution {
                 swap(i, nums[i]-1, nums);
             }
         }
-        for(int i=0; i<n; i++) if(nums[i] != i+1) return i+1;
+        for(int i=1; i<=n; i++) if(nums[i-1] != i) return i;
         return n+1;
     }
 }
