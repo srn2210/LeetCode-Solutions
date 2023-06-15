93f35a516ed0410db79d809a4d7d965a08a40d96
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-06-16 00:25:03 +0530

    Time: 7 ms (12.57%) | Memory: 43.9 MB (15.40%) - LeetSync

---
+++ generate-parentheses/generate-parentheses.java
@@ -0,0 +1,42 @@
+class Solution {
+    boolean valid(StringBuilder s) {
+        var st = new Stack<Character>();
+        for(int i=0; i<s.length(); i++) {
+            char ch = s.charAt(i);
+            switch(ch) {
+                case '(':
+                    st.push(ch);
+                    break;
+                default:
+                    if(!st.isEmpty()) st.pop();
+                    else return false;
+                    break;
+            }
+        }
+        return st.isEmpty();
+    }
+    void backtrack(int remOpen, int remClose, StringBuilder s, List<String> ans) {
+        if(remOpen == 0 && remClose == 0 && valid(s)) {
+            ans.add(s.toString());
+            return;
+        }
+        else if(remOpen == 0 && remClose == 0) return;
+        else {
+            if(remOpen > 0) {
+                s.append('(');
+                backtrack(remOpen-1, remClose, s, ans);
+                s.deleteCharAt(s.length()-1);
+            }
+            if(remClose > 0) {
+                s.append(')');
+                backtrack(remOpen, remClose-1, s, ans);
+                s.deleteCharAt(s.length()-1);
+            }
+        }
+    }
+    public List<String> generateParenthesis(int n) {
+        var ans = new ArrayList<String>();
+        backtrack(n, n, new StringBuilder(), ans);
+        return ans;
+    }
+}
