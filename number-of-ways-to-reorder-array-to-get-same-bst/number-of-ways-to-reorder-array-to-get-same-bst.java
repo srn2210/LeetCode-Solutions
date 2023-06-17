905975b65ba18ff6ab9f0dc42d0294a88046baad
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-06-17 17:26:35 +0530

    Time: 181 ms (27.27%) | Memory: 62.4 MB (28.79%) - LeetSync

--- number-of-ways-to-reorder-array-to-get-same-bst/number-of-ways-to-reorder-array-to-get-same-bst.java
+++ number-of-ways-to-reorder-array-to-get-same-bst/number-of-ways-to-reorder-array-to-get-same-bst.java
@@ -1,25 +1,24 @@
 class Solution {
-    private long mod = (long)1e9 + 7;
-    private long[][] table;
+    long mod = (long)1e9 + 7;
+    long[][] dp;
 
     public int numOfWays(int[] nums) {
         int m = nums.length;
         
         // Table of Pascal's triangle
-        table = new long[m][m];
-        for (int i = 0; i < m; ++i) {
-            table[i][0] = table[i][i] = 1;
-        }
-        for (int i = 2; i < m; i++) {
-            for (int j = 1; j < i; j++) {
-                table[i][j] = (table[i - 1][j - 1] + table[i - 1][j]) % mod;
-            }
-        }
+        dp = new long[m][m];
+        for(int i=0; i<m; i++) Arrays.fill(dp[i], -1);
         List<Integer> arrList = Arrays.stream(nums).boxed().collect(Collectors.toList());
         return (int)((dfs(arrList) - 1) % mod);
     }
 
-    private long dfs(List<Integer> nums) {
+    long calc(int left, int right) {
+        if(left == 0 || right == 0) return 1;
+        if(dp[left][right] != -1) return dp[left][right];
+        return dp[left][right] = (calc(left-1, right) + calc(left, right-1)) % mod;
+    }
+
+    long dfs(List<Integer> nums) {
         int m = nums.size();
         if (m < 3) {
             return 1;
@@ -37,6 +36,6 @@ class Solution {
         long leftWays = dfs(leftNodes) % mod;
         long rightWays = dfs(rightNodes) % mod;
 
-        return (((leftWays * rightWays) % mod) * table[m - 1][leftNodes.size()]) % mod;
+        return (((leftWays * rightWays) % mod) * calc(leftNodes.size(), rightNodes.size())) % mod;
     }
 }
