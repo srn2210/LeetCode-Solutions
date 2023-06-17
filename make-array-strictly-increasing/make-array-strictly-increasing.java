6bb0e4b948f050a41eec9116ddb7e8e9879232df
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-06-17 14:57:12 +0530

    Time: 317 ms (35.37%) | Memory: 119.6 MB (29.93%) - LeetSync

--- make-array-strictly-increasing/make-array-strictly-increasing.java
+++ make-array-strictly-increasing/make-array-strictly-increasing.java
@@ -1,18 +1,18 @@
 class Solution {
     Map<Pair<Integer, Integer>, Integer> dp;
-    int solve(int[] arr, TreeMap<Integer, Integer> map, int idx, int prev) {
+    int solve(int[] arr, TreeSet<Integer> map, int idx, int prev) {
         if(idx == arr.length) return 0;
         if(dp.containsKey(new Pair<>(idx, prev))) return dp.get(new Pair<>(idx, prev));
         int ans = 100000;
-        var low = map.higherKey(prev);
+        var low = map.higher(prev);
         if(arr[idx] > prev) ans = solve(arr, map, idx+1, arr[idx]);
         if(low != null) ans = Math.min(ans, 1 + solve(arr, map, idx+1, low));
         dp.put(new Pair<>(idx, prev), ans);
         return ans;
     }
     public int makeArrayIncreasing(int[] arr1, int[] arr2) {
-        var map = new TreeMap<Integer, Integer>();
-        for(int i : arr2) map.put(i, map.getOrDefault(i, 0)+1);
+        var map = new TreeSet<Integer>();
+        for(int i : arr2) map.add(i);
         dp = new HashMap<>();
         int ans = solve(arr1, map, 0, -1);
         return ans >= 100000 ? -1 : ans;
