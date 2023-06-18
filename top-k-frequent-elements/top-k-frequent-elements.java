198ffa062ab0ce5c0b0ccdc114f0cb6bbfaed574
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-06-18 17:23:57 +0530

    Time: 12 ms (88.22%) | Memory: 48 MB (17.42%) - LeetSync

--- top-k-frequent-elements/top-k-frequent-elements.java
+++ top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,14 +1,54 @@
 class Solution {
+    Random rand;
+    void swap(int[][] arr, int pivot, int hi) {
+        int t1 = arr[pivot][0];
+        int t2 = arr[pivot][1];
+        arr[pivot][0] = arr[hi][0];
+        arr[pivot][1] = arr[hi][1];
+        arr[hi][0] = t1;
+        arr[hi][1] = t2;
+    }
+    void solve(int[][] arr, int lo, int hi, int k) {
+        if(lo >= hi) return;
+        int pivot = rand.nextInt(hi-lo) + lo;
+        swap(arr, pivot, hi);
+
+        int left = lo, right = hi-1;
+        //while(right > left && arr[right][1] > arr[hi][1]) right--;
+        while(left <= right) {
+            if(arr[left][1] < arr[hi][1]) {
+                left++;
+            }
+            else {
+                swap(arr, right, left);
+                right--;
+            }
+        }
+        swap(arr, hi, left);
+
+        if(left == k) return;
+        else if(left < k) {
+            solve(arr, left+1, hi, k);
+        }
+        else solve(arr, lo, hi-1, k);
+
+    }
     public int[] topKFrequent(int[] nums, int k) {
+        rand = new Random();
         var map = new HashMap<Integer, Integer>();
         for(int num : nums) map.put(num, map.getOrDefault(num, 0) + 1);
-        var pq = new PriorityQueue<Integer>((a,b) -> Integer.compare(map.get(a), map.get(b)));
-        for(int key : map.keySet()) {
-            pq.add(key);
-            if(pq.size() > k) pq.poll();
+        int[][] arr = new int[map.size()][2];
+        int idx = 0;
+        for(var entry : map.entrySet()) {
+            arr[idx][0] = entry.getKey();
+            arr[idx][1] = entry.getValue();
+            idx++;
         }
-        var ans = new int[k];
-        for(int i=0; i<k; i++) ans[i] = pq.poll();
+        solve(arr, 0, arr.length-1, arr.length-k);
+        int[] ans = new int[k];
+        int j= 0;
+        //for(int[] a : arr) System.out.println(Arrays.toString(a));
+        for(int i=arr.length-k; i<arr.length; i++) ans[j++] = arr[i][0];
         return ans;
     }
 }
