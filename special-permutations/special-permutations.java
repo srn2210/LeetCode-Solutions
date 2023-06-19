cb77edb44ead618bde9ac59a39b3012fd1dcfd4c
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-06-19 19:42:04 +0530

    Time: 188 ms (100.00%) | Memory: 44.8 MB (100.00%) - LeetSync

--- special-permutations/special-permutations.java
+++ special-permutations/special-permutations.java
@@ -1,52 +1,26 @@
 class Solution {
     int mod = (int)1e9+7;
-    int ans;
-    void swap(int[] arr, int i, int j) {
-        int t = arr[i];
-        arr[i] = arr[j];
-        arr[j] = t;
-    }
-    void solve(int[] nums, int end) {
-        if(end == 0) {
-            for(int i=0; i<nums.length-1; i++) {
-                var flag = (nums[i] % nums[i+1] == 0) || (nums[i+1] % nums[i] == 0);
-                if(!flag) return;
-            }
-            ans += 1;
-            ans %= mod;
-            return;
-        }
-        else {
-            for(int i=0; i<=end; i++) {
-                if(end + 1 < nums.length && (nums[i] % nums[end+1] == 0 || nums[end+1] % nums[i] == 0)) {
-                    swap(nums, i, end);
-                    solve(nums, end-1);
-                    swap(nums, i, end);
-                }
-                else if(end == nums.length-1) {
-                    swap(nums, i, end);
-                    solve(nums, end-1);
-                    swap(nums, i, end);
-                }
+    int solve(int[] nums, int prev, int mask, int[][] dp) {
+        if(mask == (1 << dp.length)-1) return 1;
+        if(dp[prev][mask] != -1) return dp[prev][mask];
+        int ans = 0;
+        for(int i=0; i<dp.length; i++) {
+            int m = 1 << i;
+            if((mask & m) == 0 && (nums[i] % nums[prev] == 0 || nums[prev] % nums[i] == 0)) {
+                ans += solve(nums, i, mask | m, dp);
+                ans %= mod;
             }
         }
-    }
-    public boolean checkAll(int[] nums){
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]%nums[j]!=0 && nums[j]%nums[i]!=0) return false;
-            }
-        }
-        return true;
-    }
-    public long factorial(int n){
-        if(n==1 || n==0) return 1;
-        else return n*factorial(n-1);
+        return dp[prev][mask] = ans % mod;
     }
     public int specialPerm(int[] nums) {
-        if(checkAll(nums)) return (int)(factorial(nums.length)%1000000007L);
-        ans = 0;
-        solve(nums, nums.length-1);
-        return ans % mod;
+        int[][] dp = new int[nums.length][1 << nums.length];
+        for(int[] d : dp) Arrays.fill(d, -1);
+        int ans = 0;
+        for(int i=0; i<nums.length; i++) {
+            ans += solve(nums, i, 1 << i, dp);
+            ans %= mod;
+        }
+        return ans;
     }
 }
