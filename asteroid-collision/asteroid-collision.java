b0f7fed6157122390e99a93bd0ac9228bf80e563
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-06-21 23:14:01 +0530

    Time: 3 ms (96.35%) | Memory: 44.1 MB (46.54%) - LeetSync

--- asteroid-collision/asteroid-collision.java
+++ asteroid-collision/asteroid-collision.java
@@ -10,7 +10,7 @@ class Solution {
         }
     }
     public int[] asteroidCollision(int[] asteroids) {
-        var st = new Stack<Integer>();
+        var st = new ArrayDeque<Integer>();
         for(int i : asteroids) {
             if(st.isEmpty()) st.push(i);
             else if(!st.isEmpty()) {
