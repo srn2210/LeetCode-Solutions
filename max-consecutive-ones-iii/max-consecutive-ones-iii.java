7ccc68795db04b247b8620a06e39e4964794a2a7
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-06-21 22:00:26 +0530

    Time: 10 ms (7.61%) | Memory: 46.5 MB (32.96%) - LeetSync

--- max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -1,6 +1,19 @@
 class Solution {
+    boolean valid(int window, int[] nums, int k) {
+        int count = 0;
+        for(int i=0; i<window-1; i++) if(nums[i] == 1) count++;
+        int left = 0, right = window-1;
+        while(right < nums.length) {
+            if(nums[right] == 1) count++;
+            if(count + k >= window) return true;
+            if(nums[left] == 1) count--;
+            right++;
+            left++;
+        }
+        return false;
+    }
     public int longestOnes(int[] nums, int k) {
-        var q = new LinkedList<Integer>();
+        /*var q = new LinkedList<Integer>();
         int left = 0, right = 0, ans = 0;
         while(right < nums.length) {
             if(nums[right] == 1) {
@@ -25,6 +38,13 @@ class Solution {
                 left = right;
             }
         }
-        return ans;
+        return ans;*/
+        int lo = 0, hi = nums.length, ans = 0;
+        while(lo < hi) {
+            int mid = lo + (hi - lo) / 2 + 1;
+            if(valid(mid, nums, k)) lo = mid;
+            else hi = mid - 1;
+        }
+        return lo;
     }
 }
