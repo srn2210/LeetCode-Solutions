348752bdd2a325c783599c96c236a5a6623671e4
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-06-28 20:49:26 +0530

    Time: 10 ms (97.43%) | Memory: 55.7 MB (68.53%) - LeetSync

--- path-with-maximum-probability/path-with-maximum-probability.java
+++ path-with-maximum-probability/path-with-maximum-probability.java
@@ -2,33 +2,22 @@ class Solution {
     public double maxProbability(int n, int[][] edges, double[] succProb, int start, int end) {
         double[] probs = new double[n];
         probs[start] = 1.0;
-        var pq = new PriorityQueue<double[]>((a,b) -> Double.compare(b[0], a[0]));
-        var adj = new HashMap<Integer, List<Pair<Integer, Double>>>();
-        for(int i=0; i<edges.length; i++) {
-            int[] edge = edges[i];
-            int src = edge[0], des = edge[1];
-            adj.computeIfAbsent(src, a -> new ArrayList<>()).add(new Pair<>(des, succProb[i]));
-            adj.computeIfAbsent(des, a -> new ArrayList<>()).add(new Pair<>(src, succProb[i]));
-        }
-        var vis = new boolean[n];
-        vis[start] = true;
-        pq.add(new double[]{1.0, start, start});
-        while(!pq.isEmpty()) {
-            var arr = pq.poll();
-            int src = (int)arr[1];
-            int des = (int)arr[2];
-            double cost = arr[0];
-            vis[des] = true;
-            probs[des] = Math.max(probs[des], cost);
-            if(adj.containsKey(des)) {
-                for(var nei : adj.get(des)) {
-                    int node = nei.getKey();
-                    double c = nei.getValue();
-                    if(!vis[node]) {
-                        pq.add(new double[]{cost * c, des, node});
-                    }
+        for(int j=0; j<n-1; j++) {
+            var flag = false;
+            for(int i=0; i<edges.length; i++) {
+                int[] edge = edges[i];
+                int src = edge[0];
+                int des = edge[1];
+                if(probs[des] * succProb[i] > probs[src]) {
+                    probs[src] = probs[des] * succProb[i];
+                    flag = true;
+                }
+                if(probs[src] * succProb[i] > probs[des]) {
+                    probs[des] = probs[src] * succProb[i];
+                    flag = true;
                 }
             }
+            if(!flag) break;
         }
         return probs[end];
     }
