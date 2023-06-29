ccc77617100b871ffe1e1704378c50a738b53eb8
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-06-30 00:43:47 +0530

    Time: 30 ms (78.11%) | Memory: 44.3 MB (77.51%) - LeetSync

--- shortest-path-to-get-all-keys/shortest-path-to-get-all-keys.java
+++ shortest-path-to-get-all-keys/shortest-path-to-get-all-keys.java
@@ -6,29 +6,23 @@ class Solution {
     public int shortestPathAllKeys(String[] grid) {
         int k = 0, m = grid.length, n = grid[0].length();
         var map = Map.of('a', 0, 'b', 1, 'c', 2, 'd', 3, 'e', 4, 'f', 5);
+        var vis = new boolean[64][m][n];
         var q = new LinkedList<int[]>();
         for(int i=0; i<grid.length; i++) {
             for(int j=0; j<grid[i].length(); j++) {
                 var ch = grid[i].charAt(j);
                 if(ch >= 'a' && ch <= 'f') k++;
-            }
-        }
-        int res = (1 << k) - 1;
-        var vis = new boolean[res+1][m][n];
-        for(int i=0; i<grid.length; i++) {
-            for(int j=0; j<grid[i].length(); j++) {
-                var ch = grid[i].charAt(j);
-                if(ch == '@') {
+                else if(ch == '@') {
                     vis[0][i][j] = true;
                     q.add(new int[]{i, j, 0, 0});
                 }
             }
         }
+        int res = (1 << k) - 1;
         while(!q.isEmpty()) {
             var arr = q.poll();
             int mask = arr[2];
             int steps = arr[3];
-            //System.out.println(arr[0] + " , " + arr[1] + " , " + mask + " , " + steps);
             if(mask == res) return steps;
             for(var dir : directions) {
                 int x = dir[0] + arr[0];
@@ -38,7 +32,6 @@ class Solution {
                     var ch = grid[x].charAt(y);
                     if(Character.isLowerCase(ch)) {
                         int t = mask | (1 << map.get(ch));
-                        //System.out.println(Integer.toBinaryString(t));
                         if(vis[t][x][y]) continue;
                         vis[t][x][y] = true;
                         q.add(new int[]{x, y, t, steps+1});
@@ -46,8 +39,6 @@ class Solution {
                     else if(Character.isUpperCase(ch)) {
                         int num = map.get(Character.toLowerCase(ch));
                         if(vis[mask][x][y]) continue;
-                        //System.out.println("here" + x + ", " + y);
-                        //System.out.println(num);
                         if(((1 << num) & mask) != 0) {
                             vis[mask][x][y] = true;
                             q.add(new int[]{x, y, mask, steps+1});
