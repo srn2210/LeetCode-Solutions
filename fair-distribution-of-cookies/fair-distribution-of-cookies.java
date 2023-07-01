6f64eb8636c399ca5664dc50ac9cf956bd9c89fe
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-07-02 00:55:53 +0530

    Time: 7 ms (85.42%) | Memory: 40.1 MB (44.79%) - LeetSync

--- fair-distribution-of-cookies/fair-distribution-of-cookies.java
+++ fair-distribution-of-cookies/fair-distribution-of-cookies.java
@@ -9,23 +9,22 @@ class Solution {
         if(dp[idx][curr] != -1) return dp[idx][curr];
         int ans = solve(cookies, k, idx+1, curr);
         for(int i=1; i<(1 << cookies.length); i++) {
-            if((i & curr) == 0)ans = Math.min(ans, Math.max(solve(cookies, k, idx+1, curr | i), c[i]));
+            if((i & curr) == 0) ans = Math.min(ans, Math.max(solve(cookies, k, idx+1, curr | i), c[i]));
         }
         return dp[idx][curr] = ans;
     }
     public int distributeCookies(int[] cookies, int k) {
         dp = new int[k][1 << cookies.length];
         for(int[] d : dp) Arrays.fill(d, -1);
-        c = new int[1 << cookies.length];
-        for(int i=0; i<(1<<cookies.length); i++) {
-            //c[i] = 0;
-            for(int j=0; j<cookies.length; j++) {
+        int n = cookies.length;
+        c = new int[1 << n];
+        for(int i=0; i<(1<<n); i++) {
+            for(int j=0; j<n; j++) {
                 if((i & (1 << j)) != 0) {
                     c[i] += cookies[j];
                 }
             }
         }
-        //System.out.println(Arrays.toString(c));
         return solve(cookies, k, 0, 0);
     }
 }
