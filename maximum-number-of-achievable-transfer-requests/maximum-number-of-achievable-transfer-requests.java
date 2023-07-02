328004701fad9fd2bc4bf877541edffc5cff27a3
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-07-02 20:10:30 +0530

    Time: 22 ms (36.36%) | Memory: 40.2 MB (69.70%) - LeetSync

---
+++ maximum-number-of-achievable-transfer-requests/maximum-number-of-achievable-transfer-requests.java
@@ -0,0 +1,20 @@
+class Solution {
+    int solve(int[][] requests, int[] arr, int n, int idx) {
+        if(idx == requests.length) {
+            for(int i : arr) {
+                if(i != 0) return -100;
+            }
+            return 0;
+        }
+        int ans = solve(requests, arr, n, idx+1);
+        arr[requests[idx][0]]--;
+        arr[requests[idx][1]]++;
+        ans = Math.max(ans, 1 + solve(requests, arr, n, idx+1));
+        arr[requests[idx][0]]++;
+        arr[requests[idx][1]]--;
+        return ans;
+    }
+    public int maximumRequests(int n, int[][] requests) {
+        return solve(requests, new int[n], n, 0);
+    }
+}
