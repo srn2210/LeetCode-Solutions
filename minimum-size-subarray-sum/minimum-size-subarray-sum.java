0708631ccf64fbd7b0d6370b9f90e17675158e79
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-07-07 01:00:52 +0530

    Time: 24 ms (5.12%) | Memory: 56.1 MB (20.00%) - LeetSync

--- minimum-size-subarray-sum/minimum-size-subarray-sum.java
+++ minimum-size-subarray-sum/minimum-size-subarray-sum.java
@@ -1,14 +1,24 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
-        int max = 100001, left = 0, right = 0, sum = 0, ans = max;
-        while(right < nums.length) {
-            sum += nums[right];
-            while(left <= right && sum >= target) {
-                ans = Math.min(ans, right-left+1);
-                sum -= nums[left++];
+        //if(nums.length == 1) return nums[0] >= target ? 1 : 0;
+        int[] arr = new int[nums.length+1];
+        int ans = 100001;
+        for(int i=0; i<nums.length; i++) arr[i+1] = nums[i] + arr[i];
+        nums = arr;
+        //System.out.println(Arrays.toString(nums));
+        for(int i=1; i<nums.length; i++) {
+            int complement = nums[i-1] + target;
+            //if(target >= nums[i]) complement = target - nums[i];
+            //else complement = nums[i] - target;
+            int left = i, right = nums.length-1;
+            while(left < right) {
+                int mid = left + (right - left) / 2;
+                if(nums[mid] < complement) left = mid + 1;
+                else right = mid;
             }
-            right++;
+            //System.out.println(left + " " +i);
+            if(nums[left] - nums[i-1] >= target) ans = Math.min(ans, left-i+1);
         }
-        return ans == max ? 0 : ans;
+        return ans == 100001 ? 0 : ans;
     }
 }
