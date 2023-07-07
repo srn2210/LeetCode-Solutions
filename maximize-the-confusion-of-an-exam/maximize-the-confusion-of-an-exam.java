1d4f5bec5eedf9e380136480596677f52eeeb605
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-07-08 01:09:51 +0530

    Time: 267 ms (5.00%) | Memory: 43.7 MB (51.53%) - LeetSync

---
+++ maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.java
@@ -0,0 +1,33 @@
+class Solution {
+    int getMax(String str, int k, char c) {
+        int left = 0, right = 0, max = 0;
+        while(right < str.length()) {
+            var ch = str.charAt(right);
+            if(ch == c && k > 0) k--;
+            else if(ch == c && k == 0) {
+                while(left <= right && str.charAt(left) != c) {
+                    left++;
+                }
+                if(left <= right) left++;
+                //System.out.println(left + "  " + right + "   " + c);
+            }
+            max = Math.max(max, right - left + 1);
+            right++;
+        }
+        return max;
+    }
+    boolean safe(int cand, String str, int k) {
+        //System.out.println(" ----- " + cand + " ----- ");
+        return Math.max(getMax(str, k, 'T'), getMax(str, k, 'F')) >= cand;
+
+    }
+    public int maxConsecutiveAnswers(String answerKey, int k) {
+        int n = answerKey.length(), left = 1, right = n;
+        while(left < right) {
+            int mid = (left + (right - left) / 2) + 1;
+            if(safe(mid, answerKey, k)) left = mid;
+            else right = mid - 1;
+        }
+        return left;
+    }
+}
