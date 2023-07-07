b90b9faf3b13b6347440ae93cb31920c6946da98
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-07-08 01:11:32 +0530

    Time: 21 ms (43.44%) | Memory: 44 MB (27.61%) - LeetSync

--- maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.java
+++ maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.java
@@ -9,7 +9,6 @@ class Solution {
                     left++;
                 }
                 if(left <= right) left++;
-                //System.out.println(left + "  " + right + "   " + c);
             }
             max = Math.max(max, right - left + 1);
             right++;
@@ -17,17 +16,17 @@ class Solution {
         return max;
     }
     boolean safe(int cand, String str, int k) {
-        //System.out.println(" ----- " + cand + " ----- ");
         return Math.max(getMax(str, k, 'T'), getMax(str, k, 'F')) >= cand;
 
     }
     public int maxConsecutiveAnswers(String answerKey, int k) {
-        int n = answerKey.length(), left = 1, right = n;
+        /*int n = answerKey.length(), left = 1, right = n;
         while(left < right) {
             int mid = (left + (right - left) / 2) + 1;
             if(safe(mid, answerKey, k)) left = mid;
             else right = mid - 1;
         }
-        return left;
+        return left;*/
+        return Math.max(getMax(answerKey, k, 'T'), getMax(answerKey, k, 'F'));
     }
 }
