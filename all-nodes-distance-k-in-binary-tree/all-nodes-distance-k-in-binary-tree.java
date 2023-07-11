724b2cbd59af995c57452336734d9ce542480e7f
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-07-11 21:03:44 +0530

    Time: 15 ms (12.27%) | Memory: 43.9 MB (5.44%) - LeetSync

--- all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
+++ all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
@@ -8,29 +8,7 @@
  * }
  */
 class Solution {
-    /*int ref;
-    int dfs(TreeNode node, TreeNode target, int k, List<Integer> ans) {
-        if(node == null) return 0;
-        if(k == 0) ans.add(node.val);
-        if(flag) {
-            int left = node.left == null ? 0 : 1 + dfs(node.left, target, k-1, ans);
-            int right = node.right == null ? 0 : 1 + dfs(node.right, target, k-1, ans);
-            if(left + right == ref) ans.add(node.val);
-        }
-        else {
-            int left = node.left == null ? 0 : 1 + dfs(node.left, target, k, ans);
-            int right = node.right == null ? 0 : 1 + dfs(node.right, target, k, ans);
-            if(left + right == ref) ans.add(node.val);
-        }
-        if(node.val == target.val) return 1;
-        else return 0;
-    }
-    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
-        var ans = new ArrayList<Integer>();
-        ref = k;
-        dfs(root, target, k, ans);
-        return ans;
-    }*/
+    
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
         var adj = new HashMap<Integer, List<Integer>>();
         var g = new LinkedList<TreeNode>();
@@ -58,14 +36,7 @@ class Solution {
         int dist = 0;
         while(!q.isEmpty()) {
             var s = q.size();
-            if(dist == k) {
-                /*var ans = new ArrayList<Integer>();
-                while(!q.isEmpty()) {
-                    ans.add(q.poll());
-                }
-                return ans;*/
-                return q;
-            }
+            if(dist == k) return q;
             while(s-- > 0) {
                 var node = q.poll();
                 if(!adj.containsKey(node)) continue;
