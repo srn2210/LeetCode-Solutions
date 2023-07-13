8448c65b6e52d2c80e6744960dc21a6f4a7bfb29
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-07-14 00:01:04 +0530

    Time: 6 ms (53.11%) | Memory: 44.8 MB (13.06%) - LeetSync

---
+++ course-schedule/course-schedule.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        var adj = new HashMap<Integer, List<Integer>>();
+        var q = new LinkedList<Integer>();
+        var indegree = new int[numCourses];
+        for(int[] req : prerequisites) {
+            adj.computeIfAbsent(req[1], a -> new ArrayList<>()).add(req[0]);
+            indegree[req[0]]++;
+        }
+        for(int i=0; i<numCourses; i++) {
+            if(indegree[i] == 0) q.add(i);
+        }
+        while(!q.isEmpty()) {
+            var curr = q.poll();
+            if(!adj.containsKey(curr)) continue;
+            for(int neigh : adj.get(curr)) {
+                //indegree[neigh]--;
+                if(--indegree[neigh] == 0) q.add(neigh);
+            }
+        }
+        for(int i : indegree) if(i != 0) return false;
+        return true;
+    }
+}
