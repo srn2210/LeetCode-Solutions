c1a0264b48ad7e4d96601decb8441e425707a704
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-07-19 16:42:06 +0530

    Time: 3 ms (47.47%) | Memory: 43.8 MB (46.11%) - LeetSync

--- 445-add-two-numbers-ii/add-two-numbers-ii.java
+++ 445-add-two-numbers-ii/add-two-numbers-ii.java
@@ -9,34 +9,32 @@
  * }
  */
 class Solution {
-    ListNode reverse(ListNode list) {
-        ListNode prev = list;
-        list = list.next;
-        prev.next = null;
+    void push(ListNode list, ArrayDeque<Integer> st) {
         while(list != null) {
-            ListNode next = list.next;
-            list.next = prev;
-            prev = list;
-            list = next;
+            st.push(list.val);
+            list = list.next;
         }
-        return prev;
     }
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        l1 = reverse(l1);
-        l2 = reverse(l2);
+        var st1 = new ArrayDeque<Integer>();
+        var st2 = new ArrayDeque<Integer>();
+        push(l1, st1);
+        push(l2, st2);
         int carry = 0;
-        ListNode ans = new ListNode(0);
-        ListNode curr = ans;
-        while(l1 != null || l2 != null || carry != 0) {
-            int l1val = l1 == null ? 0 : l1.val;
-            int l2val = l2 == null ? 0 : l2.val;
-            int val = l1val + l2val + carry;
-            curr.next = new ListNode(val%10);
-            curr = curr.next;
+        var st3 = new ArrayDeque<Integer>();
+        ListNode ans = new ListNode();
+        ListNode res = ans;
+        while(!st1.isEmpty() || !st2.isEmpty() || carry != 0) {
+            int val1 = st1.isEmpty() ? 0 : st1.pop();
+            int val2 = st2.isEmpty() ? 0 : st2.pop();
+            int val = val1 + val2 + carry;
+            st3.push(val%10);
             carry = val / 10;
-            l1 = l1 == null ? l1 : l1.next;
-            l2 = l2 == null ? l2 : l2.next;
         }
-        return reverse(ans.next);
+        while(!st3.isEmpty()) {
+            ans.next = new ListNode(st3.pop());
+            ans = ans.next;
+        }
+        return res.next;
     }
 }
