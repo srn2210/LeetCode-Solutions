2abe7ed9fbc823b05c920064c153f5a457866367
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-07-20 18:04:19 +0530

    Time: 6 ms (88.29%) | Memory: 44 MB (65.19%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -3,18 +3,14 @@ class Solution {
         var st = new ArrayDeque<Integer>();
         for(int i=0; i<asteroids.length; i++) {
             int curr = asteroids[i];
-            if(st.isEmpty() || (st.peek() > 0 && curr > 0) || (st.peek() < 0 && curr < 0)) st.push(curr);
+            if(st.isEmpty() || (st.peek() > 0 && curr > 0) || (st.peek() < 0 && curr < 0) || (st.peek() < 0 && curr > 0)) st.push(curr);
             else if(st.peek() > 0 && curr < 0) {
-                if(Math.abs(curr) > Math.abs(st.peek())) {
-                    while(!st.isEmpty() && st.peek() > 0 && Math.abs(st.peek()) < Math.abs(curr)) st.pop();
-                    if(!st.isEmpty() && st.peek() > 0) {
-                        if(Math.abs(st.peek()) == Math.abs(curr)) st.pop();
-                    }
-                    else st.push(curr);
+                while(!st.isEmpty() && st.peek() > 0 && Math.abs(st.peek()) < Math.abs(curr)) st.pop();
+                if(!st.isEmpty() && st.peek() > 0) {
+                    if(Math.abs(st.peek()) == Math.abs(curr)) st.pop();
                 }
-                else if(Math.abs(curr) == Math.abs(st.peek())) st.pop();
+                else st.push(curr);
             }
-            else if(st.peek() < 0 && curr > 0) st.push(curr);
         }
         int[] ans = new int[st.size()];
         int i = st.size()-1;
