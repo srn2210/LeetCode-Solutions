329dff889ab17a92b7fdf317cdcd13add7f4b286
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-07-20 18:08:09 +0530

    Time: 5 ms (93.26%) | Memory: 43.9 MB (78.00%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -3,14 +3,14 @@ class Solution {
         var st = new ArrayDeque<Integer>();
         for(int i=0; i<asteroids.length; i++) {
             int curr = asteroids[i];
-            if(st.isEmpty() || (st.peek() > 0 && curr > 0) || (st.peek() < 0 && curr < 0) || (st.peek() < 0 && curr > 0)) st.push(curr);
-            else if(st.peek() > 0 && curr < 0) {
+            if(!st.isEmpty() && st.peek() > 0 && curr < 0) {
                 while(!st.isEmpty() && st.peek() > 0 && Math.abs(st.peek()) < Math.abs(curr)) st.pop();
                 if(!st.isEmpty() && st.peek() > 0) {
                     if(Math.abs(st.peek()) == Math.abs(curr)) st.pop();
                 }
                 else st.push(curr);
             }
+            else st.push(curr);
         }
         int[] ans = new int[st.size()];
         int i = st.size()-1;
