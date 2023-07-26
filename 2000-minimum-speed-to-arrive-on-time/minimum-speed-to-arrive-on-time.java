bfb644fbf0b7d83c7bd381b51a1d62ad5cd024fe
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-07-27 00:00:09 +0530

    Time: 244 ms (26.99%) | Memory: 58.9 MB (81.66%) - LeetSync

---
+++ 2000-minimum-speed-to-arrive-on-time/minimum-speed-to-arrive-on-time.java
@@ -0,0 +1,20 @@
+class Solution {
+    boolean safe(int speed, int[] dist, double hour) {
+        double time = 0;
+        for(int i=0; i<dist.length; i++) {
+            double t = dist[i] / (double) speed;
+            if(i != dist.length-1) t = Math.ceil(t);
+            time += t;
+        }
+        return time <= hour;
+    }
+    public int minSpeedOnTime(int[] dist, double hour) {
+        int max = (int)(1e9), left = 1, right = max;
+        while(left < right) {
+            int mid = left + (right - left) / 2;
+            if(safe(mid, dist, hour)) right = mid;
+            else left = mid + 1;
+        }
+        return left == max ? -1 : left;
+    }
+}
