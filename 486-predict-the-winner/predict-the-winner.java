db9038851140d52ee1c71d3f2ee90171be9f52ee
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-07-28 23:20:40 +0530

    Time: 0 ms (100.00%) | Memory: 39.4 MB (99.12%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.java
+++ 486-predict-the-winner/predict-the-winner.java
@@ -1,11 +1,14 @@
 class Solution {
-    int solve(int[] nums, int start, int end) {
+    int solve(int[] nums, int start, int end, int[][] dp) {
         if(start > end) return 0;
+        if(dp[start][end] != -1) return dp[start][end];
         int ans = 0;
-        ans = Math.max(nums[start] - solve(nums, start+1, end), nums[end] - solve(nums, start, end-1));
-        return ans;
+        ans = Math.max(nums[start] - solve(nums, start+1, end, dp), nums[end] - solve(nums, start, end-1, dp));
+        return dp[start][end] = ans;
     }
     public boolean PredictTheWinner(int[] nums) {
-        return solve(nums, 0, nums.length-1) >= 0;
+        int[][] dp = new int[nums.length][nums.length];
+        for(int i=0; i<dp.length; i++) Arrays.fill(dp[i], -1);
+        return solve(nums, 0, nums.length-1, dp) >= 0;
     }
 }
