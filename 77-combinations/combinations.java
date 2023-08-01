92e988d7059e75d06eae70b1e96bec0ef3d1de8d
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-08-02 00:53:33 +0530

    Time: 6 ms (84.80%) | Memory: 45.4 MB (65.09%) - LeetSync

--- 77-combinations/combinations.java
+++ 77-combinations/combinations.java
@@ -1,30 +1,17 @@
 class Solution {
     List<List<Integer>> res;
-    boolean[] vis;
-    void solve(int n, int k, int mask) {
-        if(k == 0) {
+    public List<List<Integer>> combine(int n, int k) {
+        res = new ArrayList<>();
+        for(int j=0; j<(1 << n); j++) {
+            if(Integer.bitCount(j) != k) continue;
             var ans = new ArrayList<Integer>();
             for(int i=0; i<n; i++) {
-                if((mask & (1 << i)) > 0) {
+                if((j & (1 << i)) > 0) {
                     ans.add(i+1);
                 }
             }
             res.add(ans);
-            return;
         }
-        else {
-            for(int i=0; i<n; i++) {
-                if((mask & (1 << i)) == 0 && !vis[(mask | (1 << i))]) {
-                    vis[(mask | (1 << i))] = true;
-                    solve(n, k-1, mask | (1 << i));
-                }
-            }
-        }
-    }
-    public List<List<Integer>> combine(int n, int k) {
-        res = new ArrayList<>();
-        vis = new boolean[(1 << n)];
-        solve(n, k, 0);
         return res;
     }
 }
