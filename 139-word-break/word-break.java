3ceef3366d5c343d6d9cdb32ba22786eb7864e7e
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-08-04 20:39:19 +0530

    Time: 6 ms (64.15%) | Memory: 43.8 MB (28.12%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -1,17 +1,17 @@
 class Solution {
-    Boolean[][] dp;
-    boolean solve(String s, Set<String> set, int start, int end) {
-        if(start > end) return true;
-        if(dp[start][end] != null) return dp[start][end];
+    Boolean[] dp;
+    boolean solve(String s, Set<String> set, int curr) {
+        if(curr >= s.length()) return true;
+        if(dp[curr] != null) return dp[curr];
         boolean ans = false;
-        for(int i=start; i<=end+1; i++) {
-            ans = ans || (set.contains(s.substring(start, i)) && solve(s, set, i, end));
+        for(int i=curr; i<=s.length(); i++) {
+            ans = ans || (set.contains(s.substring(curr, i)) && solve(s, set, i));
         }
-        return dp[start][end] = ans;
+        return dp[curr] = ans;
     }
     public boolean wordBreak(String s, List<String> wordDict) {
         var set = new HashSet<String>(wordDict);
-        dp = new Boolean[s.length()][s.length()];
-        return solve(s, set, 0, s.length()-1);
+        dp = new Boolean[s.length()];
+        return solve(s, set, 0);
     }
 }
