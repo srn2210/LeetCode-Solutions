e9d0869d0863951587cf36090e3604a614e4bd10
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-08-06 22:32:39 +0530

    Time: 8 ms (28.66%) | Memory: 44.8 MB (45.99%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,34 +1,33 @@
 class Solution {
-    boolean flag = false;
-    void dfs(int src, List<List<Integer>> adj, int[] visited, Stack<Integer> st) {
-        visited[src] = 1;
-        for(int neigh : adj.get(src)) {
-            if(visited[neigh] == 1) flag = true;
-            else if(visited[neigh] == 0) dfs(neigh, adj, visited, st);
-            else continue;
-        }
-        st.push(src);
-        visited[src] = 2;
-    }
+    
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         var adj = new ArrayList<List<Integer>>();
         var ans = new int[numCourses];
+        int[] indegree = new int[numCourses];
         for(int i=0; i<numCourses; i++) {
             adj.add(new ArrayList<>());
         }
         for(int[] edge : prerequisites) {
             adj.get(edge[1]).add(edge[0]);
+            indegree[edge[0]]++;
         }
-        var st = new Stack<Integer>();
-        int[] visited = new int[numCourses];
-        for(int i=0; i<numCourses; i++) {
-            if(visited[i] == 0) {
-                dfs(i, adj, visited, st);
+        var q = new LinkedList<Integer>();
+        for(int i=0; i<numCourses; i++) if(indegree[i] == 0) q.add(i);
+        var list = new LinkedList<Integer>();
+        while(!q.isEmpty()) {
+            var node = q.poll();
+            list.add(node);
+            for(int edge : adj.get(node)) {
+                indegree[edge]--;
+                if(indegree[edge] == 0) q.add(edge);
             }
         }
-        if(flag) return new int[0];
-        int ptr = 0;
-        while(!st.isEmpty()) ans[ptr++] = st.pop();
+        if(list.size() != numCourses) return new int[0];
+        else {
+            for(int i=0; i<list.size(); i++) {
+                ans[i] = list.get(i);
+            }
+        }
         return ans;
     }
 }
