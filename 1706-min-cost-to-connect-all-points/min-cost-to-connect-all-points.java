d0a678788db3fb0e509c6fc358e4a8bda5d63de4
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-08-10 21:14:44 +0530

    Time: 494 ms (37.69%) | Memory: 222 MB (5.03%) - LeetSync

---
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int minCostConnectPoints(int[][] points) {
+        var adj = new ArrayList<List<Pair<Integer, Integer>>>();
+        for(int i=0; i<points.length; i++) adj.add(new ArrayList<>());
+        for(int i=0; i<points.length; i++) {
+            for(int j=i+1; j<points.length; j++) {
+                adj.get(i).add(new Pair<>(Math.abs(points[i][0]-points[j][0]) + Math.abs(points[i][1]-points[j][1]), j));
+                adj.get(j).add(new Pair<>(Math.abs(points[i][0]-points[j][0]) + Math.abs(points[i][1]-points[j][1]), i));
+            }
+        }
+        var pq = new PriorityQueue<List<Integer>>((a,b) -> Integer.compare(a.get(0), b.get(0)));
+        //System.out.println(adj);
+        int ans = 0;
+        boolean[] vis = new boolean[points.length];
+        int edgeCount = 0;
+        pq.add(List.of(0, 0));
+        while(!pq.isEmpty()) {
+            var list = pq.poll();
+            int cost = list.get(0);
+            int node = list.get(1);
+            //int parent = list.get(2);
+            //System.out.println(pq);
+            
+            if(edgeCount == points.length) break;
+            if(!vis[node]) {
+                //System.out.println(cost + " --- " + node + " ---- " + parent);
+                ans += cost;
+                edgeCount++;
+                vis[node] = true;
+            }
+            
+            for(var edge : adj.get(node)) {
+                int c = edge.getKey();
+                int neigh = edge.getValue();
+                if(!vis[neigh]) {
+                    pq.add(List.of(c, neigh));
+                }
+            }
+        }
+        return ans;
+    }
+}
