8c53f1faf685a51dfd0052afd37c20e99ef5bc0f
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-08-10 19:24:36 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (84.36%) - LeetSync

---
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int left = 0, right = nums.length-1;
+        while(left < right) {
+            int mid = left + (right - left) / 2;
+            if(nums[left] <= nums[mid]) {
+                if(nums[left] <= target && target <= nums[mid]) {
+                    right = mid;
+                }
+                else left = left + 1;
+            }
+            else {
+                if(nums[mid] <= target && target <= nums[right]) left = mid;
+                else right = right - 1;
+            }
+        }
+        return nums[left] == target;
+    }
+}
