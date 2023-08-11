ea39f418c44bd3864dee144932294ef7f9aff5d2
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-08-11 23:46:02 +0530

    Time: 160 ms (5.48%) | Memory: 46.1 MB (21.42%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -3,17 +3,16 @@ class Solution {
     int solve(int amount, int[] coins, int idx) {
         if(amount == 0) return 1;
         if(idx >= coins.length || amount < 0) return 0;
-        if(dp[amount][idx] != -1) return dp[amount][idx];
+        if(dp[idx][amount] != -1) return dp[idx][amount];
         int ans = 0;
         for(int i=idx; i<coins.length; i++) {
             ans += solve(amount-coins[i], coins, i);
         }
-        return dp[amount][idx] = ans;
+        return dp[idx][amount] = ans;
     }
     public int change(int amount, int[] coins) {
-        Arrays.sort(coins);
-        dp = new int[amount+1][coins.length];
-        for(int i=0; i<=amount; i++) Arrays.fill(dp[i], -1);
+        dp = new int[coins.length][amount+1];
+        for(int i=0; i<coins.length; i++) Arrays.fill(dp[i], -1);
         return solve(amount, coins, 0);
     }
 }
