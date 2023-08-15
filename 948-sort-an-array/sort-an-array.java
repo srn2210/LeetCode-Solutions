bcb576cd08e795fabd45e19f752fb0343e09e38c
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-08-15 10:27:41 +0530

    Time: 208 ms (11.66%) | Memory: 55.1 MB (59.01%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,52 +1,88 @@
+public class Heap {
+    List<Integer> arr;
+    Heap() {
+        arr = new ArrayList<>();
+    }
+    int parent(int num) {
+        return (num-1)/2;
+    }
+    int left(int num) {
+        return (2 * num) + 1;
+    }
+    int right(int num) {
+        return (2 * num) + 2;
+    }
+    void swap(int i, int j) {
+        int temp = arr.get(i);
+        arr.set(i, arr.get(j));
+        arr.set(j, temp);
+    }
+    void bubbleUp(int idx) {
+        while(idx != 0) {
+            int par = parent(idx);
+            int curr = arr.get(idx);
+            int parVal = arr.get(par);
+            if(parVal > curr) {
+                swap(par, idx);
+                idx = par;
+            }
+            else break;
+        }
+    }
+    void bubbleDown(int num) {
+        while(num < arr.size()) {
+            int left = left(num);
+            int right = right(num);
+            int idx = num;
+            int curr = arr.get(num);
+            int leftVal = left < arr.size() ? arr.get(left) : (int)1e9;
+            int rightVal = right < arr.size() ? arr.get(right) : (int)1e9;
+            if(leftVal < curr) {
+                curr = leftVal;
+                idx = left;
+            }
+            if(rightVal < curr) {
+                idx = right;
+            }
+            if(idx != num) {
+                swap(idx, num);
+                num = idx;
+            }
+            else break;
+        }
+    }
+    void add(int num) {
+        arr.add(num);
+        int last = arr.size()-1;
+        bubbleUp(last);
+    }
+    int remove() {
+        int ans = arr.get(0);
+        swap(0, arr.size()-1);
+        arr.remove(arr.size()-1);
+        bubbleDown(0);
+        return ans;
+    }
+    int peek() {
+        return arr.get(0);
+    }
+    int size() {
+        return arr.size();
+    }
+    boolean empty() {
+        return size() == 0;
+    }
+    public String toString() {
+        return arr.toString();
+    }
+}
 class Solution {
-    private void swap(int[] arr, int index1, int index2) {
-        int temp = arr[index1];
-        arr[index1] = arr[index2];
-        arr[index2] = temp;
-    }
-    
-    // Function to heapify a subtree (in top-down order) rooted at index i.
-    private void heapify(int[] arr, int n, int i) {
-        // Initialize largest as root 'i'.
-        int largest = i; 
-        int left = 2 * i + 1;
-        int right = 2 * i + 2; 
-
-        // If left child is larger than root.
-        if (left < n && arr[left] > arr[largest]) {
-            largest = left;
-        }
-
-        // If right child is larger than largest so far.
-        if (right < n && arr[right] > arr[largest]) {
-            largest = right;
-        }
-
-        // If largest is not root swap root with largest element
-        // Recursively heapify the affected sub-tree (i.e. move down).
-        if (largest != i) {
-            swap(arr, i, largest); 
-            heapify(arr, n, largest);
-        }
-    }
-
-    private void heapSort(int[] arr) {
-        int n = arr.length;
-        // Build heap; heapify (top-down) all elements except leaf nodes.
-        for (int i = n / 2 - 1; i >= 0; i--) {
-            heapify(arr, n, i);
-        }
-
-        // Traverse elements one by one, to move current root to end, and
-        for (int i = n - 1; i >= 0; i--) {
-            swap(arr, 0, i);
-            // call max heapify on the reduced heap.
-            heapify(arr, i, 0);
-        }
-    }
-
     public int[] sortArray(int[] nums) {
-        heapSort(nums);
+        Heap pq = new Heap();
+        for(int num : nums) pq.arr.add(num);
+        for(int i=nums.length/2-1; i>=0; i--) pq.bubbleDown(i);
+        int i = 0;
+        while(!pq.empty()) nums[i++] = pq.remove();
         return nums;
     }
 }
