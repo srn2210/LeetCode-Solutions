8477ebb3067fa9c9637294bc8d620ed855969e76
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-08-15 14:03:45 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (18.49%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -14,47 +14,34 @@ class Node {
 */
 
 class Solution {
-    int getIndex(Node random) {
-        int count = 0;
-        while(random != null) {
-            random = random.next;
-            count++;
-        }
-        return count;
-    }
     public Node copyRandomList(Node head) {
         var curr = head;
-        int len = getIndex(head);
-        //System.out.println("len = "+len);
-        int idx = 0;
         var dummy = new Node(0);
-        var newNode = dummy;
-        var map = new HashMap<Integer, Node>();
+        var curr2 = dummy;
+        var map = new HashMap<Node, Node>();
         while(curr != null) {
-            if(map.containsKey(idx)) {
-                newNode.next = map.get(idx);
-                newNode = newNode.next;
+            if(map.containsKey(curr)) {
+                curr2.next = map.get(curr);
+                curr2 = curr2.next;
             }
             else {
                 Node t = new Node(curr.val);
-                newNode.next = t;
-                newNode = t;
-                map.put(idx, t);
+                curr2.next = t;
+                curr2 = curr2.next;
+                map.put(curr, t);
             }
             if(curr.random != null) {
-                int ind = getIndex(curr.random);
-                ind = len - ind;
-                //System.out.println(idx + " is the idx and the index is"+ind);
-                if(map.containsKey(ind)) {
-                    newNode.random = map.get(ind);
+                if(map.containsKey(curr.random)) {
+                    curr2.random = map.get(curr.random);
+                    //curr2 = curr2.next;
                 }
                 else {
                     Node t = new Node(curr.random.val);
-                    map.put(ind, t);
-                    newNode.random = t;
+                    curr2.random = t;
+                    //curr2 = curr2.next;
+                    map.put(curr.random, t);
                 }
             }
-            idx++;
             curr = curr.next;
         }
         return dummy.next;
