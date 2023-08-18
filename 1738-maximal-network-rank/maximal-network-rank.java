ec9497483ea8a9acafc27ca0940027b26ef65a47
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-08-18 16:40:03 +0530

    Time: 19 ms (53.10%) | Memory: 43.9 MB (83.25%) - LeetSync

--- 1738-maximal-network-rank/maximal-network-rank.java
+++ 1738-maximal-network-rank/maximal-network-rank.java
@@ -1,24 +1,20 @@
 class Solution {
     public int maximalNetworkRank(int n, int[][] roads) {
         var adj = new ArrayList<Set<Integer>>();
-        int[][] out = new int[n][2];
         for(int i=0; i<n; i++) {
             adj.add(new HashSet<>());
-            out[i][0] = i;
         }
         for(int[] road : roads) {
             adj.get(road[0]).add(road[1]);
             adj.get(road[1]).add(road[0]);
-            out[road[0]][1]++;
-            out[road[1]][1]++;
         }
         int ans = 0;
         for(int i=0; i<n; i++) {
-            int node = out[i][0];
-            int edges = out[i][1];
+            int node = i;
+            int edges = adj.get(i).size();
             for(int j=0; j<n; j++) {
-                int node2 = out[j][0];
-                int edges2 = out[j][1];
+                int node2 = j;
+                int edges2 = adj.get(j).size();
                 if(node == node2) continue;
                 else {
                     int sum = edges + edges2;
