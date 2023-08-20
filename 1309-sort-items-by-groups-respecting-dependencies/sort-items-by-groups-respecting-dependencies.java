2fd26e77dfe4b5a4c57ab94f1a334c923f73ecc7
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-08-21 00:52:36 +0530

    Time: 163 ms (7.53%) | Memory: 66.5 MB (13.98%) - LeetSync

---
+++ 1309-sort-items-by-groups-respecting-dependencies/sort-items-by-groups-respecting-dependencies.java
@@ -0,0 +1,93 @@
+class Solution {
+    boolean flag;
+    void dfs(int grp, Map<Integer, Set<Integer>> grpDep, int[] vis, Deque<Integer> st) {
+        vis[grp] = 1;
+        if(!grpDep.containsKey(grp)) {
+            vis[grp] = 2;
+            st.addLast(grp);
+            return;
+        }
+        for(int dep : grpDep.get(grp)) {
+            if(vis[dep] == 0) {
+                dfs(dep, grpDep, vis, st);
+            }
+            else if(vis[dep] == 1) flag = true;
+        }
+        vis[grp] = 2;
+        st.addLast(grp);
+    }
+    public int[] sortItems(int n, int m, int[] group, List<List<Integer>> beforeItems) {
+        int gID = m;
+        flag = false;
+        //int[] idToi = new int[n];
+        for(int i=0; i<n; i++) {
+            if(group[i] == -1) group[i] = gID++;
+        //    idToi[group[i]]
+        }
+        var grpDep = new HashMap<Integer, Set<Integer>>();
+        var intDep = new HashMap<Integer, Set<Integer>>();
+        var grpMap = new HashMap<Integer, Set<Integer>>();
+        for(int i=0; i<n; i++) {
+            //grpDep.computeIfAbsent(group[i], a -> new HashSet<>()).addAll(beforeItems.get(i))
+            if(!grpDep.containsKey(group[i])) grpDep.put(group[i], new HashSet<>());
+            if(!intDep.containsKey(i)) intDep.put(i, new HashSet<>());
+            if(!grpMap.containsKey(group[i])) grpMap.put(group[i], new HashSet<>());
+            grpMap.get(group[i]).add(i);
+            for(int neigh : beforeItems.get(i)) {
+                if(group[i] == group[neigh]) {
+                    intDep.get(i).add(neigh);
+                }
+                else {
+                    grpDep.get(group[i]).add(group[neigh]);
+                }
+            }
+        }
+        //System.out.println(grpDep);
+        m = gID;
+        var st = new ArrayDeque<Integer>();
+        int[] vis = new int[m];
+        for(int i : grpMap.keySet()) {
+            if(vis[i] == 0) {
+                dfs(i, grpDep, vis, st);
+            }
+        }
+        if(flag) return new int[0];
+
+
+        var map = new HashMap<Integer, List<Integer>>();
+        for(var entry : grpMap.entrySet()) {
+            int grpId = entry.getKey();
+            var set = entry.getValue();
+            var intSt = new ArrayDeque<Integer>();
+            int[] visited = new int[n];
+            for(int i : set) {
+                if(visited[i] == 0) {
+                    dfs(i, intDep, visited, intSt);
+                }
+            }
+            var arr = new ArrayList<Integer>();
+            while(!intSt.isEmpty()) arr.add(intSt.pollFirst());
+            map.put(grpId, arr);
+        }
+        if(flag) return new int[0];
+        int[] ans = new int[n];
+        int ind = 0;
+        while(!st.isEmpty()) {
+            int grpId = st.pollFirst();
+            if(map.containsKey(grpId)) {
+                for(int i : map.get(grpId)) {
+                    ans[ind++] = i;
+                }
+            }
+            else {
+                //System.out.println(beforeItems);
+                //System.out.println(grpMap);
+                //System.out.println(grpId);
+                for(int i : grpMap.get(grpId)) {
+                    ans[ind++] = i;
+                }
+            }
+        }
+        return ans;
+    }
+}
