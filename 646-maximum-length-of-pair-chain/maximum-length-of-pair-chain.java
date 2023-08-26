88f0351a98296d54baedf04ae25c7052fc7330aa
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-08-27 00:33:11 +0530

    Time: 7 ms (99.63%) | Memory: 43.6 MB (89.47%) - LeetSync

--- 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.java
+++ 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.java
@@ -1,16 +1,28 @@
 class Solution {
-    int[][] dp;
+    /*int[][] dp;
     int solve(int[][] pairs, int idx, int prev) {
         if(idx == pairs.length) return 0;
         if(dp[idx][prev+1001] != -1) return dp[idx][prev+1001];
         int ans = solve(pairs, idx+1, prev);
         if(prev < pairs[idx][0]) ans = Math.max(ans, 1 + solve(pairs, idx+1, pairs[idx][1]));
         return dp[idx][prev+1001] = ans;
-    }
+    }*/
     public int findLongestChain(int[][] pairs) {
-        Arrays.sort(pairs, (a,b) -> Integer.compare(a[0], b[0]) == 0 ? Integer.compare(a[1], b[1]) : Integer.compare(a[0], b[0]));
+        /*Arrays.sort(pairs, (a,b) -> Integer.compare(a[0], b[0]) == 0 ? Integer.compare(a[1], b[1]) : Integer.compare(a[0], b[0]));
         dp = new int[pairs.length][2002];
         for(int i=0; i<pairs.length; i++) Arrays.fill(dp[i], -1);
-        return solve(pairs, 0, -1001);
+        return solve(pairs, 0, -1001);*/
+        var pq = new PriorityQueue<int[]>((a,b) -> Integer.compare(a[1], b[1]));
+        for(int[] pair : pairs) pq.add(pair);
+        int prev = -1001;
+        int ans = 0;
+        while(!pq.isEmpty()) {
+            int[] t = pq.poll();
+            if(prev < t[0]) {
+                prev = t[1];
+                ans++;
+            }
+        }
+        return ans;
     }
 }
