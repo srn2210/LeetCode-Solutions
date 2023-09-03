cae3a526ec096ddabbdb29149b983e333259b0fe
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-09-03 16:52:13 +0530

    Time: 42 ms (36.46%) | Memory: 54.6 MB (73.29%) - LeetSync

--- 2449-maximum-number-of-robots-within-budget/maximum-number-of-robots-within-budget.java
+++ 2449-maximum-number-of-robots-within-budget/maximum-number-of-robots-within-budget.java
@@ -1,21 +1,20 @@
 class Solution {
     public int maximumRobots(int[] chargeTimes, int[] runningCosts, long budget) {
         int left = 0, right = 0, n = chargeTimes.length;
-        var pq = new TreeMap<Integer, Integer>(Collections.reverseOrder());
+        var pq = new PriorityQueue<Integer>((a,b) -> Integer.compare(chargeTimes[b], chargeTimes[a]));
         long sum = 0;
         int ans = 0;
         while(right < n) {
             sum += runningCosts[right];
-            pq.put(chargeTimes[right], pq.getOrDefault(chargeTimes[right], 0) + 1);
-            long max = pq.firstEntry().getKey();
+            pq.add(right);
+            long max = chargeTimes[pq.peek()];
             long cost = max + ((right - left + 1) * sum);
             while(cost > budget) {
                 sum -= runningCosts[left];
-                if(pq.get(chargeTimes[left]) > 1) pq.put(chargeTimes[left], pq.get(chargeTimes[left])-1);
-                else pq.remove(chargeTimes[left]);
-                max = 0;
-                if(pq.size() > 0) max = pq.firstEntry().getKey();
                 left++;
+                while(!pq.isEmpty() && pq.peek() < left) pq.poll();
+                max = 0;
+                if(pq.size() > 0) max = chargeTimes[pq.peek()];
                 cost = max + ((right - left + 1) * sum);
             }
             ans = Math.max(ans, right - left + 1);
