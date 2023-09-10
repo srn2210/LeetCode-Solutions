c8573571ad7f19235a6109ec994d924c656fc61b
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-09-11 00:52:28 +0530

    Time: 41 ms (58.30%) | Memory: 16.6 MB (22.13%) - LeetSync

--- 1461-count-all-valid-pickup-and-delivery-options/count-all-valid-pickup-and-delivery-options.py
+++ 1461-count-all-valid-pickup-and-delivery-options/count-all-valid-pickup-and-delivery-options.py
@@ -1,14 +1,5 @@
 class Solution:
     def countOrders(self, n: int) -> int:
+        if n == 0: return 1
         mod = int(1e9+7)
-        @cache
-        def solve(pick : int, deli : int) -> int:
-            if pick == n and deli == 0: return 1
-            ans = 0
-            remPick = n - pick
-            if remPick > 0:
-                ans += remPick * solve(pick+1, deli+1)
-            if deli > 0:
-                ans += deli * solve(pick, deli-1)
-            return ans % mod
-        return solve(0, 0)
+        return (self.countOrders(n-1) * n * (2*n-1)) % mod
