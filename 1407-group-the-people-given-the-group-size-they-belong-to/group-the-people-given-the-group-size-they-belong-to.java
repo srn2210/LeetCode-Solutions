c79d66de8557483d0283cd528376e1d827fa0455
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-09-11 23:25:11 +0530

    Time: 9 ms (18.91%) | Memory: 44.3 MB (80.52%) - LeetSync

--- 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.java
+++ 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.java
@@ -1,20 +1,28 @@
 class Solution {
     public List<List<Integer>> groupThePeople(int[] groupSizes) {
-        int[][] group = new int[groupSizes.length][2];
+        var map = new HashMap<Integer, List<List<Integer>>>();
         for(int i=0; i<groupSizes.length; i++) {
-            group[i][0] = i;
-            group[i][1] = groupSizes[i];
-        }
-        Arrays.sort(group, (a,b) -> Integer.compare(a[1], b[1]));
-        var ans = new ArrayList<List<Integer>>();
-        for(int i=0; i<group.length;) {
-            int size = group[i][1];
-            var list = new ArrayList<Integer>();
-            for(int j=i; j<i+size; j++) {
-                list.add(group[j][0]);
+            int size = groupSizes[i];
+            map.putIfAbsent(size, new ArrayList<>());
+            var list = map.get(size);
+            if(list.size() == 0) {
+                var t = new ArrayList<Integer>();
+                t.add(i);
+                map.get(size).add(t);
             }
-            ans.add(list);
-            i = i + size;
+            else if(list.get(list.size()-1).size() < size) list.get(list.size()-1).add(i);
+            else {
+                var t = new ArrayList<Integer>();
+                t.add(i);
+                map.get(size).add(t);
+            }
+            
+        }
+        var ans = new ArrayList<List<Integer>>();
+        for(var it : map.values()) {
+            for(var l : it) {
+                ans.add(l);
+            }
         }
         return ans;
     }
