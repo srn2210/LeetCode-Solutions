38531404569a14cc9fc18d098b622dced19c4a4c
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-10-18 22:56:22 +0530

    Time: 16 ms (45.16%) | Memory: 45.7 MB (17.10%) - LeetSync

---
+++ 1275-validate-binary-tree-nodes/validate-binary-tree-nodes.java
@@ -0,0 +1,24 @@
+class Solution {
+    boolean solve(int n, int[] left, int[] right, int idx, Set<Integer> set) {
+        if(set.contains(idx)) return false;
+        set.add(idx);
+        if(left[idx] != -1) {
+            if(!solve(n, left, right, left[idx], set)) return false;
+        }
+        if(right[idx] != -1) {
+            if(!solve(n, left, right, right[idx], set)) return false;
+        }
+        return true;
+    }
+    public boolean validateBinaryTreeNodes(int n, int[] leftChild, int[] rightChild) {
+        var set = new HashSet<Integer>();
+        var root = new HashSet<Integer>();
+        for(int i=0; i<n; i++) root.add(i);
+        for(int i=0; i<n; i++) {
+            root.remove(leftChild[i]);
+            root.remove(rightChild[i]);
+        }
+        if(root.size() != 1) return false;
+        return solve(n, leftChild, rightChild, root.iterator().next(), set) && set.size() == n;
+    }
+}
