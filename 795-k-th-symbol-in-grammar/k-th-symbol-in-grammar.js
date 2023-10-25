f025048aca94b9605b7d26340a15f0368b0c06a8
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-10-26 02:01:47 +0530

    Time: 52 ms (52.81%) | Memory: 42.2 MB (15.73%) - LeetSync

--- 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.js
+++ 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.js
@@ -5,9 +5,9 @@
  */
 var kthGrammar = function(n, k) {
     function solve(n, k) {
-        if(n === 1) return 0;
-        if(k % 2 === 0) return solve(n-1, k/2) ^ 1;
-        else return solve(n-1, (k+1)/2);
+        if(n === 1) return 0
+        if(k % 2 === 0) return solve(n-1, k/2) ^ 1
+        else return solve(n-1, (k+1)/2)
     }
     return solve(n, k)
 };
