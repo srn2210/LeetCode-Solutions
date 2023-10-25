eda60d36e269fbc05e6e13012880b3579a293d83
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-10-25 23:34:13 +0530

    Time: 1 ms (100.00%) | Memory: 39.2 MB (34.33%) - LeetSync

--- 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.java
+++ 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.java
@@ -1,17 +1,17 @@
 class Solution {
-    void solve(int n, int k, Stack<String> st) {
+    void solve(int n, int k, Stack<Integer> st) {
         if(n == 0) return;
-        if(k % 2 == 0) st.push("right");
-        else st.push("left");
+        if(k % 2 == 0) st.push(1);
+        else st.push(0);
         solve(n-1, (int)Math.ceil(k/2.0), st);
     }
     public int kthGrammar(int n, int k) {
-        var st = new Stack<String>();
+        var st = new Stack<Integer>();
         solve(n, k, st);
         int ans = 0;
         while(!st.isEmpty()) {
             var dir = st.pop();
-            if(dir.equals("right")) ans ^= 1;
+            if(dir.equals(1)) ans ^= 1;
         }
         return ans;
     }
