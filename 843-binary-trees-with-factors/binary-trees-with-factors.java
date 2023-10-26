e5b99e79cb5d68de8d52612471c958db571446b1
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-10-26 21:39:30 +0530

    Time: 42 ms (15.56%) | Memory: 44.3 MB (5.93%) - LeetSync

---
+++ 843-binary-trees-with-factors/binary-trees-with-factors.java
@@ -0,0 +1,41 @@
+class Solution {
+    int mod = (int)1e9+7;
+    Map<Integer, Integer> dp;
+    int solve(int[] arr, Map<Integer, Integer> map, int val) {
+        if(dp.containsKey(val)) return dp.get(val);
+        long ans = 1;
+        for(int i=0; i<arr.length; i++) {
+            if(val % arr[i] == 0 && map.containsKey(val/arr[i])) {
+                ans += (((long)solve(arr, map, arr[i]) * solve(arr, map, val/arr[i])) % mod);
+                ans %= mod;
+            }
+        }
+        //if(ans == 0) ans = 1;
+        dp.put(val, (int)ans);
+        return (int)ans;
+    }
+    public int numFactoredBinaryTrees(int[] arr) {
+        var map = new HashMap<Integer, Integer>();
+        for(int i=0; i<arr.length; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0)+1);
+        }
+        dp = new HashMap<Integer, Integer>();
+        for(int i=0; i<arr.length; i++) {
+            int counter = 0;
+            for(int j=0; j<arr.length; j++) {
+                if(arr[i] % arr[j] == 0 && map.containsKey(arr[i] / arr[j])) counter++;
+            }
+            if(counter == 0) dp.put(arr[i], 1);
+        }
+        long ans = 0;
+        Arrays.sort(arr);
+        for(int i=0; i<arr.length; i++) {
+            ans += solve(arr, map, arr[i]);
+            ans %= mod;
+        }
+        
+        //System.out.println(dp);
+        // return (int)(ans + arr.length) % mod;
+        return (int)ans;
+    }
+}
