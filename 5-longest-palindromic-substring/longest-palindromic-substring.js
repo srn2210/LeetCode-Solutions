cc4ca704b10468eea5de8d9b17d0e0186a041bf1
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-10-27 14:31:17 +0530

    Time: 78 ms (63.98%) | Memory: 43.2 MB (83.54%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.js
+++ 5-longest-palindromic-substring/longest-palindromic-substring.js
@@ -6,9 +6,8 @@ var longestPalindrome = function(s) {
     maxLen = 0
     maxStart = 0
     maxEnd = 0
-    for(let i=0; i<s.length; i++) {
-        left = i-1
-        right= i+1
+
+    function iterateInner(left, right) {
         while(left >= 0 && right < s.length) {
             if(s[left] === s[right]) {
                 if(right - left + 1 > maxLen) {
@@ -22,21 +21,16 @@ var longestPalindrome = function(s) {
             right++
         }
     }
+
+    for(let i=0; i<s.length; i++) {
+        left = i-1
+        right= i+1
+        iterateInner(left, right)
+    }
     for(let i=1; i<s.length; i++) {
         left = i-1
         right= i
-        while(left >= 0 && right < s.length) {
-            if(s[left] === s[right]) {
-                if(right - left + 1 > maxLen) {
-                    maxLen = right - left + 1
-                    maxStart = left
-                    maxEnd = right
-                }
-            }
-            else break
-            left--
-            right++
-        }
+        iterateInner(left, right)
     }
     return s.substring(maxStart, maxEnd+1)
 };
