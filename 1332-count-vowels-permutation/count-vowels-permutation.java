b764e605418caf2c011a854cb559aad3d52d386b
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-10-29 00:42:53 +0530

    Time: 23 ms (30.03%) | Memory: 45.4 MB (28.05%) - LeetSync

--- 1332-count-vowels-permutation/count-vowels-permutation.java
+++ 1332-count-vowels-permutation/count-vowels-permutation.java
@@ -2,9 +2,24 @@ class Solution {
     int mod = (int)1e9+7;
     char[] vowels = {'a', 'e', 'i', 'o', 'u'};
     int[][] dp;
+    int idx(char ch) {
+        switch(ch) {
+            case 'a':
+                return 0;
+            case 'e':
+                return 1;
+            case 'i':
+                return 2;
+            case 'o':
+                return 3;
+            case 'u':
+                return 4;
+        }
+        return 0;
+    }
     int solve(char ch, int n) {
         if(n == 0) return 1;
-        if(dp[ch-'a'][n] != -1) return dp[ch-'a'][n];
+        if(dp[idx(ch)][n] != -1) return dp[idx(ch)][n];
         long ans = 0;
         switch(ch) {
             case 'a':
@@ -35,12 +50,12 @@ class Solution {
                 ans %= mod;
                 break;
         }
-        return dp[ch-'a'][n] = (int)ans;
+        return dp[idx(ch)][n] = (int)ans;
     }
     public int countVowelPermutation(int n) {
         int ans = 0;
-        dp = new int[26][n+1];
-        for(int i=0; i<26; i++) Arrays.fill(dp[i], -1);
+        dp = new int[5][n+1];
+        for(int i=0; i<dp.length; i++) Arrays.fill(dp[i], -1);
         for(char ch : vowels) {
             ans += solve(ch, n-1);
             ans %= mod;
