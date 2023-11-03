21f91e5d7b89c4dbc6b9197b26b34ccabb95bc95
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-11-03 12:24:09 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (90.24%) - LeetSync

--- 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.java
+++ 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.java
@@ -1,13 +1,11 @@
 class Solution {
     public List<String> buildArray(int[] target, int n) {
         var ans = new ArrayList<String>();
-        var st = new Stack<Integer>();
         int ptr = 0;
         for(int i=1; i<=n; i++) {
             if(ptr == target.length) break;
             else if(target[ptr] == i) {
                 ans.add("Push");
-                st.push(i);
                 ptr++;
             }
             else {
