a4034f18480f11a0b584cafc8329c7447ff06d72
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-11-04 20:59:38 +0530

    Time: 73 ms (89.62%) | Memory: 101.7 MB (74.39%) - LeetSync

---
+++ 1300-critical-connections-in-a-network/critical-connections-in-a-network.java
@@ -0,0 +1,42 @@
+class Solution {
+    int time;
+    int[] low;
+    int[] disc;
+    List<List<Integer>> bridges;
+    void dfs(int u, int p, List<List<Integer>> graph) {
+        low[u] = disc[u] = ++time;
+        for(int v : graph.get(u)) {
+            if(v == p) continue;
+            if(disc[v] == 0) {
+                dfs(v, u, graph);
+                if(disc[u] < low[v]) {
+                    bridges.add(List.of(u, v));
+                }
+                low[u] = Math.min(low[u], low[v]);
+            }
+            else {
+                low[u] = Math.min(low[u], disc[v]);
+            }
+        }
+    }
+    public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
+        low = new int[n];
+        disc = new int[n];
+        time = 0;
+        bridges = new ArrayList<>();
+        var graph = new ArrayList<List<Integer>>();
+        for(int i=0; i<n; i++) {
+            graph.add(new ArrayList<>());
+        }
+        for(var con : connections) {
+            graph.get(con.get(0)).add(con.get(1));
+            graph.get(con.get(1)).add(con.get(0));
+        }
+        for(int i=0; i<n; i++) {
+            if(disc[i] == 0) {
+                dfs(i, i, graph);
+            }
+        }
+        return bridges;
+    }
+}
