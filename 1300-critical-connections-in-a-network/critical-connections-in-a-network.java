b0128fd5f31f6d1251a88c60d01fb6257a710280
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-11-04 21:15:06 +0530

    Time: 78 ms (80.13%) | Memory: 103.2 MB (69.87%) - LeetSync

--- 1300-critical-connections-in-a-network/critical-connections-in-a-network.java
+++ 1300-critical-connections-in-a-network/critical-connections-in-a-network.java
@@ -3,10 +3,10 @@ class Solution {
     int[] low;
     int[] disc;
     List<List<Integer>> bridges;
-    List<List<Integer>> graph;
+    List<Integer>[] graph;
     void dfs(int u, int p) {
         low[u] = disc[u] = ++time;
-        for(int v : graph.get(u)) {
+        for(int v : graph[u]) {
             if(v == p) continue;
             if(disc[v] == 0) {
                 dfs(v, u);
@@ -24,14 +24,14 @@ class Solution {
         low = new int[n];
         disc = new int[n];
         bridges = new ArrayList<>();
-        graph = new ArrayList<List<Integer>>(n);
+        graph = new ArrayList[n];
         time = 0;
         for(int i=0; i<n; i++) {
-            graph.add(new ArrayList<>());
+            graph[i] = new ArrayList<>();
         }
         for(var con : connections) {
-            graph.get(con.get(0)).add(con.get(1));
-            graph.get(con.get(1)).add(con.get(0));
+            graph[con.get(0)].add(con.get(1));
+            graph[con.get(1)].add(con.get(0));
         }
         for(int i=0; i<n; i++) {
             if(disc[i] == 0) {
