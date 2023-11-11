082c398260d2d1271ab75cf71df30235fa5eca14
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-11-11 13:08:03 +0530

    Time: 3222 ms (10.00%) | Memory: 19.3 MB (76.47%) - LeetSync

---
+++ 2678-design-graph-with-shortest-path-calculator/design-graph-with-shortest-path-calculator.py
@@ -0,0 +1,39 @@
+class Graph:
+
+    def __init__(self, n: int, edges: List[List[int]]):
+        d = int(1e8)
+        self.graph = [[d] * n for i in range(n)]
+
+        for u, v, c in edges:
+            self.graph[u][v] = c
+        
+        for k in range(n):
+            for i in range(n):
+                for j in range(n):
+                    if i == j:
+                        self.graph[i][j] = 0
+                    elif self.graph[i][k] + self.graph[k][j] < self.graph[i][j]:
+                        self.graph[i][j] = self.graph[i][k] + self.graph[k][j]
+
+
+    def addEdge(self, edge: List[int]) -> None:
+        u, v, c = edge[0], edge[1], edge[2]
+
+        if self.graph[u][v] > c:
+            self.graph[u][v] = c
+            self.update(u, v, c)
+
+    def update(self, u: int, v: int, c: int) -> None:
+        for i in range(len(self.graph)):
+            for j in range(len(self.graph)):
+                if self.graph[i][u] + self.graph[v][j] + c < self.graph[i][j]:
+                    self.graph[i][j] = self.graph[i][u] + self.graph[v][j] + c
+
+    def shortestPath(self, node1: int, node2: int) -> int:
+        return self.graph[node1][node2] if self.graph[node1][node2] != int(1e8) else -1
+
+
+# Your Graph object will be instantiated and called as such:
+# obj = Graph(n, edges)
+# obj.addEdge(edge)
+# param_2 = obj.shortestPath(node1,node2)
