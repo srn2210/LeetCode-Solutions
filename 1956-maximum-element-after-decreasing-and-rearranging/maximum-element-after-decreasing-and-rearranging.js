0f79f772da412d3061262fb1b8e82e6d8fb2c740
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-11-15 23:44:21 +0530

    Time: 75 ms (38.10%) | Memory: 54.9 MB (9.52%) - LeetSync

---
+++ 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var maximumElementAfterDecrementingAndRearranging = function(arr) {
+    const n = arr.length;
+    const ar = new Array(n+1).fill(0);
+    for(let num of arr) {
+        ar[Math.min(num, n)]++;
+    }
+    let ans = 1;
+    for(let i=1; i<=n; i++) {
+        ans = Math.min(i, ans + ar[i]);
+    }
+    return ans;
+};
