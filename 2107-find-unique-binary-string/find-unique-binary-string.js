e96ff1cd2c01a3748899c9c33b3aca233d0fafa1
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-11-16 23:45:41 +0530

    Time: 51 ms (65.88%) | Memory: 42.7 MB (32.94%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.js
+++ 2107-find-unique-binary-string/find-unique-binary-string.js
@@ -10,9 +10,7 @@ var findDifferentBinaryString = function(nums) {
     }
     for(let i=0; i<=nums.length; i++) {
         if(!s.has(i)) {
-            ans = i.toString(2);
-            ans = ans.padStart(nums.length, '0');
-            return ans;
+            return i.toString(2).padStart(nums.length, '0');
         }
     }
     return ans;
