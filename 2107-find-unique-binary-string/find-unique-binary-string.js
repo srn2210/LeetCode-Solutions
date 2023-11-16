c361c2acf6854dccc4a8436e3cffb01b957f0691
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-11-16 23:38:20 +0530

    Time: 58 ms (40.00%) | Memory: 42.2 MB (58.82%) - LeetSync

---
+++ 2107-find-unique-binary-string/find-unique-binary-string.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string[]} nums
+ * @return {string}
+ */
+var findDifferentBinaryString = function(nums) {
+    let s = new Set();
+    let ans = "";
+    for(let num of nums) {
+        s.add(parseInt(num, 2));
+    }
+    for(let i=0; i<=nums.length; i++) {
+        if(!s.has(i)) ans = i;
+    }
+    ans = (ans >>> 0).toString(2);
+    while(ans.length < nums.length) {
+        ans = '0' + ans;
+    }
+    return ans;
+};
