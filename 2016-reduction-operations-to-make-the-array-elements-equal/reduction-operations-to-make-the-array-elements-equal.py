f52ce44da065333c4b99bbcd27910dbfb4b87218
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-11-19 12:08:05 +0530

    Time: 911 ms (20.10%) | Memory: 23.9 MB (58.25%) - LeetSync

--- 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.py
+++ 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.py
@@ -2,7 +2,7 @@ class Solution:
     def reductionOperations(self, nums: List[int]) -> int:
         ans = 0
         maxi = max(nums)
-        buckets = defaultdict(int)
+        buckets = [0 for i in range(maxi+1)]
 
         for num in nums:
             buckets[num] = buckets[num] + 1
