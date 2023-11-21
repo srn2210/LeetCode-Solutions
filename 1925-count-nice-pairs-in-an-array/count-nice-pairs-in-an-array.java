505952f63142271455a3dd3f140b4bfe5f4c4616
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-11-21 20:25:32 +0530

    Time: 26 ms (96.63%) | Memory: 54.1 MB (84.85%) - LeetSync

--- 1925-count-nice-pairs-in-an-array/count-nice-pairs-in-an-array.java
+++ 1925-count-nice-pairs-in-an-array/count-nice-pairs-in-an-array.java
@@ -1,5 +1,5 @@
 class Solution {
-    int reverse(int num, Map<Integer, Integer> map) {
+    void reverse(int num, Map<Integer, Integer> map) {
         int ans = 0;
         int cpy = num;
 
@@ -10,39 +10,14 @@ class Solution {
 
         int diff = cpy - ans;
         map.put(diff, map.getOrDefault(diff, 0) + 1);
-
-        return ans;
     }
     public int countNicePairs(int[] nums) {
         int n = nums.length, mod = (int)1e9+7;
         long ans = 0;
-        int[] rev = new int[n];
         var map = new HashMap<Integer, Integer>();
         for(int i=0; i<n; i++) {
-            rev[i] = reverse(nums[i], map);
+            reverse(nums[i], map);
         }
-
-        // System.out.println(map);
-
-        // var set = new HashSet<Integer>();
-
-        // for(var entry : map.entrySet()) {
-        //     int diff = entry.getKey();
-        //     int numDiff = entry.getValue();
-
-        //     int negDiff = -diff;
-
-        //     if(set.contains(negDiff) || diff == 0) continue;
-
-        //     if(map.containsKey(negDiff)) {
-        //         long t = numDiff * (long)map.get(negDiff);
-        //         t %= mod;
-        //         ans += t;
-        //         ans %= mod;
-        //         set.add(diff);
-        //         set.add(negDiff);
-        //     }
-        // }
 
         for(var entry : map.entrySet()) {
             int numDiff = entry.getValue();
