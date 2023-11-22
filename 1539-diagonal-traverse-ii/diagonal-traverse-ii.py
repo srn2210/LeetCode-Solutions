ba5daeef960e3bfc8a78551b554150171a52afa0
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-11-23 00:12:01 +0530

    Time: 986 ms (5.02%) | Memory: 44.2 MB (13.03%) - LeetSync

--- 1539-diagonal-traverse-ii/diagonal-traverse-ii.py
+++ 1539-diagonal-traverse-ii/diagonal-traverse-ii.py
@@ -7,15 +7,12 @@ class Solution:
         q.append([0,0])
         vis.add((0,0))
         while q:
-            size = len(q)
-            while size:
-                node = q.popleft()
-                ans.append(nums[node[0]][node[1]])
-                if node[0] + 1 < len(nums) and node[1] < len(nums[node[0]+1]) and (node[0]+1, node[1]) not in vis:
-                    q.append([node[0]+1, node[1]])
-                    vis.add((node[0]+1, node[1]))
-                if node[1] + 1 < len(nums[node[0]]) and (node[0], node[1]+1) not in vis:
-                    q.append([node[0], node[1]+1])
-                    vis.add((node[0], node[1]+1))
-                size -= 1
+            node = q.popleft()
+            ans.append(nums[node[0]][node[1]])
+            if node[0] + 1 < len(nums) and node[1] < len(nums[node[0]+1]) and (node[0]+1, node[1]) not in vis:
+                q.append([node[0]+1, node[1]])
+                vis.add((node[0]+1, node[1]))
+            if node[1] + 1 < len(nums[node[0]]) and (node[0], node[1]+1) not in vis:
+                q.append([node[0], node[1]+1])
+                vis.add((node[0], node[1]+1))
         return ans
