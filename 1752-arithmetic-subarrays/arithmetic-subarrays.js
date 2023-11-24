bb6a773d41dff9cdaed41bc2025fcaef430b7491
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-11-24 20:23:19 +0530

    Time: 90 ms (99.02%) | Memory: 48.1 MB (94.12%) - LeetSync

---
+++ 1752-arithmetic-subarrays/arithmetic-subarrays.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number[]} nums
+ * @param {number[]} l
+ * @param {number[]} r
+ * @return {boolean[]}
+ */
+var checkArithmeticSubarrays = function(nums, l, r) {
+    let n = nums.length;
+    const ans = [];
+
+    for(let i=0; i<l.length; i++) {
+        let s = new Set();
+        let min = 1e9;
+        let max = -1e9;
+        for(let curr=l[i]; curr<=r[i]; curr++) {
+            s.add(nums[curr]);
+            min = Math.min(min, nums[curr]);
+            max = Math.max(max, nums[curr]);
+        }
+        let diff = (max - min) / (r[i] - l[i]);
+        if(diff % 1 != 0) {
+            ans.push(false);
+            continue;
+        }
+        // console.log(min + " " + max + " " + l[i] + " " + r[i]);
+        let curr = min;
+        let flag = true;
+        for(let k=0; k<=r[i]-l[i]; k++) {
+            // console.log(curr);
+            if(!s.has(curr)) {
+                flag = false;
+                break;
+            }
+            curr += diff;
+        }
+        ans.push(flag);
+    }
+
+    return ans;
+};
