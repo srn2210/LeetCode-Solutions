858fd855dd599b215589e6e5b4379678c344aeac
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-11-25 17:46:16 +0530

    Time: 184 ms (57.14%) | Memory: 67.7 MB (96.43%) - LeetSync

--- 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.js
+++ 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.js
@@ -3,15 +3,16 @@
  * @return {number[]}
  */
 var getSumAbsoluteDifferences = function(nums) {
-    let sum = 0;
+    let post = 0;
     for(let num of nums) {
-        sum += num;
+        post += num;
     }
     let ans = [];
     let pre = 0, n = nums.length;
     for(let i=0; i<n; i++) {
+        post -= nums[i];
         let left = (nums[i] * i) - pre;
-        let right = (sum - pre - nums[i]) - ((n-i-1) * nums[i]);
+        let right = post - ((n-i-1) * nums[i]);
         ans.push(left + right);
         pre += nums[i];
     }
