bf1fc7547207690bc6ec8fa306a7332129a70dc3
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-11-28 20:33:38 +0530

    Time: 35 ms (55.26%) | Memory: 47.3 MB (63.16%) - LeetSync

--- 2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.java
+++ 2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.java
@@ -1,34 +1,7 @@
 class Solution {
     int mod = (int)1e9+7;
-    // int[] dp;
-    // int solve(String corr, int idx) {
-    //     if(idx >= corr.length()) return 1;
-    //     if(dp[idx] != -1) return dp[idx];
-    //     int curr = idx;
-    //     int count = 0;
-    //     long ans = 0;
-    //     while(idx < corr.length() && count < 2) {
-    //         if(corr.charAt(idx) == 'S') count++;
-    //         idx++;
-    //     }
-    //     if(count < 2) return 0;
-    //     ans += solve(corr, idx);
-    //     ans %= mod;
-    //     while(idx < corr.length()) {
-    //         if(corr.charAt(idx) == 'P') {
-    //             ans += solve(corr, idx+1);
-    //             ans %= mod;
-    //             idx++;
-    //         }
-    //         else break;
-    //     }
-    //     return dp[curr] = (int)ans%mod;
-    // }
     public int numberOfWays(String corridor) {
         long ans = 1;
-        // dp = new int[corridor.length()+1];
-        // Arrays.fill(dp, -1);
-        StringBuilder s = new StringBuilder();
         int streak = 0;
         var q = new LinkedList<String>();
         for(int i=0; i<corridor.length(); i++) {
@@ -40,7 +13,6 @@ class Solution {
                 boolean flag = true;
                 for(int j=i+1; j<corridor.length(); j++) {
                     if(corridor.charAt(j) == 'S') {
-                        s.append('S');
                         flag = false;
                         i = j;
                         q.add("S");
@@ -55,7 +27,6 @@ class Solution {
         }
         if(q.isEmpty()) return 0;
         if(!q.peek().equals("S")) q.poll();
-        // System.out.println(q);
         while(!q.isEmpty()) {
             if(q.peek().equals("S")) q.poll();
             else {
@@ -64,8 +35,6 @@ class Solution {
                 ans %= mod;
             }
         }
-        // for(int[] a : q) System.out.println(Arrays.toString(a));
-        // return solve(s.toString(), 0);
         return (int)ans;
     }
 }
