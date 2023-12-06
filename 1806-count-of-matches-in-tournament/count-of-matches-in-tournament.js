5cde011863ac90d183b5e42a581402d1b29d87d0
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-12-06 19:05:25 +0530

    Time: 61 ms (7.52%) | Memory: 41.6 MB (70.53%) - LeetSync

--- 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.js
+++ 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.js
@@ -6,7 +6,7 @@ var numberOfMatches = function(n) {
     let solve = (teams) => {
         if(teams === 1) return 0;
         let ans = 0;
-        if(teams % 2 == 0) {
+        if(teams % 2 === 0) {
             ans = teams / 2 + solve(teams / 2);
         }
         else {
