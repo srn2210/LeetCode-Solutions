ea4ba47faeb0e0329553bb617071a7515bf26b55
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-12-07 20:31:41 +0530

    Time: 85 ms (7.83%) | Memory: 53.4 MB (8.95%) - LeetSync

---
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} num
+ * @return {string}
+ */
+var largestOddNumber = function(num) {
+    let right = -1;
+    for(let i=num.length-1; i>=0; i--) {
+        if(num[i] % 2 != 0) {
+            right = i;
+            break;
+        }
+    }
+    let ans = '';
+    for(let i=0; i<=right; i++) {
+        ans += num[i];
+    }
+    return ans;
+};
