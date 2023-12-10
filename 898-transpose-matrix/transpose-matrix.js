842b6e2587d694ee5d29f50ff29048c2260ec7a7
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-12-10 17:56:54 +0530

    Time: 56 ms (87.17%) | Memory: 44.9 MB (22.19%) - LeetSync

--- 898-transpose-matrix/transpose-matrix.js
+++ 898-transpose-matrix/transpose-matrix.js
@@ -5,10 +5,7 @@
 var transpose = function(matrix) {
     let ans = new Array(matrix[0].length);
     for(let i=0; i<ans.length; i++) {
-        ans[i] = [];
-        for(let j=0; j<matrix.length; j++) {
-            ans[i].push(0);
-        }
+        ans[i] = new Array(matrix.length).fill(0);
     }
     for(let i=0; i<matrix.length; i++) {
         for(let j=0; j<matrix[0].length; j++) {
