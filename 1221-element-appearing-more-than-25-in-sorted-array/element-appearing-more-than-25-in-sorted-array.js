d1a1573125ba9e65900ad643ed81991295c162f1
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-12-11 14:19:30 +0530

    Time: 45 ms (94.15%) | Memory: 43.1 MB (54.79%) - LeetSync

--- 1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.js
+++ 1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.js
@@ -3,19 +3,32 @@
  * @return {number}
  */
 var findSpecialInteger = function(arr) {
-    let prev = -1;
-    let streak = 0;
-    let ans = 0;
-    for(let num of arr) {
-        if(prev === num) streak++;
-        else {
-            if(streak > arr.length / 4) return prev;
-            prev = num;
-            streak = 1;
+    let cand1 = arr[Math.round(arr.length/4)], cand2 = arr[Math.round(3 * (arr.length/4))], cand3 = arr[Math.round(arr.length/2)];
+
+    //console.log(cand1 + "  " + cand2 + "  " + cand3);
+    let solve = (cand) => {
+        let left = 0, right = arr.length-1;
+        while(left < right) {
+            let mid = (right + left) / 2;
+            // mid = Math.round(mid);
+            mid = mid - (mid % 1);
+            if(arr[mid] >= cand) right = mid;
+            else left = mid + 1;
         }
+        let leftMost = left;
+        //console.log(leftMost);
+        left = 0;
+        right = arr.length-1;
+        while(left < right) {
+            let mid = (right + left) / 2 + 1;
+            mid = mid - (mid % 1);
+            if(arr[mid] <= cand) left = mid;
+            else right = mid - 1;
+        }
+        let rightMost = right;
+        return rightMost - leftMost + 1 > arr.length / 4;
     }
-    if(streak != 0 && streak > arr.length / 4) {
-        return prev;
-    }
-    return ans;
+    if(solve(cand1)) return cand1;
+    else if(solve(cand2)) return cand2;
+    else return cand3;
 };
