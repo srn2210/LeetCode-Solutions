958bbcde6783107f2eacbb36855b6cd57a131891
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-12-23 02:17:15 +0530

    Time: 7 ms (14.60%) | Memory: 6.8 MB (29.65%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
@@ -1,13 +1,34 @@
 class Solution {
 public:
     int maxScore(string s) {
-        int ans = 0, curr = 0, total = 0;
-        for(auto ch : s) total += ch - '0';
-        for(int i=0; i<s.size()-1; i++) {
-            curr += ((s[i] - '0') ^ 1);
-            total -= s[i] - '0';
-            ans = max(ans, curr + total);
+        int ls,rs;
+        ls=0;
+        rs=0;
+        int maxval,ind;
+        maxval=INT_MIN;
+        int i;
+        int c0=0,c1=0,t0=0;
+        for(i=0;i<s.length();i++) {
+            if(s[i]=='0') {
+                ls++;
+                t0++;
+            }
+            else {
+                ls--;
+                c1++;
+            }
+            cout<<ls<<" ";
+            if(ls>maxval) {
+                maxval=ls;
+                c0=t0;
+                ind=i;
+                c1=0;
+            }
+            cout<<"count 0="<<c0<<" count 1="<<c1<<endl;
         }
-        return ans;
+        cout<<maxval<<" "<<ind<<endl;
+        cout<<c0<<" "<<c1<<endl;
+        if(ind==s.length()-1) return c0+c1-1;
+        return c0+c1;
     }
 };
