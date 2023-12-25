2ee09e488fe8e25aae1eede5213f83c7e574cdd5
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-12-25 19:57:04 +0530

    Time: 1 ms (84.19%) | Memory: 41.9 MB (8.23%) - LeetSync

---
+++ 91-decode-ways/decode-ways.java
@@ -0,0 +1,16 @@
+class Solution {
+    int[] dp;
+    int solve(String s, int idx) {
+        if(idx >= s.length()) return 1;
+        if(s.charAt(idx) == '0') return 0;
+        if(dp[idx] != -1) return dp[idx];
+        int ans = solve(s, idx+1);
+        if(idx + 2 <= s.length() && Integer.parseInt(s.substring(idx, idx+2)) <= 26) ans += solve(s, idx+2);
+        return dp[idx] = ans;
+    }
+    public int numDecodings(String s) {
+        dp = new int[s.length()+1];
+        Arrays.fill(dp, -1);
+        return solve(s, 0);
+    }
+}
