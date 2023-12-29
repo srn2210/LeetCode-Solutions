3928c9dc0f67b5e21fc4647c8bf070905e2009c4
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2023-12-29 21:53:57 +0530

    Time: 23 ms (26.52%) | Memory: 41.1 MB (26.72%) - LeetSync

--- 1457-minimum-difficulty-of-a-job-schedule/minimum-difficulty-of-a-job-schedule.java
+++ 1457-minimum-difficulty-of-a-job-schedule/minimum-difficulty-of-a-job-schedule.java
@@ -2,18 +2,14 @@ class Solution {
     int max = (int)1e8;
     int[][] dp;
     int solve(int[] diff, int d, int idx) {
-        if(idx == diff.length && d == 0) {
-            return 0;
-        }
+        if(idx == diff.length && d == 0) return 0;
         if(d < 0 || idx == diff.length) return max;
         if(dp[idx][d] != -1) return dp[idx][d];
-        // int currDiff = Math.max(currDiff, diff[idx]);
         int ans = max;
         int currDiff = -1;
         for(int i=idx; i<diff.length; i++) {
             currDiff = Math.max(currDiff, diff[i]);
             ans = Math.min(ans, currDiff + solve(diff, d-1, i+1));
-            // System.out.println(ans);
         }
         return dp[idx][d] = ans;
     }
