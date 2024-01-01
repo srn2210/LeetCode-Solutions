65bd1c4478fbfa27d4b80a272532557fa234617d
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2024-01-01 22:15:11 +0530

    Time: 376 ms (24.28%) | Memory: 44.1 MB (92.03%) - LeetSync

--- 1637-string-compression-ii/string-compression-ii.js
+++ 1637-string-compression-ii/string-compression-ii.js
@@ -4,7 +4,11 @@
  * @return {number}
  */
 var getLengthOfOptimalCompression = function(s, k) {
-    let dp = new Map();
+    let dp = new Array(s.length+1);
+
+    for(let i=0; i<dp.length; i++) {
+        dp[i] = new Array(k+1).fill(-1);
+    }
 
     function cost(run) {
         if(run === 0) return 0;
@@ -14,22 +18,27 @@ var getLengthOfOptimalCompression = function(s, k) {
         return 4;
     }
 
-    function solve(si, ki, prev, run) {
+    function solve(si, ki) {
         if(ki < 0) return 1e8;
-        if(si === s.length) return cost(run);
+        if(si === s.length) return 0;
 
-        const key = `${si}_${ki}_${prev}_${run}`;
-        if(key in dp) return dp[key];
+        // const key = `${si}_${ki}`;
+        // if(key in dp) return dp[key];
+        if(dp[si][ki] != -1) return dp[si][ki];
 
-        let ans = solve(si+1, ki-1, prev, run);
-        if(prev === s[si]) {
-            ans = Math.min(ans, solve(si+1, ki, s[si], run+1));
+        let ans = 1e8;
+        let run = 0;
+        let deleted = 0;
+        for(let j=si; j<s.length; j++) {
+            if(s[j] === s[si]) {
+                run++;
+            }
+            else deleted++;
+            ans = Math.min(ans, solve(j+1, ki-j+si-1));
+            ans = Math.min(ans, cost(run) + solve(j+1, ki-deleted));
         }
-        else {
-            ans = Math.min(ans, cost(run) + solve(si+1, ki, s[si], 1));
-        }
-        return dp[key] = ans;
+        return dp[si][ki] = ans;
     }
 
-    return solve(0, k, '', 0);
+    return solve(0, k);
 };
