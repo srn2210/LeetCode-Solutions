c32227830fdd8151d32f45381b33fe50f58adac4
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2024-01-03 20:11:25 +0530

    Time: 102 ms (32.94%) | Memory: 49.9 MB (74.12%) - LeetSync

---
+++ 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string[]} bank
+ * @return {number}
+ */
+var numberOfBeams = function(bank) {
+    let prevCount = 0, n = bank.length, m = bank[0].length;
+    let ans = 0;
+    for(let floor of bank) {
+        let count = 0;
+        for(let device of floor) {
+            if(device === '1') count++;
+        }
+        ans += count * prevCount;
+        if(count != 0) prevCount = count;
+    } 
+    return ans;
+};
