61d7f5a27ace596b4b419cc3af53d1030f20e254
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2024-01-24 20:33:02 +0530

    Time: 8 ms (87.19%) | Memory: 70.4 MB (40.57%) - LeetSync

--- 1568-pseudo-palindromic-paths-in-a-binary-tree/pseudo-palindromic-paths-in-a-binary-tree.java
+++ 1568-pseudo-palindromic-paths-in-a-binary-tree/pseudo-palindromic-paths-in-a-binary-tree.java
@@ -16,8 +16,7 @@
 class Solution {
     int solve(TreeNode node, int mask) {
         if(node == null) return 0;
-        int val = node.val;
-        mask ^= (1 << val);
+        mask ^= (1 << node.val);
         int ans = solve(node.left, mask) + solve(node.right, mask);
         if(node.left == null && node.right == null) ans += Integer.bitCount(mask) <= 1 ? 1 : 0;
         return ans;
