c86a22092407903fb101dc8ee8af2b60c89fc605
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2024-01-29 23:36:21 +0530

    Time: 27 ms (48.88%) | Memory: 46.3 MB (72.04%) - LeetSync

--- 629-k-inverse-pairs-array/k-inverse-pairs-array.java
+++ 629-k-inverse-pairs-array/k-inverse-pairs-array.java
@@ -1,19 +1,28 @@
 class Solution {
-    public int kInversePairs(int n, int k) {
-        int[][] dp = new int[1001][1001];
-        dp[0][0] = 1;
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <= k; j++) {
-                for (int x = 0; x <= Math.min(j, i - 1); x++) {
-                    if (j - x >= 0) {
-                        dp[i][j] = (dp[i][j] + dp[i - 1][j - x]) % 1000000007;
-                    }
-                }
+    int[][] dp;
+    int mod = (int)1e9+7;
+    int solve(int n, int k) {
+        if(n == 0) {
+            if(k == 0) return 1;
+            return 0;
+        }
+        if(dp[n][k] != -1) return dp[n][k];
+        long ans = 0;
+        ans += solve(n-1, k);
+        if(k >= 1) {
+            ans += solve(n, k-1);
+            ans %= mod;
+            if(k >= n + 1) {
+                ans = (ans - solve(n-1, k - n - 1) + mod) % mod;
             }
         }
-
-        return dp[n][k];
+        
+        ans %= mod;
+        return dp[n][k] = (int)ans;
+    }
+    public int kInversePairs(int n, int k) {
+        dp = new int[n][k+1];
+        for(int i=0; i<n; i++) Arrays.fill(dp[i], -1);
+        return solve(n-1, k);
     }
 }
-
