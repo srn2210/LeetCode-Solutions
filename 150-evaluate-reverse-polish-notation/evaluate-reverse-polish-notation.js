158a452799e598c3101a3625aef59b128b11e4ab
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2024-01-30 22:05:46 +0530

    Time: 71 ms (35.16%) | Memory: 52.2 MB (16.42%) - LeetSync

---
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function(tokens) {
+    let list = [];
+
+    for(let token of tokens) {
+        // console.log(list);
+        if(token === '+') {
+            let second = list.pop();
+            let first = list.pop();
+            let third = first + second;
+            list.push(third);
+        }
+        else if(token === '-') {
+            let second = list.pop();
+            let first = list.pop();
+            list.push(first-second);
+        }
+        else if(token === '*') {
+            let second = list.pop();
+            let first = list.pop();
+            list.push(first*second);
+        }
+        else if(token === '/') {
+            let second = list.pop();
+            let first = list.pop();
+            let res = Math.trunc(first / second);
+            list.push(res);
+        }
+        else {
+            list.push(token*1);
+        }
+    }
+    return list.pop();
+};
