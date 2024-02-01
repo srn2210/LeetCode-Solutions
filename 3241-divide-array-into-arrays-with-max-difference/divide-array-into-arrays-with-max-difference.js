9ad5babc6e67b1c73532c77501f34ae01076ed9b
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2024-02-01 21:38:57 +0530

    Time: 371 ms (16.83%) | Memory: 79.9 MB (94.92%) - LeetSync

---
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[][]}
+ */
+var divideArray = function(nums, k) {
+    let ans = [];
+    nums.sort((a,b) => a - b);
+    for(let i=0; i<nums.length; i+=3) {
+        if(nums[i] + k >= nums[i+2]) ans.push([nums[i], nums[i+1], nums[i+2]]);
+        else return [];
+    }
+    return ans;
+};
