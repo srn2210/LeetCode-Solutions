9b73e5a08ac461ba0245d2d96635ba75c555519a
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2024-02-05 01:42:00 +0530

    Time: 75 ms (85.71%) | Memory: 54 MB (11.09%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.js
@@ -0,0 +1,40 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {string}
+ */
+var minWindow = function(s, t) {
+    let left = 0, right = 0;
+    let ansLeft = -1, ansRight = -1;
+    let count = 0;
+    let arr = new Array(128).fill(0);
+    for(let i=0; i<t.length; i++) {
+        arr[t[i].charCodeAt(0)-'A'.charCodeAt(0)]++;
+    }
+    for(let i=0; i<128; i++) if(arr[i] > 0) count++;
+    let currCount = new Array(128).fill(0);
+    let ans = s.length;
+    while(left <= right && right < s.length) {
+        currCount[s[right].charCodeAt(0)-'A'.charCodeAt(0)]++;
+        if(currCount[s[right].charCodeAt(0)-'A'.charCodeAt(0)] === arr[s[right].charCodeAt(0)-'A'.charCodeAt(0)]) {
+            count--;
+        }         
+        while(count === 0) {
+            if(ans >= right - left + 1) {
+                ansLeft = left;
+                ansRight = right;
+                ans = right - left + 1;
+            }
+            currCount[s[left].charCodeAt(0)-'A'.charCodeAt(0)]--;
+            if(currCount[s[left].charCodeAt(0)-'A'.charCodeAt(0)] + 1 === arr[s[left].charCodeAt(0)-'A'.charCodeAt(0)]) {
+                count++;
+            }
+            left++;
+        }        
+        right++;
+    }
+    if(ansLeft === -1 && ansRight === -1) return '';
+    let res = '';
+    for(let i=ansLeft; i<=ansRight; i++) res += s[i];
+    return res;
+};
