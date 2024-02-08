85697bd5649405b8c1f6cf88c89054c1a315c96f
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2024-02-08 21:40:10 +0530

    Time: 264 ms (26.87%) | Memory: 53.1 MB (26.04%) - LeetSync

---
+++ 279-perfect-squares/perfect-squares.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numSquares = function(n) {
+    let perf = [];
+    for(let i=1; i<=n; i++) {
+        if(Math.sqrt(i) % 1 === 0) perf.push(i);
+    }
+
+    let dp = new Array(n+1).fill(-1);
+    
+    function solve(num) {
+        if(num === 0) return 0;
+        if(dp[num] !== -1) return dp[num];
+        let ans = 1e9;
+        let ind = Math.trunc(Math.sqrt(num));
+        for(let i=ind-1; i>=0; i--) {
+            if(num-perf[i] >= 0) ans = Math.min(ans, solve(num-perf[i]) + 1);
+        }
+
+        return dp[num] = ans;
+    }
+
+    return solve(n);
+};
