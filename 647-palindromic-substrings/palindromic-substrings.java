2e7e2eec4345a58889c6f0f91e0f240477afab07
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2024-02-10 22:27:56 +0530

    Time: 22 ms (18.60%) | Memory: 50.3 MB (5.04%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,4 +1,5 @@
 class Solution {
+    int res;
     Boolean[][] dp;
     boolean solve(String s, int start, int end) {
         if(start > end) return true;
@@ -6,20 +7,22 @@ class Solution {
         boolean ans = false;
         if(s.charAt(start) == s.charAt(end)) {
             ans = solve(s, start+1, end-1);
+            if(ans) res++;
         }
         solve(s, start+1, end);
         solve(s, start, end-1);
         return dp[start][end] = ans;
     }
     public int countSubstrings(String s) {
+        res = 0;
         dp = new Boolean[s.length()][s.length()];
         solve(s, 0, s.length()-1);
-        int ans = 0;
-        for(int i=0; i<s.length(); i++) {
-            for(int j=i; j<s.length(); j++) {
-                if(dp[i][j]) ans++;
-            }
-        }
-        return ans;
+        // int ans = 0;
+        // for(int i=0; i<s.length(); i++) {
+        //     for(int j=i; j<s.length(); j++) {
+        //         if(dp[i][j]) ans++;
+        //     }
+        // }
+        return res;
     }
 }
