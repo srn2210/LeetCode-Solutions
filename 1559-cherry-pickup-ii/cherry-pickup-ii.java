7c78c7e5636bb830cefb3ce0eca28906c12dedb6
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2024-02-11 14:17:39 +0530

    Time: 53 ms (14.57%) | Memory: 44.5 MB (92.94%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.java
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.java
@@ -1,18 +1,20 @@
 class Solution {
     int[] directions = {-1, 0, 1};
     public int cherryPickup(int[][] grid) {
-        int[][][] dp = new int[grid.length][grid[0].length][grid[0].length];
-        for(int i=0; i<dp.length; i++) {
-            for(int j=0; j<dp[0].length; j++) {
-                Arrays.fill(dp[i][j], -100000);
-            }
+        int n = grid[0].length;
+        int[][] prev = new int[grid[0].length][grid[0].length];
+        for(int i=0; i<n; i++) {
+            Arrays.fill(prev[i], -100000);
         }
-        dp[0][0][grid[0].length-1] = grid[0][0] + grid[0][grid[0].length-1];
+        prev[0][grid[0].length-1] = grid[0][0] + grid[0][grid[0].length-1];
         int ans = 0;
-
         for(int i=1; i<grid.length; i++) {
+            int[][] dp = new int[grid[0].length][grid[0].length];
+            for(int l=0; l<n; l++) {
+                Arrays.fill(dp[l], -100000);
+            }
             for(int j=0; j<grid[0].length; j++) {
-                for(int k=j+1; k<grid[0].length; k++) {
+                for(int k=0; k<grid[0].length; k++) {
                     for(int j1 : directions) {
                         for(int j2 : directions) {
                             int y1 = j1 + j;
@@ -21,12 +23,13 @@ class Solution {
                             int val = 0;
                             if(y1 == y2) val = grid[i][y1];
                             else val = grid[i][y1] + grid[i][y2];
-                            dp[i][y1][y2] = Math.max(dp[i-1][j][k] + val, dp[i][y1][y2]);
-                            ans = Math.max(ans, dp[i][y1][y2]);
+                            dp[y1][y2] = Math.max(prev[j][k] + val, dp[y1][y2]);
+                            ans = Math.max(ans, dp[y1][y2]);
                         }
                     }
                 }
             }
+            prev = dp;
         }
         return ans;
     }
