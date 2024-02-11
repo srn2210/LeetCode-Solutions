d288e27810739b788f5f7007228cda7ae4008564
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2024-02-11 14:06:44 +0530

    Time: 57 ms (11.26%) | Memory: 46.1 MB (43.27%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.java
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.java
@@ -1,31 +1,33 @@
 class Solution {
-    int[][][] dp;
     int[] directions = {-1, 0, 1};
-    int solve(int[][] grid, int i, int j1, int j2) {
-        if(j1 < 0 || j2 < 0 || j1 >= grid[0].length || j2 >= grid[0].length) return -100000;
-        if(i == grid.length) return 0;
-        if(dp[i][j1][j2] != -1) return dp[i][j1][j2];
-        int ans = 0;
-        if(j1 == j2) ans = grid[i][j1];
-        else ans = grid[i][j1] + grid[i][j2];
-        int res = 0;
-        for(int dir : directions) {
-            for(int dir2 : directions) {
-                int x = i + 1;
-                int y1 = j1 + dir;
-                int y2 = j2 + dir2;
-                res = Math.max(res, ans + solve(grid, x, y1, y2));
-            }
-        }
-        return dp[i][j1][j2] = res;
-    }
     public int cherryPickup(int[][] grid) {
-        dp = new int[grid.length][grid[0].length][grid[0].length];
+        int[][][] dp = new int[grid.length][grid[0].length][grid[0].length];
         for(int i=0; i<dp.length; i++) {
-            for(int j=0; j<dp[i].length; j++) {
-                Arrays.fill(dp[i][j], -1);
+            for(int j=0; j<dp[0].length; j++) {
+                Arrays.fill(dp[i][j], -100000);
             }
         }
-        return solve(grid, 0, 0, grid[0].length-1);
+        dp[0][0][grid[0].length-1] = grid[0][0] + grid[0][grid[0].length-1];
+        int ans = 0;
+
+        for(int i=1; i<grid.length; i++) {
+            for(int j=0; j<grid[0].length; j++) {
+                for(int k=0; k<grid[0].length; k++) {
+                    for(int j1 : directions) {
+                        for(int j2 : directions) {
+                            int y1 = j1 + j;
+                            int y2 = j2 + k;
+                            if(y1 < 0 || y2 < 0 || y1 >= grid[0].length || y2 >= grid[0].length) continue;
+                            int val = 0;
+                            if(y1 == y2) val = grid[i][y1];
+                            else val = grid[i][y1] + grid[i][y2];
+                            dp[i][y1][y2] = Math.max(dp[i-1][j][k] + val, dp[i][y1][y2]);
+                            ans = Math.max(ans, dp[i][y1][y2]);
+                        }
+                    }
+                }
+            }
+        }
+        return ans;
     }
 }
