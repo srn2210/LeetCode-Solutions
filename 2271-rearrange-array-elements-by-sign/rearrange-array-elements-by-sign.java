67aff945f78d2f965031eeb518cccd6ef33e73af
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2024-02-15 00:39:33 +0530

    Time: 11 ms (9.02%) | Memory: 84.9 MB (11.26%) - LeetSync

---
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+        var pos = new LinkedList<Integer>();
+        var neg = new LinkedList<Integer>();
+        int ptr = 0, fwd = 0;
+        while(ptr < nums.length) {
+            int num = nums[ptr];
+            if(ptr % 2 == 0) {
+                if(!pos.isEmpty()) nums[ptr++] = pos.poll();
+                else {
+                    while(fwd < nums.length && nums[fwd] < 0) {
+                        neg.add(nums[fwd++]);
+                    }
+                    nums[ptr++] = nums[fwd++];
+                }
+            }
+            else {
+                if(!neg.isEmpty()) nums[ptr++] = neg.poll();
+                else {
+                    while(fwd < nums.length && nums[fwd] > 0) {
+                        pos.add(nums[fwd++]);
+                    }
+                    nums[ptr++] = nums[fwd++];
+                }
+            }
+        }
+        return nums;
+    }
+}
