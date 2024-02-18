9606229e028c8464c11779ae109c6baa2a1ccc97
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2024-02-18 16:17:19 +0530

    Time: 84 ms (27.93%) | Memory: 97 MB (94.33%) - LeetSync

---
+++ 2479-meeting-rooms-iii/meeting-rooms-iii.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int mostBooked(int n, int[][] meetings) {
+        Arrays.sort(meetings, (a,b) -> Integer.compare(a[0], b[0]));
+        int[] arr = new int[n];
+        var process = new PriorityQueue<long[]>((a,b) -> Long.compare(a[0], b[0]) == 0 ? Long.compare(a[1], b[1]) : Long.compare(a[0], b[0]));
+        var pq = new PriorityQueue<Integer>();
+        for(int i=0; i<n; i++) pq.add(i);
+        long time = 0;
+        for(int[] meeting : meetings) {
+            int start = meeting[0];
+            int end = meeting[1];
+            int duration = end - start;
+            time = Math.max(time, start);
+            while(!process.isEmpty() && time >= process.peek()[0]) {
+                pq.add((int)process.poll()[1]);
+            }
+            if(!pq.isEmpty()) {
+                int room = pq.poll();
+                arr[room]++;
+                long[] t = {time + duration, room};
+                process.add(t);
+            }
+            else {
+                long[] tt = process.poll();
+                time = tt[0];
+                pq.add((int)tt[1]);
+                int room = pq.poll();
+                arr[room]++;
+                long[] t = {time + duration, room};
+                process.add(t);
+            }
+        }
+        int max = 0, idx = 0;
+        for(int i=0; i<n; i++) {
+            if(arr[i] > max) {
+                max = arr[i];
+                idx = i;
+            }
+        }
+        return idx;
+    }
+}
