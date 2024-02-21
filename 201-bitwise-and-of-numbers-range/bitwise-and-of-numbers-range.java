7f3e87e35023eaecf69a7f2099e3c1b4ddc09e6b
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2024-02-21 23:36:55 +0530

    Time: 4 ms (24.31%) | Memory: 44.1 MB (30.56%) - LeetSync

--- 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.java
+++ 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.java
@@ -1,26 +1,11 @@
 class Solution {
     public int rangeBitwiseAnd(int left, int right) {
-        int ans = 0;
-        var s1 = Integer.toBinaryString(left);
-        var s2 = Integer.toBinaryString(right);
-        if(s1.length() < 31) {
-            var sbr = new StringBuilder();
-            for(int i=0; i<(31-s1.length()); i++) sbr.append('0');
-            sbr.append(s1);
-            s1 = sbr.toString();
+        int shift = 0;
+        while(left != right) {
+            left >>= 1;
+            right >>= 1;
+            shift++;
         }
-        if(s2.length() < 31) {
-            var sbr = new StringBuilder();
-            for(int i=0; i<(31-s2.length()); i++) sbr.append('0');
-            sbr.append(s2);
-            s2 = sbr.toString();
-        }
-        for(int i=0; i<31; i++) {
-            if(s1.charAt(i) == s2.charAt(i)) {
-                if(s1.charAt(i) == '1') ans = ans | (1 << (31 - i - 1));
-            }
-            else break;
-        }
-        return ans;
+        return (left << shift);
     }
 }
