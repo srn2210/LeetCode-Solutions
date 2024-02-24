3480f2580ad161f2bd5eeb34c252195ea4f280e7
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2024-02-24 23:49:40 +0530

    Time: 385 ms (21.10%) | Memory: 101 MB (24.77%) - LeetSync

--- 3266-find-longest-special-substring-that-occurs-thrice-ii/find-longest-special-substring-that-occurs-thrice-ii.java
+++ 3266-find-longest-special-substring-that-occurs-thrice-ii/find-longest-special-substring-that-occurs-thrice-ii.java
@@ -2,22 +2,18 @@ class Solution {
     public int maximumLength(String s) {
         int left = 0, right = 0;
         int ans = -1;
-        var map = new HashMap<Pair<Character, Integer>, Integer>();
+        var map = new HashMap<Character, Map<Integer, Integer>>();
         while(left <= right && right < s.length()) {
             while(right < s.length() && s.charAt(left) == s.charAt(right)) {
                 right++;
             }
             int len = right - left;
             for(int i=0; i<len; i++) {
-                var p = new Pair<Character, Integer>(s.charAt(left), len - i);
+                char ch = s.charAt(left);
+                int currLen = len - i;
                 int occurences = i + 1;
-                if(map.containsKey(p)) {
-                    map.put(p, map.get(p)+occurences);
-                }
-                else {
-                    map.put(p, occurences);
-                }
-                if(map.get(p) >= 3) ans = Math.max(ans, len - i);
+                map.computeIfAbsent(ch, a -> new HashMap<>()).put(currLen, map.get(ch).getOrDefault(currLen, 0) + occurences);
+                if(map.get(ch).get(currLen) >= 3) ans = Math.max(ans, currLen);
             }
             left = right;
         }
