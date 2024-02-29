aac7e6134a5143a36f0c6b7fbbc8f74e3505f9d4
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2024-02-29 22:29:42 +0530

    Time: 9 ms (99.22%) | Memory: 66.4 MB (27.54%) - LeetSync

---
+++ 1731-even-odd-tree/even-odd-tree.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isEvenOddTree(TreeNode root) {
+        var q = new LinkedList<TreeNode>();
+        q.add(root);
+
+        int row = 0;
+        while(!q.isEmpty()) {
+            int size = q.size();
+            int prev = row % 2 == 0 ? 0 : Integer.MAX_VALUE;
+            while(size-- > 0) {
+                var node = q.poll();
+                if(row % 2 == 0) {
+                    if(node.val % 2 == 0 || node.val <= prev) return false;
+                    prev = node.val;
+                }
+                else {
+                    if(node.val % 2 != 0 || node.val >= prev) return false;
+                    prev = node.val;
+                }
+                if(node.left != null) q.add(node.left);
+                if(node.right != null) q.add(node.right);
+            }
+            row++;
+        }
+
+        return true;
+    }
+}
