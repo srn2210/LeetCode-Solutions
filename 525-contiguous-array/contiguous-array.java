50e69275c55f977295c67dc93f7b11938161905d
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2024-03-16 16:23:16 +0530

    Time: 34 ms (5.92%) | Memory: 55.1 MB (42.69%) - LeetSync

--- 525-contiguous-array/contiguous-array.java
+++ 525-contiguous-array/contiguous-array.java
@@ -3,13 +3,11 @@ class Solution {
         int n = nums.length;
         int sum = 0, ans = 0;
         var map = new HashMap<Integer, Integer>();
+        map.put(0, -1);
         for(int i=0; i<n; i++) {
             sum += nums[i] == 0 ? -1 : 1;
-            if(sum == 0) ans = Math.max(ans, i+1);
-            else {
-                if(!map.containsKey(sum)) map.put(sum, i);
-                if(map.containsKey(sum)) ans = Math.max(ans, i - map.get(sum));
-            }
+            if(!map.containsKey(sum)) map.put(sum, i);
+            if(map.containsKey(sum)) ans = Math.max(ans, i - map.get(sum));
         }
         return ans;
     }
