db3fea31b219a1fc12de825695c6a5bb099aa770
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2024-03-16 16:24:53 +0530

    Time: 23 ms (80.05%) | Memory: 55.3 MB (30.88%) - LeetSync

--- 525-contiguous-array/contiguous-array.java
+++ 525-contiguous-array/contiguous-array.java
@@ -6,8 +6,8 @@ class Solution {
         map.put(0, -1);
         for(int i=0; i<n; i++) {
             sum += nums[i] == 0 ? -1 : 1;
-            if(!map.containsKey(sum)) map.put(sum, i);
             if(map.containsKey(sum)) ans = Math.max(ans, i - map.get(sum));
+            else map.put(sum, i);
         }
         return ans;
     }
