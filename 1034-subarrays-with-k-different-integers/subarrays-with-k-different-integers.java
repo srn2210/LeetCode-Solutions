425fa91cb132e9fb8626ae64a4f3c55b2b5ddcad
Purijala Sreeramanath <110111501+srn2210@users.noreply.github.com>
2024-03-31 00:20:33 +0530

    Time: 36 ms (74.18%) | Memory: 47 MB (61.27%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
@@ -17,39 +17,19 @@ class Solution {
         }
         left = 0;
         right = 0;
-        // while(right <= n) {
-        //     while(right < n && map.size() <= k) {
-        //         int curr = nums[right];
-        //         map.put(curr, map.getOrDefault(curr, 0)+1);
-        //         right++;
-        //     }
-        //     if(right == n && map.size() <= k) break;
-        //     ans += left;
-        //     if(map.get(nums[left]) == 1) map.remove(nums[left]);
-        //     else map.put(nums[left], map.get(nums[left])-1);
-        //     left++;
-        // }
         map.clear();
         long res = 0;
-        boolean flag = false;
         while(right < n) {
             int curr = nums[right];
             map.put(curr, map.getOrDefault(curr, 0)+1);
-            // System.out.println("hi = " + res + " right = " + right + " left = " + left);
             while(map.size() > k) {
                 if(map.get(nums[left]) == 1) map.remove(nums[left]);
                 else map.put(nums[left], map.get(nums[left])-1);
                 left++;
             }
             res += right - left + 1;
-            // System.out.println("hi = " + res + " right = " + right + " left = " + left);
             right++;
         }
-        // System.out.println(res);
-        // System.out.println(ans);
-        // System.out.println(res - ans);
-        // long total = (long)n * (n+1) / 2;
-        // total = total - ans;
         return (int)(res-ans);
     }
 }
